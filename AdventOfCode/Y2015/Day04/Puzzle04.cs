09546cc8cc3a4a3a579e83611cf8910cacf66f64
Richard Flamsholt <richard@flamsholt.dk>
2020-01-27 01:45:37 +0100

    Day 4: The Ideal Stocking Stuffer

--- AdventOfCode/Y2015/Day04/Puzzle04.cs
+++ AdventOfCode/Y2015/Day04/Puzzle04.cs
@@ -1,10 +1,8 @@
-using AdventOfCode.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.IO;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace AdventOfCode.Y2015.Day04
 {
@@ -12,22 +10,37 @@ namespace AdventOfCode.Y2015.Day04
 	{
 		public static void Run()
 		{
-			Puzzle1();
-			Puzzle2();
+			Puzzle1And2();
 		}
 
-		private static void Puzzle1()
+		private static void Puzzle1And2()
 		{
+			var answer1 = FirstZerosAt("bgvyzdsv", 0, hash => hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0);
+			Console.WriteLine($"Day  4 Puzzle 1: {answer1}");
+			Debug.Assert(answer1 == 254575);
 
-			//Console.WriteLine($"Day 04 Puzzle 1: {result}");
-			//Debug.Assert(result == );
+			// Puzzle 2 can resume searching where Puzzle 1 ended
+			var answer2 = FirstZerosAt("bgvyzdsv", answer1, hash => hash[0] == 0 && hash[1] == 0 && hash[2] == 0);
+			Console.WriteLine($"Day  4 Puzzle 2: {answer2}");
+			Debug.Assert(answer2 == 1038736);
 		}
 
-		private static void Puzzle2()
+		private static int FirstZerosAt(string input, int start, Func<byte[], bool> condition)
 		{
-
-			//Console.WriteLine($"Day 04 Puzzle 2: {result}");
-			//Debug.Assert(result == );
+			var md5 = MD5.Create();
+			var secret = input.ToCharArray().Select(x => (byte)x).ToArray();
+			var buffer = new byte[100]; // more than big enough
+			Array.Copy(secret, 0, buffer, 0, secret.Length);
+			for (var i = start; ; i++)
+			{
+				var guess = Encoding.ASCII.GetBytes(i.ToString());
+				Array.Copy(guess, 0, buffer, secret.Length, guess.Length);
+				var hash = md5.ComputeHash(buffer, 0, secret.Length + guess.Length);
+				if (condition(hash))
+				{
+					return i;
+				}
+			}
 		}
 	}
 }
