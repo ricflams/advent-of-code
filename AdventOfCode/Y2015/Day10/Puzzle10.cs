1456989a024655e361b38479953485f190504e28
Richard Flamsholt <richard@flamsholt.dk>
2020-01-28 00:57:00 +0100

    Day 10: Elves Look, Elves Say

--- AdventOfCode/Y2015/Day10/Puzzle10.cs
+++ AdventOfCode/Y2015/Day10/Puzzle10.cs
@@ -1,10 +1,6 @@
-using AdventOfCode.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2015.Day10
 {
@@ -18,16 +14,79 @@ namespace AdventOfCode.Y2015.Day10
 
 		private static void Puzzle1()
 		{
+			var input = "3113322113";
 
-			//Console.WriteLine($"Day 10 Puzzle 1: {result}");
-			//Debug.Assert(result == );
+			var length = LookAndSayLengthAfter(input, 40);
+			Console.WriteLine($"Day 10 Puzzle 1: {length}");
+			Debug.Assert(length == 329356);
 		}
 
 		private static void Puzzle2()
 		{
+			var input = "3113322113";
 
-			//Console.WriteLine($"Day 10 Puzzle 2: {result}");
-			//Debug.Assert(result == );
+			var length = LookAndSayLengthAfter(input, 50);
+			Console.WriteLine($"Day 10 Puzzle 2: {length}");
+			Debug.Assert(length == 4666278);
 		}
+
+		private static int LookAndSayLengthAfter(string input, int iterations)
+		{
+			var sequence = input.Select(ch => (byte)(ch - '0')).ToArray();
+			var length = sequence.Length;
+			for (var i = 0; i < iterations; i++)
+			{
+				(sequence, length) = LookAndSay(sequence, length);
+			}
+			return length;
+		}
+
+		private static (byte[], int) LookAndSay(byte[] input, int inputlen)
+		{
+			// 1 becomes 11 (1 copy of digit 1).
+			// 11 becomes 21 (2 copies of digit 1).
+			// 21 becomes 1211 (one 2 followed by one 1).
+			// 1211 becomes 111221 (one 1, one 2, and two 1s).
+			// 111221 becomes 312211 (three 1s, two 2s, and one 1).
+			var output = new byte[inputlen * 2];
+			var outputlen = 0;
+			for (var i = 0; i < inputlen; i++)
+			{
+				var ch = input[i];
+				var n = 1;
+				while (i + 1 < inputlen && input[i + 1] == ch)
+				{
+					n++;
+					i++;
+				}
+				output[outputlen++] = (byte)n;
+				output[outputlen++] = ch;
+			}
+			return (output, outputlen);
+		}
+
+		// First attempt; slow string-operations
+		//private static string LookAndSay(string s)
+		//{
+		//	// 1 becomes 11 (1 copy of digit 1).
+		//	// 11 becomes 21 (2 copies of digit 1).
+		//	// 21 becomes 1211 (one 2 followed by one 1).
+		//	// 1211 becomes 111221 (one 1, one 2, and two 1s).
+		//	// 111221 becomes 312211 (three 1s, two 2s, and one 1).
+		//	var sb = new StringBuilder(s.Length * 2);
+		//	for (var i = 0; i < s.Length; i++)
+		//	{
+		//		var ch = s[i];
+		//		var n = 1;
+		//		while (i + 1 < s.Length && s[i + 1] == ch)
+		//		{
+		//			n++;
+		//			i++;
+		//		}
+		//		sb.Append(n);
+		//		sb.Append(ch);
+		//	}
+		//	return sb.ToString();
+		//}
 	}
 }
