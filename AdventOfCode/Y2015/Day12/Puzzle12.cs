f660839bfc0fe37d84c14469af41da4857d2f2bc
Richard Flamsholt <richard@flamsholt.dk>
2020-01-28 20:48:01 +0100

    Day 12: JSAbacusFramework.io

--- AdventOfCode/Y2015/Day12/Puzzle12.cs
+++ AdventOfCode/Y2015/Day12/Puzzle12.cs
@@ -1,10 +1,9 @@
-using AdventOfCode.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.IO;
-using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AdventOfCode.Y2015.Day12
 {
@@ -18,16 +17,50 @@ namespace AdventOfCode.Y2015.Day12
 
 		private static void Puzzle1()
 		{
+			var input = File.ReadAllText("Y2015/Day12/input.txt");
 
-			//Console.WriteLine($"Day 12 Puzzle 1: {result}");
-			//Debug.Assert(result == );
+			var obj = JsonConvert.DeserializeObject<JToken>(input);
+			var sum = CountValues(obj);
+			Console.WriteLine($"Day 12 Puzzle 1: {sum}");
+			Debug.Assert(sum == 156366);
+
+			int CountValues(JToken o)
+			{
+				return o.Type == JTokenType.Integer
+					? o.Value<int>()
+					: o.Children().Sum(c => CountValues(c));
+			}
 		}
 
 		private static void Puzzle2()
 		{
+			var input = File.ReadAllText("Y2015/Day12/input.txt");
 
-			//Console.WriteLine($"Day 12 Puzzle 2: {result}");
-			//Debug.Assert(result == );
+			var obj = JsonConvert.DeserializeObject<JToken>(input);
+			var sum = CountNonRedValues(obj);
+			Console.WriteLine($"Day 12 Puzzle 2: {sum}");
+			Debug.Assert(sum == 96852);
+
+			int CountNonRedValues(JToken o)
+			{
+				return o.Type == JTokenType.Integer
+					? o.Value<int>()
+					: o.Children().Where(Include).Sum(c => CountNonRedValues(c));
+			}
+
+			bool Include(JToken jt)
+			{
+				if (jt.Type != JTokenType.Object)
+				{
+					return true;
+				}
+				foreach (var c in jt.Children<JProperty>().Select(p => p.Value))
+				{
+					if (c.Type == JTokenType.String && c.Value<string>() == "red")
+						return false;
+				}
+				return true;
+			}
 		}
 	}
 }
