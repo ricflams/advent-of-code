fcc28ba29443ad08ce2d64f08d0c4db7a918bf68
Richard Flamsholt <richard@flamsholt.dk>
2020-01-31 00:45:27 +0100

    Day 16: Aunt Sue

--- AdventOfCode/Y2015/Day16/Puzzle16.cs
+++ AdventOfCode/Y2015/Day16/Puzzle16.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2015.Day16
 {
@@ -12,22 +11,75 @@ namespace AdventOfCode.Y2015.Day16
 	{
 		public static void Run()
 		{
-			Puzzle1();
-			Puzzle2();
+			Puzzle1And2();
 		}
 
-		private static void Puzzle1()
+		private static void Puzzle1And2()
 		{
+			var input = File.ReadAllLines("Y2015/Day16/input.txt");
+			var message = new string[]
+			{
+				"children: 3",
+				"cats: 7",
+				"samoyeds: 2",
+				"pomeranians: 3",
+				"akitas: 0",
+				"vizslas: 0",
+				"goldfish: 5",
+				"trees: 3",
+				"cars: 2",
+				"perfumes: 1"
+			};
 
-			//Console.WriteLine($"Day 16 Puzzle 1: {result}");
-			//Debug.Assert(result == );
+			var posessions = input.Select(x => new Posessions(x)).ToList();
+			var measured = message
+				.Select(x => x.Split(':'))
+				.ToDictionary(x => x[0], x => int.Parse(x[1]));
+
+			var sue = posessions.First(AllMustMatch).Number;
+			Console.WriteLine($"Day 16 Puzzle 1: {sue}");
+			Debug.Assert(sue == 40);
+
+			var realSue = posessions.First(MatchOutdatedRetroencabulator).Number;
+			Console.WriteLine($"Day 16 Puzzle 2: {realSue}");
+			Debug.Assert(realSue == 241);
+
+			bool AllMustMatch(Posessions p) => p.All(i => measured.ContainsKey(i.Key) && i.Value == measured[i.Key]);
+
+			bool MatchOutdatedRetroencabulator(Posessions p) => p.All(i =>
+			{
+				var name = i.Key;
+				var value = i.Value;
+				// cats and trees readings indicates that there are greater than that many
+				// pomeranians and goldfish readings indicate that there are fewer than that many
+				if (name == "cats" || name == "trees")
+				{
+					return measured.ContainsKey(i.Key) && i.Value > measured[i.Key];
+				}
+				if (name == "pomeranians" || name == "goldfish")
+				{
+					return !measured.ContainsKey(i.Key) || i.Value < measured[i.Key];
+				}
+				return measured.ContainsKey(i.Key) && i.Value == measured[i.Key];
+			});
 		}
 
-		private static void Puzzle2()
+		private class Posessions : Dictionary<string, int>
 		{
-
-			//Console.WriteLine($"Day 16 Puzzle 2: {result}");
-			//Debug.Assert(result == );
+			public Posessions(string line)
+			{
+				// Example:
+				// Sue 16: vizslas: 6, cats: 6, pomeranians: 10
+				var val = SimpleRegex.Match(line, "Sue %d:(.*)");
+				Number = int.Parse(val[0]);
+				var posessions = val[1];
+				foreach (var p in posessions.Split(','))
+				{
+					var item = SimpleRegex.Match(p.Trim(), "%s: %d");
+					this[item[0]] = int.Parse(item[1]);
+				}
+			}
+			public int Number { get; private set; }
 		}
 	}
 }
