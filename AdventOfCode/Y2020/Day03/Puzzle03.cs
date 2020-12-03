73fdc1c64a1e2d77e4d55bd7b260d1f75e6f884b
Richard Flamsholt <richard@flamsholt.dk>
2020-12-03 23:38:38 +0100

    2020 Day 3: Toboggan Trajectory

--- AdventOfCode/Y2020/Day03/Puzzle03.cs
+++ AdventOfCode/Y2020/Day03/Puzzle03.cs
@@ -1,10 +1,7 @@
-using AdventOfCode.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2020.Day03
 {
@@ -18,18 +15,43 @@ namespace AdventOfCode.Y2020.Day03
 
 		private static void Puzzle1()
 		{
-			var input = File.ReadLines("Y2020/Day03/input.txt");
+			var input = File.ReadAllLines("Y2020/Day03/input.txt");
 
-			//Console.WriteLine($"Day 03 Puzzle 1: {result}");
-			//Debug.Assert(result == );
+			var result = input
+				.Select((line, i) => line[i * 3 % line.Length] == '#' ? 1 : 0)
+				.Sum();
+
+			Console.WriteLine($"Day 03 Puzzle 1: {result}");
+			Debug.Assert(result == 237);
 		}
 
 		private static void Puzzle2()
 		{
-			var input = File.ReadLines("Y2020/Day03/input.txt");
+			var input = File.ReadAllLines("Y2020/Day03/input.txt");
 
-			//Console.WriteLine($"Day 03 Puzzle 2: {result}");
-			//Debug.Assert(result == );
+			var result =
+				CountTrees(input, 1, 1) *
+				CountTrees(input, 3, 1) *
+				CountTrees(input, 5, 1) *
+				CountTrees(input, 7, 1) *
+				CountTrees(input, 1, 2);
+
+			Console.WriteLine($"Day 03 Puzzle 2: {result}");
+			Debug.Assert(result == 2106818610);
+		}
+
+		private static long CountTrees(string[] input, int right, int down)
+		{
+			var trees = 0;
+			for (var (xpos, ypos) = (0, 0); ypos < input.Length; ypos += down, xpos += right)
+			{
+				var line = input[ypos];
+				if (line[xpos % line.Length] == '#')
+				{
+					trees++;
+				}
+			}
+			return trees;
 		}
 	}
 }
