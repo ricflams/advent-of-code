807fb45697734b9dfa6010ec772483c15cbcb1d6
Richard Flamsholt <richard@flamsholt.dk>
2020-12-14 03:15:58 +0100

    2020 Day 13: Shuttle Search

--- AdventOfCode/Y2020/Day13/Puzzle13.cs
+++ AdventOfCode/Y2020/Day13/Puzzle13.cs
@@ -1,8 +1,7 @@
-using AdventOfCode.Helpers;
+﻿using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace AdventOfCode.Y2020.Day13
 {
@@ -25,135 +24,59 @@ namespace AdventOfCode.Y2020.Day13
 
 		protected override long Puzzle1(string[] input)
 		{
-			var t0 = int.Parse(input[0]);
-			var ids = input[1].Split(",").Select(x => x == "x" ? (int?)null : int.Parse(x));
-
-			var ids2 = ids
-				.Where(x => x.HasValue)
-				.Select(x => x.Value)
-				.Select(id => (id, id - t0 % id))
-				.OrderBy(x => x.Item2)
-				.ToArray();
-			var id1 = ids2
+			var departure = int.Parse(input[0]);
+			var id = input[1]
+				.Replace(",x", "")
+				.Split(",")
+				.Select(int.Parse)
+				.Select(id => new
+				{
+					Id = id,
+					Time = id - departure % id
+				})
+				.OrderBy(x => x.Time)
 				.First();
-			var result1 = id1.id * id1.Item2;
-
-			return result1;
+			return id.Id * id.Time;
 		}
 
 		protected override long Puzzle2(string[] input)
 		{
-			var xxx = input[1].Split(",").ToArray();
-			var cyclus =
-				xxx.Select((x, i) =>
-					{
-						if (x == "x")
-							return null;
-						return new
+			var cyclus = input[1]
+				.Split(",")
+				.Select((x, i) =>
+					x == "x"
+						? null
+						: new
 						{
 							Id = int.Parse(x),
-							Diff = i
-						};
-					})
+							Position = i
+						}
+				)
 				.Where(x => x != null)
-				//.OrderBy(x => x.Diff)
 				.ToArray();
 
-			//var ids = xxx.Skip(1).Select((x, i) => new
-			//	{
-			//		Id1 = xxx[i-1],
-			//		Id2 = x,
+			static long FindNextTimestamp(long a, long b, long n, int position)
+			{
+				// Find the factor x that satisfies this next timestamp (all mod n):
+				//       ax + b ≡ n - position
+				// <=>   ax ≡ n - position - b
+				// <=>   ax ≡ target, where target = n - offset - b
+				// <=>   a⁻¹ax = a⁻¹ target
+				// <=>   x ≡ a⁻¹ target
+				var target = ((n - position - b) % n + n) % n; // make sure target is positive, mod n
+				var ainv = (long)new BigInteger(a).ModInverse(n);
+				var x = (ainv * target) % n;
+				return a * x + b;
+			}
 
-			//	})
-			//	.ToArray();
-
-			//.Select(x => x == "x" ? (int?)null : int.Parse(x))
-
-
-			Console.WriteLine();
-			//Console.WriteLine(input[1]);
-			//for (var i = 1; i < cyclus.Length; i++)
-			//{
-			//	var t = FindX(cyclus[0].Id, cyclus[i].Id, cyclus[i].Diff);
-			//	var t2 = string.Join(" ", FindManyX(cyclus[0].Id, cyclus[i].Id, cyclus[i].Diff).Take(18));
-			//	Console.WriteLine($"For {cyclus[i].Id} with diff={cyclus[i].Diff}: {cyclus[0].Id} at {t}: diff={t % cyclus[i].Id} +lcm={MathHelper.LeastCommonMultiple(cyclus[0].Id, cyclus[i].Id)}  t1..4={t2}");
-			//}
-			//Console.WriteLine(MathHelper.LeastCommonMultiple(cyclus.Select(x => (long)x.Id).ToArray()));
-			//var z = FindX(221, 323, 187 - 102);
-
-			long offset = 0;
+			long timestamp = 0;
 			long cycle = cyclus[0].Id;
 			for (var i = 1; i < cyclus.Length; i++)
 			{
-				//Console.WriteLine($"Checking {i} of {cyclus.Length - 1}: offset={offset} cycle={cycle}");
-				var t0 = FindX(cycle, offset, cyclus[i].Id, cyclus[i].Diff);
-				var lcm = MathHelper.LeastCommonMultiple(cycle, cyclus[i].Id);
-				// now looking for to + n*lcm
-				offset = t0;
-				cycle = lcm;
-
-				//var t2 = string.Join(" ", FindManyX(cyclus[0].Id, cyclus[i].Id, cyclus[i].Diff).Take(18));
-				//Console.WriteLine($"For {cyclus[i].Id} with diff={cyclus[i].Diff}: {cyclus[0].Id} at {t}: diff={t % cyclus[i].Id} +lcm={MathHelper.LeastCommonMultiple(cyclus[0].Id, cyclus[i].Id)}  t1..4={t2}");
+				timestamp = FindNextTimestamp(cycle, timestamp, cyclus[i].Id, cyclus[i].Position);
+				cycle = MathHelper.LeastCommonMultiple(cycle, cyclus[i].Id);
 			}
-			//Console.WriteLine($"offset={offset} cycle={cycle}");
-			//var z = FindX(221, 323, 187 - 102);
-
-
-
-
-			return offset;
-		}
-
-		private long FindX(long a, long offset, long b, int diff)
-		{
-			var target = b - diff;
-			while (target < 0)
-				target += b;
-			Console.WriteLine($"   FindX: a={a} offset={offset} b={b} diff={diff} target={target}");
-			var loops = 0;
-			for (var x = offset; ; x += a)
-			{
-				loops++;
-				if (x % b == target)
-				{
-					Console.WriteLine($"      loops={loops}");
-					return x;
-				}
-			}
-		}
-
-		private IEnumerable<int> FindManyX(int a, int b, int diff)
-		{
-			for (var x = 1; ; x++)
-			{
-				if (b - x * a % b == diff)
-					yield return x * a;
-			}
-		}
-	}
-
-
-
-
-	internal class Puzzle2 : PuzzleRunner<int,int>
-	{
-		public static Puzzle2 Instance = new Puzzle2();
-		protected override int Year => 2020;
-		protected override int Day => 13;
-
-		public void Run()
-		{
-			//RunPuzzles("test1.txt", );
-			//RunPuzzles("test2.txt", );
-			//RunPuzzles("test3.txt", );
-			RunPuzzles("input.txt", 0, 0);
-		}
-
-		protected override (int, int) Puzzle1And2(string[] input)
-		{
-
-
-			return (0, 0);
+			return timestamp;
 		}
 	}
 }
