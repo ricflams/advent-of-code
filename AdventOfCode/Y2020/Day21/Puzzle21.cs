3bc8edd2da98ab0ee984f18f2055dcc0368fdd38
Richard Flamsholt <richard@flamsholt.dk>
2020-12-21 23:16:30 +0100

    2020 Day 21: Allergen Assessment

--- AdventOfCode/Y2020/Day21/Puzzle21.cs
+++ AdventOfCode/Y2020/Day21/Puzzle21.cs
@@ -2,14 +2,11 @@ using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2020.Day21
 {
-	internal class Puzzle : SoloParts<int>
+	internal class Puzzle : ComboParts<string>
 	{
 		public static Puzzle Instance = new Puzzle();
 		protected override int Year => 2020;
@@ -17,57 +14,79 @@ namespace AdventOfCode.Y2020.Day21
 
 		public void Run()
 		{
-			RunFor("test1", null, null);
-			//RunFor("test2", null, null);
-			//RunFor("input", null, null);
+			RunFor("test1", "5", "mxmxvkd,sqjhc,fvjkl");
+			RunFor("input", "2423", "jzzjz,bxkrd,pllzxb,gjddl,xfqnss,dzkb,vspv,dxvsp");
 		}
 
-		protected override int Part1(string[] input)
+		internal class Food
 		{
-
-
-
-
-
-			return 0;
+			public HashSet<string> Ingredients { get; set; }
+			public HashSet<string> Allergies { get; set; }
 		}
 
-		protected override int Part2(string[] input)
+		protected override (string,string) Part1And2(string[] input)
 		{
+			// mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+			// trh fvjkl sbzzf mxmxvkd (contains dairy)
+			// sqjhc fvjkl (contains soy)
+			// sqjhc mxmxvkd sbzzf (contains fish)
+			var food = input
+				.Select(line =>
+				{
+					line.RegexCapture(@"%* \(contains %*\)")
+						.Get(out string ingredients)
+						.Get(out string allergies);
+					return new Food
+					{
+						Ingredients = new HashSet<string>(ingredients.Split(" ", StringSplitOptions.RemoveEmptyEntries)),
+						Allergies = new HashSet<string>(allergies.Split(", ", StringSplitOptions.RemoveEmptyEntries))
+					};
+				})
+				.ToArray();
 
+			// All ingredients anda allergies
+			var allIngredients = food.Aggregate(new HashSet<string>(), (set, f) => { set.UnionWith(f.Ingredients); return set; });
+			var allAllergies = food.Aggregate(new HashSet<string>(), (set, f) => { set.UnionWith(f.Allergies); return set; });
 
+			var allergyCandidates = allAllergies
+				.Select(a =>
+				{
+					var foodopt = food
+						.Where(f => f.Allergies.Contains(a))
+						.ToArray();
+					var ingopt = new HashSet<string>(foodopt.First().Ingredients);
+					foreach (var f in foodopt.Skip(1))
+					{
+						ingopt.IntersectWith(f.Ingredients);
+					}
+					return (a, ingopt);
+				})
+				.ToDictionary(x => x.a, x => x.ingopt);
 
+			var allergyIngredients = new Dictionary<string, string>();
+			while (allergyCandidates.Any())
+			{
+				var a = allergyCandidates.OrderBy(x => x.Value.Count()).First();
+				var ingredient = a.Value.First();
+				allergyIngredients[a.Key] = ingredient;
+				allergyCandidates.Remove(a.Key);
+				foreach (var ac in allergyCandidates)
+				{
+					ac.Value.Remove(ingredient);
+				}
+			}
 
+			var ingredientsWithoutAllergy = new HashSet<string>(allIngredients.Except(allergyIngredients.Values));
+			var result1 = food
+				.Sum(f => f.Ingredients.Count(i => ingredientsWithoutAllergy.Contains(i)))
+				.ToString();
 
-			return 0;
+			var ingredientsWithAllergy = allergyIngredients
+				.OrderBy(x => x.Key)
+				.Select(x => x.Value);
+			var result2 = string.Join(",", ingredientsWithAllergy);
+
+			return (result1, result2);
 		}
 	}
-
-
-
-
-	//  internal class Puzzle : ComboPart<int>
-	//  {
-	//  	public static Puzzle Instance = new Puzzle();
-	//  	protected override int Year => 2020;
-	//  	protected override int Day => 21;
-	//  
-	//  	public void Run()
-	//  	{
-	//  		RunFor("test1", null, null);
-	//  		//RunFor("test2", null, null);
-	//  		//RunFor("input", null, null);
-	//  	}
-	//  
-	//  	protected override (int, int) Part1And2(string[] input)
-	//  	{
-	//  
-	//  
-	//  
-	//  
-	//  
-	//  		return (0, 0);
-	//  	}
-	//  }
-
 }
