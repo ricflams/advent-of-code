6d8269f5c7221d7164d389a589e98e26334d9571
Richard Flamsholt <richard@flamsholt.dk>
2020-12-22 23:12:31 +0100

    2020 Day 22: Crab Combat (raw)
    - Including an attempted super-optimized deck

--- AdventOfCode/Helpers/Hashing.cs
+++ AdventOfCode/Helpers/Hashing.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -22,6 +23,36 @@ namespace AdventOfCode.Helpers
 		{
 			var longhash = LongHash(data);
 			var hash = (uint)(longhash >> 32 ^ longhash & 0xffffffff);
+			return hash;
+		}
+
+		public static ulong KnuthHash(string s) => Hash(Encoding.Default.GetBytes(s));
+		public static ulong KnuthHash(int[] ia) => Hash(ia.SelectMany(BitConverter.GetBytes).ToArray());
+		public static ulong KnuthHash(IEnumerable<byte> bytes)
+		{
+			var hashedValue = 3074457345618258791UL;
+			foreach (var b in bytes)
+			{
+				hashedValue += b;
+				hashedValue *= 3074457345618258799UL;
+			}
+			return hashedValue;
+		}
+
+		public static uint JenkinsHash(string s) => Hash(Encoding.Default.GetBytes(s));
+		public static uint JenkinsHash(int[] ia) => Hash(ia.SelectMany(BitConverter.GetBytes).ToArray());
+		public static uint JenkinsHash(IEnumerable<byte> bytes)
+		{
+			var hash = 0U;
+			foreach (var b in bytes)
+			{
+				hash += b;
+				hash += hash << 10;
+				hash ^= hash >> 6;
+			}
+			hash += hash << 3;
+			hash ^= hash >> 11;
+			hash += hash << 15;
 			return hash;
 		}
 	}
