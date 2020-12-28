de4db88f0d9ccec5503d8c0db3b92b40a18b1113
ricflams <richard@flamsholt.dk>
2020-12-28 17:28:59 +0100

    2020 Day 16: Ticket Translation (clean)

--- AdventOfCode/Y2020/Day16/Puzzle16.cs
+++ AdventOfCode/Y2020/Day16/Puzzle16.cs
@@ -1,17 +1,11 @@
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 
 
 namespace AdventOfCode.Y2020.Day16
 {
-	// TODO: Cleanup
-	internal class Puzzle : ComboParts<long>
+	internal class Puzzle : SoloParts<long>
 	{
 		public static Puzzle Instance = new Puzzle();
 		protected override int Year => 2020;
@@ -19,11 +13,9 @@ namespace AdventOfCode.Y2020.Day16
 
 		public void Run()
 		{
-			//RunPart1For("test1", 71);
-			//RunPart2For("test2", 0);
+			RunPart1For("test1", 71);
 			RunFor("input", 20048, 4810284647569);
 		}
-
 
 		internal class TicketField
 		{
@@ -35,10 +27,9 @@ namespace AdventOfCode.Y2020.Day16
 			public bool IsValid(int x) => R1min <= x && x <= R1max || R2min <= x && x <= R2max;
 		}
 
-		protected override (long, long) Part1And2(string[] input)
+		internal static (TicketField[], int[], int[][])  ParseInput(string[] lines)
 		{
-			var parts = input.GroupByEmptyLine().ToArray();
-
+			var parts = lines.GroupByEmptyLine().ToArray();
 			var fields = parts[0]
 				.Select(x =>
 				{
@@ -58,66 +49,44 @@ namespace AdventOfCode.Y2020.Day16
 					};
 				})
 				.ToArray();
-
-			var yourticket = parts[1][1].Split(",").Select(int.Parse).ToArray();
-
-			var allNearby = parts[2]
+			var yourTicket = parts[1][1].Split(",").Select(int.Parse).ToArray();
+			var otherTickets = parts[2]
 				.Skip(1)
 				.Select(x => x.Split(",").Select(int.Parse).ToArray())
 				.ToArray();
+			return (fields, yourTicket, otherTickets);
+		}
 
-			var errorCount = allNearby
+		protected override long Part1(string[] input)
+		{
+			var (fields, _, otherTickets) = ParseInput(input);
+			var invalids = otherTickets
 				.SelectMany(t => t.Where(v => !fields.Any(f => f.IsValid(v))))
 				.Sum();
+			return invalids;
+		}
 
+		protected override long Part2(string[] input)
+		{
+			var (fields, yourTicket, otherTickets) = ParseInput(input);
 
-			var all = allNearby
+			// Determine the required fields
+			var allTickets = otherTickets
 				.Where(t => t.All(v => fields.Any(f => f.IsValid(v))))
-				.Prepend(yourticket)
+				.Prepend(yourTicket)
 				.ToArray();
-
-			// Turn the matrix
-			var fieldMustHaveTheseValues = new int[yourticket.Length][];
+			var fieldMustHaveTheseValues = new int[yourTicket.Length][];
 			for (var i = 0; i < fieldMustHaveTheseValues.Length; i++)
 			{
-				fieldMustHaveTheseValues[i] = new int[all.Length];
-				for (var j = 0; j < all.Length; j++)
+				fieldMustHaveTheseValues[i] = new int[allTickets.Length];
+				for (var j = 0; j < allTickets.Length; j++)
 				{
-					fieldMustHaveTheseValues[i][j] = all[j][i];
+					fieldMustHaveTheseValues[i][j] = allTickets[j][i];
 				}
 			}
 
-
-			foreach (var f in fields)
-			{
-				var matches = 0;
-				for (var i = 0; i < fieldMustHaveTheseValues.Length; i++)
-				{
-					var values = fieldMustHaveTheseValues[i];
-					var allMatch = values.All(v => f.IsValid(v));
-					if (allMatch)
-						matches++;
-				}
-			}
-
-
-			var fields2 = fields
-				.Select(f =>
-				{
-					var matches = 0;
-					for (var i = 0; i < fieldMustHaveTheseValues.Length; i++)
-					{
-						var values = fieldMustHaveTheseValues[i];
-						var allMatch = values.All(v => f.IsValid(v));
-						//Console.WriteLine($"   index {i:D2}: {allMatch}:");
-						if (allMatch)
-							matches++;
-					}
-					return new { f, matches };
-				})
-				.OrderBy(x => x.matches)
-				.ToArray();
-
+			// Find all candidates for each field, ordered by fewest matches first so the
+			// fields with the fewest matches can be examined first
 			var fieldCandidates = fields
 				.Select(f => new 
 				{
@@ -128,6 +97,8 @@ namespace AdventOfCode.Y2020.Day16
 				.OrderBy(x => x.matches.Count)
 				.ToArray();
 
+			// Fields are ordered by least matches, and they just happens to be 1, 2, ... N
+			// so we can pick them out, one by one removing selected candidates along the way.
 			var order = new TicketField[fieldCandidates.Length];
 			foreach (var fc in fieldCandidates)
 			{
@@ -139,47 +110,16 @@ namespace AdventOfCode.Y2020.Day16
 				}
 			}
 
-
-			var sum = 1L;
+			// Calc the product of all departure-fields on your ticket
+			var product = 1L;
 			for (var i = 0; i < order.Length; i++)
 			{
 				if (order[i].Name.StartsWith("departure"))
 				{
-					var yourvalue = (long)yourticket[i];
-					sum *= yourvalue;
+					product *= yourTicket[i];
 				}
 			}
-
-			return (errorCount, sum);
+			return product;
 		}
-
 	}
-
-
-
-
-	//  internal class Puzzle : ComboPart<int>
-	//  {
-	//  	public static Puzzle Instance = new Puzzle();
-	//  	protected override int Year => 2020;
-	//  	protected override int Day => 16;
-	//  
-	//  	public void Run()
-	//  	{
-	//  		RunFor("test1", null, null);
-	//  		//RunFor("test2", null, null);
-	//  		//RunFor("input", null, null);
-	//  	}
-	//  
-	//  	protected override (int, int) Part1And2(string[] input)
-	//  	{
-	//  
-	//  
-	//  
-	//  
-	//  
-	//  		return (0, 0);
-	//  	}
-	//  }
-
 }
