580364cae9fd133b1da282a742d3ed796c3073a1
Richard Flamsholt <richard@flamsholt.dk>
2021-01-04 00:40:46 +0100

    2015 Day 4: optimized (parallelized)

--- AdventOfCode/Y2015/Day04/Puzzle04.cs
+++ AdventOfCode/Y2015/Day04/Puzzle04.cs
@@ -1,13 +1,10 @@
+using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using System;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace AdventOfCode.Y2015.Day04
 {
-
-	internal class Puzzle : ComboParts<int>
+	internal class Puzzle : SoloParts<int>
 	{
 		public static Puzzle Instance = new Puzzle();
 		public override string Name => "The Ideal Stocking Stuffer";
@@ -19,31 +16,20 @@ namespace AdventOfCode.Y2015.Day04
 			RunFor("input", 254575, 1038736);
 		}
 
-		protected override (int, int) Part1And2(string[] input)
+		protected override int Part1(string[] input)
 		{
 			var secret = input[0];
-			var answer1 = FirstZerosAt(secret, 0, hash => hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0);
-			// Puzzle 2 can resume searching where Puzzle 1 ended
-			var answer2 = FirstZerosAt(secret, answer1, hash => hash[0] == 0 && hash[1] == 0 && hash[2] == 0);
-			return (answer1, answer2);
+			var finder = new Md5HashFinder(Md5HashFinder.Condition5x0);
+			var iterations = finder.FindMatches(secret, 0).First().Iterations;
+			return iterations;
 		}
 
-		private static int FirstZerosAt(string input, int start, Func<byte[], bool> condition)
+		protected override int Part2(string[] input)
 		{
-			var md5 = MD5.Create();
-			var secret = input.ToCharArray().Select(x => (byte)x).ToArray();
-			var buffer = new byte[100]; // more than big enough
-			Array.Copy(secret, 0, buffer, 0, secret.Length);
-			for (var i = start; ; i++)
-			{
-				var guess = Encoding.ASCII.GetBytes(i.ToString());
-				Array.Copy(guess, 0, buffer, secret.Length, guess.Length);
-				var hash = md5.ComputeHash(buffer, 0, secret.Length + guess.Length);
-				if (condition(hash))
-				{
-					return i;
-				}
-			}
+			var secret = input[0];
+			var finder = new Md5HashFinder(Md5HashFinder.Condition6x0);
+			var iterations = finder.FindMatches(secret, 0).First().Iterations;
+			return iterations;
 		}
 	}
 }
