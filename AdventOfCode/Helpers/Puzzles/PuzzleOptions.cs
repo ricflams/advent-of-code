e4d72ad0f781e3732e499f208237c94b526345e4
Richard Flamsholt <richard@flamsholt.dk>
2021-01-12 23:53:45 +0100

    Fix day-filter bug

--- AdventOfCode/Helpers/Puzzles/PuzzleOptions.cs
+++ AdventOfCode/Helpers/Puzzles/PuzzleOptions.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode.Helpers.Puzzles
 	public static class PuzzleOptions
 	{
 		private static int? _year;
-		private static HashSet<int> _days;
+		private static HashSet<int> _days = new HashSet<int>();
 		static public bool OnlyRunForInputs { get; set; } = false;
 		static public bool ShowTimings { get; set; } = false;
 		static public int TimingLoops { get; set; } = 0;
