1591bf6a534f84cb39b0f85b240f162a6d96b791
Richard Flamsholt <richard@flamsholt.dk>
2021-02-15 21:09:36 +0100

    2017 Day 2: Corruption Checksum

---
+++ AdventOfCode/Y2017/Day02/Puzzle02.cs
@@ -0,0 +1,57 @@
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Y2017.Day02
+{
+	internal class Puzzle : Puzzle<int, int>
+	{
+		public static Puzzle Instance = new Puzzle();
+		public override string Name => "Corruption Checksum";
+		public override int Year => 2017;
+		public override int Day => 2;
+
+		public void Run()
+		{
+			RunPart1For("test1", 18);
+			RunPart2For("test2", 9);
+			RunFor("input", 34925, 221);
+		}
+
+		protected override int Part1(string[] input)
+		{
+			var sheet = ReadSheet(input);
+			var checksum = sheet.Sum(s => s.Max() - s.Min());
+			return checksum;
+		}
+
+		protected override int Part2(string[] input)
+		{
+			var sheet = ReadSheet(input);
+			var checksum = sheet.Sum(s =>
+			{
+				foreach (var i in s)
+				{
+					foreach (var j in s)
+					{
+						// Find pairs where i/j is a whole number, and they're not the same number
+						if (i % j == 0 && i != j)
+							return i / j;
+					}
+				}
+				throw new Exception($"No divider-pairs found in {s}");
+			});
+			return checksum;
+		}
+
+		private static int[][] ReadSheet(string[] input)
+		{
+			var sep = new char[] {' ', '\t'};
+			var sheet = input
+				.Select(line => line.Split(sep, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
+				.ToArray();
+			return sheet;
+		}
+	}
+}
