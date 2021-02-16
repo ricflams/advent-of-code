48e5f212b47a391e0ce1bb5b317799793874cd15
Richard Flamsholt <richard@flamsholt.dk>
2021-02-16 01:48:43 +0100

    2017 Day 5: A Maze of Twisty Trampolines, All Alike

---
+++ AdventOfCode/Y2017/Day05/Puzzle05.cs
@@ -0,0 +1,49 @@
+using AdventOfCode.Helpers.Puzzles;
+using System.Linq;
+
+namespace AdventOfCode.Y2017.Day05
+{
+	internal class Puzzle : Puzzle<int, int>
+	{
+		public static Puzzle Instance = new Puzzle();
+		public override string Name => "A Maze of Twisty Trampolines, All Alike";
+		public override int Year => 2017;
+		public override int Day => 5;
+
+		public void Run()
+		{
+			RunFor("test1", 5, 10);
+			RunFor("input", 391540, 30513679);
+		}
+
+		protected override int Part1(string[] input)
+		{
+			var jumps = input.Select(int.Parse).ToArray();
+
+			var steps = 0;
+			for (var i = 0; i >= 0 && i < jumps.Length; )
+			{
+				i += jumps[i]++;
+				steps++;
+			}
+
+			return steps;
+		}
+
+		protected override int Part2(string[] input)
+		{
+			var jumps = input.Select(int.Parse).ToArray();
+
+			var steps = 0;
+			for (var i = 0; i >= 0 && i < jumps.Length; )
+			{
+				var jmp = jumps[i];
+				jumps[i] += jmp >= 3 ? -1 : 1;
+				i += jmp;
+				steps++;
+			}
+
+			return steps;
+		}
+	}
+}
