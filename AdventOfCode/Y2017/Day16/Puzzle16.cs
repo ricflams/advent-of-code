27ff470485a0624199144338bc1e8954c1bb158c
ricflams <richard@flamsholt.dk>
2021-02-28 02:52:14 +0100

    2017 Day 16: Permutation Promenade

--- AdventOfCode/Y2017/Day16/Puzzle16.cs
+++ AdventOfCode/Y2017/Day16/Puzzle16.cs
@@ -2,10 +2,7 @@ using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2017.Day16
 {
@@ -18,130 +15,106 @@ namespace AdventOfCode.Y2017.Day16
 
 		public void Run()
 		{
-			//RunFor("test1", "", 0);
-			//RunFor("test2", 0, 0);
-			RunFor("input", "namdgkbhifpceloj", "");
+			RunFor("input", "namdgkbhifpceloj", "ibmchklnofjpdeag");
 		}
 
 		protected override string Part1(string[] input)
 		{
-			var s = "abcdefghijklmnop".ToCharArray();
-			return new string(Dance(s, input[0]));
-
-			// foreach (var move in input[0].Split(','))
-			// {
-			// 	switch (move[0])
-			// 	{
-			// 		case 's':
-			// 			// Spin, written sX, makes X programs move from the end to the front
-			// 			var shiftby = int.Parse(move[1..]);
-			// 			s = s[^shiftby..].Concat(s[..^shiftby]).ToArray();
-			// 			break;
-
-			// 		case 'x':
-			// 			// Exchange, written xA/B, makes the programs at positions A and B swap places.
-			// 			var (xa, xb) = move.RxMatch("x%d/%d").Get<int, int>();
-			// 			(s[xa], s[xb]) = (s[xb], s[xa]);
-			// 			break;
-
-			// 		case 'p':
-			// 			// Partner, written pA/B, makes the programs named A and B swap places
-			// 			var (pa, pb) = (move[1], move[3]);
-			// 			for (var i = 0; i < s.Length; i++)
-			// 			{
-			// 				// Swap every letter seen
-			// 				if (s[i] == pa)
-			// 					s[i] = pb;
-			// 				else if (s[i] == pb)
-			// 					s[i] = pa;
-			// 			}
-			// 			break;
-					
-			// 		default:
-			// 			throw new Exception($"Unknown move {move}");
-			// 	}
-			// }
-
-			// return new string(s);
-		}
-
-		private char[] Dance(char[] s, string moves)
-		{
-			foreach (var move in moves.Split(','))
-			{
-				switch (move[0])
-				{
-					case 's':
-						// Spin, written sX, makes X programs move from the end to the front
-						var shiftby = int.Parse(move[1..]);
-						s = s[^shiftby..].Concat(s[..^shiftby]).ToArray();
-						break;
-
-					case 'x':
-						// Exchange, written xA/B, makes the programs at positions A and B swap places.
-						var (xa, xb) = move.RxMatch("x%d/%d").Get<int, int>();
-						(s[xa], s[xb]) = (s[xb], s[xa]);
-						break;
-
-					case 'p':
-						// Partner, written pA/B, makes the programs named A and B swap places
-						var (pa, pb) = (move[1], move[3]);
-						for (var i = 0; i < s.Length; i++)
-						{
-							// Swap every letter seen
-							if (s[i] == pa)
-								s[i] = pb;
-							else if (s[i] == pb)
-								s[i] = pa;
-						}
-						break;
-					
-					default:
-						throw new Exception($"Unknown move {move}");
-				}
-			}
-			return s;
+			var moves = ReadMoves(input[0]);
+			var s = "abcdefghijklmnop";
+			return Dance(s, moves);
 		}
 
 		protected override string Part2(string[] input)
 		{
-			var s0 = "abcdefghijklmnop".ToCharArray();
+			var moves = ReadMoves(input[0]);
+			var s0 = "abcdefghijklmnop";
 
-
-			// for (var i = 0; i < 20; i++)
-			// {
-			// 	Console.WriteLine($"{i}: {new string(s0)}");
-			// 	s0 = Dance(s0, input[0]);
-			// }
-
-
-			var s1 = new string(Dance(s0, input[0]));
-
-			var offsets = s0
-				.Select((c, i) => (s1.IndexOf(c) - i + s0.Length) %  s0.Length)
-				.ToArray();
-
-
-			var loops = 0;
-			var pos0 = 1;
-			var pos = pos0;
-			do
+			// Dance until we've seen a complete cycle. It won't be long,
+			// just 60 dances for this specific puzzle input.
+			var dances = new List<string>();
+			dances.Add(s0);
+			while (true)
 			{
-				loops++;
-				pos = (pos + offsets[pos]) % s0.Length;
+				var s = Dance(dances.Last(), moves);
+				if (s == s0)
+					break;
+				dances.Add(s);
 			}
-			while (pos != pos0);
 
+			// Find last dance's index
 			var N = 1_000_000_000;
-			var s2 = new char[s0.Length];
-			for (var i = 0; i < s0.Length; i++)
-			{
-				var offset = (offsets[i] * N) % s2.Length;
-				Console.WriteLine($"offset {i}: {offset}");
-				s2[offset] = s0[i];
-			}
+			var lastdance = dances[N % dances.Count()];
+			return lastdance;
+		}
 
-			return new string(s2);
+		private static string Dance(string s0, IMove[] moves)
+		{
+			var s = s0.ToCharArray();
+			foreach (var move in moves)
+			{
+				move.Step(s);
+			}
+			return new string(s);
+		}
+
+		private static IMove[] ReadMoves(string moves)
+		{
+			return moves.Split(',').Select(move =>
+			{
+				IMove m = move[0] switch
+				{
+					's' => new Spin(move),
+					'x' => new Exchange(move),
+					'p' => new Partner(move),
+					_ => throw new Exception($"Unknown move {move}")
+				};
+				return m;
+			} )
+			.ToArray();
+		}
+
+		internal interface IMove
+		{
+			void Step(char[] s);
+		}
+		internal class Spin : IMove
+		{
+			private readonly int _x;
+			public Spin(string move) => _x = int.Parse(move[1..]);
+			public void Step(char[] s)
+			{
+				// Spin, written sX, makes X programs move from the end to the front
+				var tmp = s[^_x..].Concat(s[..^_x]).ToArray();
+				Array.Copy(tmp, s, s.Length);
+			}
+		}
+		internal class Exchange : IMove
+		{
+			private readonly int _a, _b;
+			public Exchange(string move) => (_a, _b) = move.RxMatch("x%d/%d").Get<int, int>();
+			public void Step(char[] s)
+			{
+				// Exchange, written xA/B, makes the programs at positions A and B swap places.
+				(s[_a], s[_b]) = (s[_b], s[_a]);
+			}
+		}
+		internal class Partner : IMove
+		{
+			private readonly char _a, _b;
+			public Partner(string move) => (_a, _b) = (move[1], move[3]);
+			public void Step(char[] s)
+			{
+				// Partner, written pA/B, makes the programs named A and B swap places
+				for (var i = 0; i < s.Length; i++)
+				{
+					// Swap every letter seen
+					if (s[i] == _a)
+						s[i] = _b;
+					else if (s[i] == _b)
+						s[i] = _a;
+				}
+			}
 		}
 	}
 }
