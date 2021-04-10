d10da34c2e017c6b4da17ce815f1d78342d1b1e5
ricflams <richard@flamsholt.dk>
2021-04-10 23:46:53 +0200

    2018 Day 12: Subterranean Sustainability (done)

--- AdventOfCode/Y2018/Day12/Puzzle12.cs
+++ AdventOfCode/Y2018/Day12/Puzzle12.cs
@@ -1,194 +1,98 @@
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2018.Day12
 {
-	internal class Puzzle : Puzzle<int, ulong>
+	internal class Puzzle : Puzzle<int, long>
 	{
 		public static Puzzle Instance = new Puzzle();
-		public override string Name => "";
+		public override string Name => "Subterranean Sustainability";
 		public override int Year => 2018;
 		public override int Day => 12;
 
 		public void Run()
 		{
-		//	Run("test1").Part1(325).Part2(0);
-			//Run("test2").Part1(0).Part2(0); 3679 too low,  3797 too high
+			Run("test1").Part1(325);
 			Run("input").Part1(3738).Part2(3900000002467);
 		}
 
 		protected override int Part1(string[] input)
 		{
-			var state = input[0].RxMatch("initial state: %*").Get<string>();
-			var rules = input[2..]
-				.Select(line => line.Split(" => ").ToArray())
-				.ToDictionary(x => x[0], x => x[1][0]);
-
-//			var plants = 0;
-			var offset = 0;//-state.IndexOf('#');
-			for (var g = 0; g < 20; g++)
+			var tunnel = new Tunnel(input);
+			while (tunnel.Iterations < 20)
 			{
-		//		Console.WriteLine(state);
-
-				state = "...." + state;
-				offset += 4;
-				// if (state[0] == '#')
-				// {
-				// 	state = ".." + state;
-				// 	offset += 2;
-				// }
-				// else if (state[1] == '#')
-				// {
-				// 	state = "." + state;
-				// 	offset++;
-				// }
-
-				// if (state[state.Length-1] == '#')
-				// 	state = state + "..";
-				// else if (state[state.Length-2] == '#')
-				// 	state = ".." + state;
-				state += "....";
-				// Console.WriteLine(state);
-				// Console.WriteLine();
-
-				var newstate = state.ToCharArray();
-				for (var i = 0; i < state.Length - 4; i++)
-				{
-					var pat = state[i..(i+5)];
-					if (rules.TryGetValue(pat, out var ch))
-					{
-						newstate[i+2] = ch;
-					}
-					else
-					{
-						newstate[i+2] = '.';
-					}
-				}
-
-				state = new string(newstate);
+				tunnel.Grow();
 			}
-
-			var plants = 0;
-			for (var i = 0; i < state.Length; i++)
-			{
-				if (state[i] == '#')
-				{
-					var id = i - offset;
-					plants += id;
-				}
-			}
-
-			return plants;
+			return tunnel.SumOfPots;
 		}
 
-		protected override ulong Part2(string[] input)
+		protected override long Part2(string[] input)
 		{
-			// 3244 too low
-			// 500011047 too low
-			// 2340000006367 too low
-			// 1416000002455 not right
-			// 2400000000487 not right
-			// 3900000002389 not right
-			// 3900000002467
+			var tunnel = new Tunnel(input);
 
-			var state = input[0].RxMatch("initial state: %*").Get<string>();
-			var orgstate = state;
-			var rules = input[2..]
-				.Select(line => line.Split(" => ").ToArray())
-				.ToDictionary(x => x[0], x => x[1][0]);
-
-			var seen = new HashSet<string>();
-//			var plants = 0;
-			var offset = 0;//-state.IndexOf('#');
-
-			var N = 50000000000 % 99;
-
-			state = $"..........{state}...............................................................................................................................";
-			var dotlen0 = state.TakeWhile(c => c == '.').Count();
-
-			for (var g = 0; g <444; g++)
+			// Grow until we've reached a "crawler", ie a pattern that shifts one
+			// position to the right for every step.
+			var last = "";
+			while (tunnel.Pots.TrimEnd('.') != "." + last.TrimEnd('.'))
 			{
-			//	Console.WriteLine(state);
-
-				var bare = state.Trim('.');
-				if (seen.Contains(bare))
-				{
-					var dotlen = state.TakeWhile(c => c == '.').Count();
-					Console.WriteLine($"Bingo at {g}");
-					break;
-				}
-				seen.Add(bare);
-
-				//state += "....";
-
-		//		Console.WriteLine(state);
-
-				// state = "...." + state;
-				// offset += 4;
-				// if (state[0] == '#')
-				// {
-				// 	state = ".." + state;
-				// 	offset += 2;
-				// }
-				// else if (state[1] == '#')
-				// {
-				// 	state = "." + state;
-				// 	offset++;
-				// }
-
-				// if (state[state.Length-1] == '#')
-				// 	state = state + "..";
-				// else if (state[state.Length-2] == '#')
-				// 	state = ".." + state;
-//				state += "....";
-				// Console.WriteLine(state);
-				// Console.WriteLine();
-
-				var newstate = state.ToCharArray();
-				for (var i = 0; i < state.Length - 4; i++)
-				{
-					var pat = state[i..(i+5)];
-					if (rules.TryGetValue(pat, out var ch))
-					{
-						newstate[i+2] = ch;
-					}
-					else
-					{
-						newstate[i+2] = '.';
-					}
-				}
-
-				state = new string(newstate);
-
-
+				last = tunnel.Pots;
+				tunnel.Grow();
 			}
 
-			// offset = 59;
-			//var add = 59 + 50_000_000_000UL - 100;
-			var add = 60 + 50_000_000_000UL - 100;
-			//var add = (ulong)offset*loops;
+			// After this many iterations the sum was x.
+			// For every next iteration the sum will grow by the number of pots,
+			// as all pots are shifted to the right, ie every pot's value will
+			// be incremented by one.
+			var sumNow = tunnel.SumOfPots;
+			var sumRest = (50_000_000_000 - tunnel.Iterations) * tunnel.NumberOfPots;
+			var sumFinal = sumNow + sumRest; 
+			return sumFinal;
+		}
 
+		internal class Tunnel
+		{
+			private readonly Dictionary<string, char> _rules;
 
-			var plants = 0UL;
-			var bare2 = state.Trim('.');
-			for (var i = 0; i < bare2.Length; i++)
+			public Tunnel(string[] input)
 			{
-				if (bare2[i] == '#')
-				{
-					var id = (ulong)i + add;
-					plants += id;
-				}
+				Pots = input[0].RxMatch("initial state: %*").Get<string>();
+				_rules = input[2..]
+					.Select(line => line.Split(" => ").ToArray())
+					.ToDictionary(x => x[0], x => x[1][0]);				
 			}
 
-			return 0;
-			return plants;
+			public string Pots { get; private set; }
+			public int Padding { get; private set; }
+			public int Iterations { get; private set; }
+			public int SumOfPots => Pots.Select((c, i) => c == '#' ? i - Padding : 0).Sum();
+			public int NumberOfPots => Pots.Count(c => c == '#');
 
+			public void Grow()
+			{
+				// Ensure that there are always 4 "empty pots" at the beginning
+				// and at the end of the pots.
+				if (Pots[0..4] != "....")
+				{
+					Pots = "...." + Pots;
+					Padding += 4;
+				}
+				if (Pots[^4..] != "....")
+				{
+					Pots = Pots + "....";
+				}
+
+				// Grow the next pots, by checking for matches to any rule-patterns.
+				var next = Pots.ToCharArray();
+				for (var i = 0; i < Pots.Length - 4; i++)
+				{
+					var pattern = Pots[i..(i+5)];
+					next[i+2] = _rules.TryGetValue(pattern, out var ch) ? ch : '.';
+				}
+				Pots = new string(next);
+				Iterations++;
+			}
 		}
 	}
 }
