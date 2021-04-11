ededa11e588704f7225da1e1f88c1cbc503a92a4
Richard Flamsholt <richard@flamsholt.dk>
2021-04-11 23:19:19 +0200

    2018 Day 16: Chronal Classification

--- AdventOfCode/Y2018/Day16/Puzzle16.cs
+++ AdventOfCode/Y2018/Day16/Puzzle16.cs
@@ -2,10 +2,8 @@ using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
+using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2018.Day16
 {
@@ -18,103 +16,108 @@ namespace AdventOfCode.Y2018.Day16
 
 		public void Run()
 		{
-			//Run("test1").Part1(0).Part2(0);
+			Run("input").Part1(607).Part2(577);
+		}
 
-			Run("input").Part1(0).Part2(0);
+		protected override int Part1(string[] input)
+		{
+			var (effects, _) = ReadInput(input);
+
+			// Count the number of effects that satisfies 3 or more opcodes
+			var n = effects
+				.Select(e => Computer.Opcodes.Count(opc => e.SatisfiesOpcode(opc)))
+				.Count(x => x >= 3);
+
+			return n;
+		}
+
+		protected override int Part2(string[] input)
+		{
+			var (effects, instructions) = ReadInput(input);
+
+			// For every opcode, find the effects that is satisified by that opcode and pick
+			// out their first instruction-value. Those are the possible numeric values that
+			// the opcode can be, so make a mapping from opcode to those candidates
+			var opcodeCandidates = Computer.Opcodes
+				.ToDictionary(x => x, x => effects.Where(e => e.SatisfiesOpcode(x)).Select(e => e.Ins[0]).ToHashSet());
+
+			// Turns out that there is (by puzzle design) always an opcode that has
+			// just one single numeric value candidate; when adding that mapping and
+			// removing that option, there's another (or more) with just one candicate,
+			// all the way through the very last opcode. That's our mapping.
+			var intToOpcode = new Dictionary<int, Computer.Opcode>();
+			while (opcodeCandidates.Any())
+			{
+				foreach (var next in opcodeCandidates.Where(x => x.Value.Count() == 1))
+				{
+					var opc = next.Key;
+					var val = next.Value.Single();
+					opcodeCandidates.Remove(opc);
+					intToOpcode[val] = opc;
+					foreach (var x in opcodeCandidates)
+					{
+						x.Value.Remove(val);
+					}
+				}
+			}
+
+			// Execute the instructions based on the int-to-opcode mapping
+			var regs = new int[4];
+			foreach (var ins in instructions)
+			{
+				var v = ins.ToIntArray();
+				var opc = intToOpcode[v[0]];
+				Computer.Process(regs, opc, v[1], v[2], v[3]);
+			}
+
+			// Result is the value of register 0
+			var register0 = regs[0];
+			return register0;
+		}
+
+		private static (Effect[], string[]) ReadInput(string[] input)
+		{
+			var parts = input.GroupByEmptyLine().ToArray();
+			var effects = parts
+				.TakeWhile(p => p.Length == 3)
+				.Select(p => new Effect(p))
+				.ToArray();
+			var instructions = parts.Last();
+			return (effects, instructions);
 		}
 
 		internal class Computer
 		{
-			// addr (add register) stores into register C the result of adding register A and register B.
-			// addi (add immediate) stores into register C the result of adding register A and value B.
-			// Multiplication:
-
-			// mulr (multiply register) stores into register C the result of multiplying register A and register B.
-			// muli (multiply immediate) stores into register C the result of multiplying register A and value B.
-			// Bitwise AND:
-
-			// banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
-			// bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
-			// Bitwise OR:
-
-			// borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
-			// bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
-			// Assignment:
-
-			// setr (set register) copies the contents of register A into register C. (Input B is ignored.)
-			// seti (set immediate) stores value A into register C. (Input B is ignored.)
-			// Greater-than testing:
-
-			// gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
-			// gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
-			// gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
-			// Equality testing:
-
-			// eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
-			// eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
-			// eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
-
 			public enum Opcode
 			{
 				addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr
 			}
-			public static int[] Process(int[] regs, Opcode opcode, int a, int b, int c)
+
+			public readonly static Computer.Opcode[] Opcodes = (Computer.Opcode[])Enum.GetValues(typeof(Computer.Opcode));
+
+			public static void Process(int[] regs, Opcode opcode, int a, int b, int c)
 			{
-				regs = regs.ToArray();
 				switch (opcode)
 				{
-					case Opcode.addr:
-						regs[c] = regs[a] + regs[b];
-						break;
-					case Opcode.addi:
-						regs[c] = regs[a] + b;
-						break;
-					case Opcode.mulr:
-						regs[c] = regs[a] * regs[b];
-						break;
-					case Opcode.muli:
-						regs[c] = regs[a] * b;
-						break;
-					case Opcode.banr:
-						regs[c] = regs[a] & regs[b];
-						break;
-					case Opcode.bani:
-						regs[c] = regs[a] & b;
-						break;
-					case Opcode.borr:
-						regs[c] = regs[a] | regs[b];
-						break;
-					case Opcode.bori:
-						regs[c] = regs[a] | b;
-						break;
-					case Opcode.setr:
-						regs[c] = regs[a];
-						break;
-					case Opcode.seti:
-						regs[c] = a;
-						break;
-					case Opcode.gtir:
-						regs[c] = a > regs[b] ? 1 : 0;
-						break;
-					case Opcode.gtri:
-						regs[c] = regs[a] > b ? 1 : 0;
-						break;
-					case Opcode.gtrr:
-						regs[c] = regs[a] > regs[b] ? 1 : 0;
-						break;
-					case Opcode.eqir:
-						regs[c] = a == regs[b] ? 1 : 0;
-						break;
-					case Opcode.eqri:
-						regs[c] = regs[a] == b ? 1 : 0;
-						break;
-					case Opcode.eqrr:
-						regs[c] = regs[a] == regs[b] ? 1 : 0;
-						break;
+					case Opcode.addr: regs[c] = regs[a] + regs[b]; break;
+					case Opcode.addi: regs[c] = regs[a] + b; break;
+					case Opcode.mulr: regs[c] = regs[a] * regs[b]; break;
+					case Opcode.muli: regs[c] = regs[a] * b; break;
+					case Opcode.banr: regs[c] = regs[a] & regs[b]; break;
+					case Opcode.bani: regs[c] = regs[a] & b; break;
+					case Opcode.borr: regs[c] = regs[a] | regs[b]; break;
+					case Opcode.bori: regs[c] = regs[a] | b; break;
+					case Opcode.setr: regs[c] = regs[a]; break;
+					case Opcode.seti: regs[c] = a; break;
+					case Opcode.gtir: regs[c] = a > regs[b] ? 1 : 0; break;
+					case Opcode.gtri: regs[c] = regs[a] > b ? 1 : 0; break;
+					case Opcode.gtrr: regs[c] = regs[a] > regs[b] ? 1 : 0; break;
+					case Opcode.eqir: regs[c] = a == regs[b] ? 1 : 0; break;
+					case Opcode.eqri: regs[c] = regs[a] == b ? 1 : 0; break;
+					case Opcode.eqrr: regs[c] = regs[a] == regs[b] ? 1 : 0; break;
 					default:
 						throw new Exception($"Unknown opcode {opcode}");
 				}
-				return regs;
 			}
 		}
 
@@ -135,47 +138,13 @@ namespace AdventOfCode.Y2018.Day16
 				After = new []{ a1, a2, a3, a4 };
 				Ins = new []{ i1, i2, i3, i4 };
 			}
-		}
 
-		protected override int Part1(string[] input)
-		{
-			var parts = input.GroupByEmptyLine().ToArray();
-			var effects = parts
-				.TakeWhile(p => p.Length == 3)
-				.Select(p => new Effect(p))
-				.ToArray();
-
-			var n = 0;
-			foreach (var e in effects)
+			public bool SatisfiesOpcode(Computer.Opcode opc)
 			{
-				var matches = 0;
-				foreach (var opc in (Computer.Opcode[])Enum.GetValues(typeof(Computer.Opcode)))
-				{
-					var regs2 = Computer.Process(e.Before, opc, e.Ins[1], e.Ins[2], e.Ins[3]);
-					if (regs2.SequenceEqual(e.After))
-					{
-						matches++;
-					}
-				}
-				if (matches >= 3)
-					n++;
+				var regs = Before.ToArray(); // Don't mutate Before, even though it really doesn't matter
+				Computer.Process(regs, opc, Ins[1], Ins[2], Ins[3]);
+				return regs.SequenceEqual(After);
 			}
-
-			// //var xx = parts.Skip(parts.Length - 5).ToArray();
-
-			// var regs = new Dictionary<char, int>();
-
-			return n;
-		}
-
-		protected override int Part2(string[] input)
-		{
-
-
-
-
-
-			return 0;
 		}
 	}
 }
