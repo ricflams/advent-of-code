b613bff6c94096e2d008c253dcaf2ad8ecc42de9
ricflams <richard@flamsholt.dk>
2021-11-27 21:21:13 +0100

    2018 Day 17: Reservoir Research

--- AdventOfCode/Y2018/Day17/Puzzle17.cs
+++ AdventOfCode/Y2018/Day17/Puzzle17.cs
@@ -1,153 +1,129 @@
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2018.Day17
 {
 	internal class Puzzle : Puzzle<int, int>
 	{
-		public static Puzzle Instance = new Puzzle();
+		public static Puzzle Instance = new();
 		public override string Name => "Reservoir Research";
 		public override int Year => 2018;
 		public override int Day => 17;
 
 		public void Run()
 		{
-			Run("test1").Part1(57);
-			//Run("input").Part1(607).Part2(577);
+			Run("test1").Part1(57).Part2(29);
+			Run("input").Part1(31883).Part2(24927);
 		}
 
 		protected override int Part1(string[] input)
 		{
-			//var (effects, _) = ReadInput(input);
-
-			//// Count the number of effects that satisfies 3 or more opcodes
-			//var n = effects
-			//	.Select(e => Computer.Opcodes.Count(opc => e.SatisfiesOpcode(opc)))
-			//	.Count(x => x >= 3);
-
-			//return n;
-			return 0;
+			var reservoir = new Reservoir(input);
+			reservoir.Fill();
+			return reservoir.RunningWater + reservoir.SteadyWater;
 		}
 
 		protected override int Part2(string[] input)
 		{
-			return 0;
-			//var (effects, instructions) = ReadInput(input);
-
-			//// For every opcode, find the effects that is satisified by that opcode and pick
-			//// out their first instruction-value. Those are the possible numeric values that
-			//// the opcode can be, so make a mapping from opcode to those candidates
-			//var opcodeCandidates = Computer.Opcodes
-			//	.ToDictionary(x => x, x => effects.Where(e => e.SatisfiesOpcode(x)).Select(e => e.Ins[0]).ToHashSet());
-
-			//// Turns out that there is (by puzzle design) always an opcode that has
-			//// just one single numeric value candidate; when adding that mapping and
-			//// removing that option, there's another (or more) with just one candicate,
-			//// all the way through the very last opcode. That's our mapping.
-			//var intToOpcode = new Dictionary<int, Computer.Opcode>();
-			//while (opcodeCandidates.Any())
-			//{
-			//	foreach (var next in opcodeCandidates.Where(x => x.Value.Count() == 1))
-			//	{
-			//		var opc = next.Key;
-			//		var val = next.Value.Single();
-			//		opcodeCandidates.Remove(opc);
-			//		intToOpcode[val] = opc;
-			//		foreach (var x in opcodeCandidates)
-			//		{
-			//			x.Value.Remove(val);
-			//		}
-			//	}
-			//}
-
-			//// Execute the instructions based on the int-to-opcode mapping
-			//var regs = new int[4];
-			//foreach (var ins in instructions)
-			//{
-			//	var v = ins.ToIntArray();
-			//	var opc = intToOpcode[v[0]];
-			//	Computer.Process(regs, opc, v[1], v[2], v[3]);
-			//}
-
-			//// Result is the value of register 0
-			//var register0 = regs[0];
-			//return register0;
+			var reservoir = new Reservoir(input);
+			reservoir.Fill();
+			return reservoir.SteadyWater;
 		}
+	}
 
-		private static (Effect[], string[]) ReadInput(string[] input)
-		{
-			var parts = input.GroupByEmptyLine().ToArray();
-			var effects = parts
-				.TakeWhile(p => p.Length == 3)
-				.Select(p => new Effect(p))
-				.ToArray();
-			var instructions = parts.Last();
-			return (effects, instructions);
-		}
+	internal class Reservoir
+	{
+		private readonly Point _spring = Point.From(500, 0);
+		private readonly CharMap _map = new('.');
+		private readonly Point _min, _max;
 
-		internal class Computer
+		public Reservoir(string[] input)
 		{
-			public enum Opcode
+			foreach (var line in input)
 			{
-				addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr
+				// Example: x=495, y=2..7
+				var (name, val1, _, val21, val22) = line.RxMatch("%c=%d, %c=%d..%d").Get<char, int, char, int, int>();
+				if (name == 'x')
+				{
+					var x = val1;
+					for (var y = val21; y <= val22; y++)
+					{
+						_map[x][y] = '#';
+					}
+				}
+				else
+				{
+					var y = val1;
+					for (var x = val21; x <= val22; x++)
+					{
+						_map[x][y] = '#';
+					}
+				}
+			}
+			(_min, _max) = _map.Area();
+
+			_map[_spring] = '+';
+		}
+
+		public int RunningWater => _map.Count('|');
+		public int SteadyWater => _map.Count('~');
+
+		public void Fill()
+		{
+			Fill(_spring);
+		}
+
+		private void Fill(Point p)
+		{
+			if (p.Y == _max.Y)
+				return;
+
+			if (!_map[p.Down].IsSurface())
+			{
+				// Only fill with running water above the min Y position
+				if (p.Down.Y >= _min.Y)
+				{
+					_map[p.Down] = '|';
+				}
+
+				Fill(p.Down);
 			}
 
-			public readonly static Computer.Opcode[] Opcodes = (Computer.Opcode[])Enum.GetValues(typeof(Computer.Opcode));
-
-			public static void Process(int[] regs, Opcode opcode, int a, int b, int c)
+			if (_map[p.Down].IsSurface())
 			{
-				switch (opcode)
+				if (_map[p.Right] == '.')
 				{
-					case Opcode.addr: regs[c] = regs[a] + regs[b]; break;
-					case Opcode.addi: regs[c] = regs[a] + b; break;
-					case Opcode.mulr: regs[c] = regs[a] * regs[b]; break;
-					case Opcode.muli: regs[c] = regs[a] * b; break;
-					case Opcode.banr: regs[c] = regs[a] & regs[b]; break;
-					case Opcode.bani: regs[c] = regs[a] & b; break;
-					case Opcode.borr: regs[c] = regs[a] | regs[b]; break;
-					case Opcode.bori: regs[c] = regs[a] | b; break;
-					case Opcode.setr: regs[c] = regs[a]; break;
-					case Opcode.seti: regs[c] = a; break;
-					case Opcode.gtir: regs[c] = a > regs[b] ? 1 : 0; break;
-					case Opcode.gtri: regs[c] = regs[a] > b ? 1 : 0; break;
-					case Opcode.gtrr: regs[c] = regs[a] > regs[b] ? 1 : 0; break;
-					case Opcode.eqir: regs[c] = a == regs[b] ? 1 : 0; break;
-					case Opcode.eqri: regs[c] = regs[a] == b ? 1 : 0; break;
-					case Opcode.eqrr: regs[c] = regs[a] == regs[b] ? 1 : 0; break;
-					default:
-						throw new Exception($"Unknown opcode {opcode}");
+					_map[p.Right] = '|';
+					Fill(p.Right);
+				}
+
+				if (_map[p.Left] == '.')
+				{
+					_map[p.Left] = '|';
+					Fill(p.Left);
+				}
+			}
+
+			if (_map[p.Left] == '#')
+			{
+				// If the running water is bounded by clay at each side then re-mark is as steady
+				var edge = p.Right;
+				while (_map[edge] == '|' && _map[edge.Down].IsSurface())
+					edge = edge.Right;
+				if (_map[edge] == '#')
+				{
+					// Yes, bounded, so so re-mark as steady
+					for (var steady = p; steady != edge; steady = steady.Right)
+					{
+						_map[steady] = '~';
+					}
 				}
 			}
 		}
+	}
 
-		internal class Effect
-		{
-			public int[] Before { get; }
-			public int[] After { get; }
-			public int[] Ins { get; }
-			public Effect(string[] input)
-			{
-				// Before: [1, 2, 0, 3]
-				// 14 0 3 2
-				// After:  [1, 2, 0, 3]
-				var (b1, b2, b3, b4) = input[0].RxMatch("Before: [%d, %d, %d, %d]").Get<int, int, int, int>();
-				var (i1, i2, i3, i4) = input[1].RxMatch("%d %d %d %d").Get<int, int, int, int>();
-				var (a1, a2, a3, a4) = input[2].RxMatch("After:  [%d, %d, %d, %d]").Get<int, int, int, int>();
-				Before = new []{ b1, b2, b3, b4 };
-				After = new []{ a1, a2, a3, a4 };
-				Ins = new []{ i1, i2, i3, i4 };
-			}
-
-			public bool SatisfiesOpcode(Computer.Opcode opc)
-			{
-				var regs = Before.ToArray(); // Don't mutate Before, even though it really doesn't matter
-				Computer.Process(regs, opc, Ins[1], Ins[2], Ins[3]);
-				return regs.SequenceEqual(After);
-			}
-		}
+	static class Extensions
+	{
+		public static bool IsSurface(this char ch) => ch is '#' or '~';
 	}
 }
