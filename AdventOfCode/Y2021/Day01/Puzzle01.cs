5d6a8b628b0d927660f7b5e9b060e1a7df1e4697
Richard Flamsholt <richard@flamsholt.dk>
2021-12-01 07:13:59 +0100

    2021 Day 1: Sonar Sweep, cleaned

--- AdventOfCode/Y2021/Day01/Puzzle01.cs
+++ AdventOfCode/Y2021/Day01/Puzzle01.cs
@@ -1,68 +1,52 @@
-using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 
 namespace AdventOfCode.Y2021.Day01
 {
 	internal class Puzzle : Puzzle<int, int>
 	{
 		public static Puzzle Instance = new Puzzle();
-		public override string Name => "DAY1";
+		public override string Name => "Sonar Sweep";
 		public override int Year => 2021;
 		public override int Day => 1;
 
 		public void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-			//Run("test2").Part1(0).Part2(0);
-			Run("input").Part1(0).Part2(0);
+			Run("test1").Part1(7).Part2(5);
+			Run("input").Part1(1482).Part2(1518);
 		}
 
 		protected override int Part1(string[] input)
 		{
-			var vs = input.Select(int.Parse).ToArray();
+			var v = input.Select(int.Parse).ToArray();
 
 			var n = 0;
-			var last = (int?)null;
-			for (var i = 0; i < vs.Length; i++)
+			for (var i = 0; i < v.Length - 1; i++)
 			{
-				var x = 0;
-				if (i == 0)
-					x = vs[0];
-				else if (i == 1)
-					x = vs[0] + vs[1];
-				else if (i == vs.Length - 1)
-					x = vs[vs.Length - 1];
-				else if (i == vs.Length-2)
-					x = vs[vs.Length - 1] + vs[vs.Length-2];
-				else
-					x = vs[i] + vs[i + 1] + vs[i + 2];
-
-				//if (last != null)
-				{
-					if (x > last)
-						n++;
-				}
-				last = x;
+				if (v[i] < v[i + 1])
+					n++;
 			}
-
 
 			return n;
 		}
 
 		protected override int Part2(string[] input)
 		{
+			var v = input.Select(int.Parse).ToArray();
 
+			var last = (int?)null;
+			var n = 0;
+			for (var i = 0; i < v.Length - 2; i++)
+			{
+				var sum = v[i] + v[i + 1] + v[i + 2];
+				if (sum > last)
+				{
+					n++;
+				}
+				last = sum;
+			}
 
-
-
-
-			return 0;
+			return n;
 		}
 	}
 }
