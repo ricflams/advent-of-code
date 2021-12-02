31d775a1cde1ff9be30e4318df05045258a8875a
Richard Flamsholt <richard@flamsholt.dk>
2021-12-03 00:02:13 +0100

    2018 Day 19: Go With The Flow, part 2

--- AdventOfCode/Y2018/Day19/Puzzle19.cs
+++ AdventOfCode/Y2018/Day19/Puzzle19.cs
@@ -1,27 +1,24 @@
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2018.Day19
 {
-	internal class Puzzle : Puzzle<long, long>
+	internal class Puzzle : Puzzle<int, int>
 	{
-		public static Puzzle Instance = new Puzzle();
+		public static Puzzle Instance = new();
 		public override string Name => "Go With The Flow";
 		public override int Year => 2018;
 		public override int Day => 19;
 
 		public void Run()
 		{
-			//Run("test1").Part1(6);
-			//Run("input").Part1(1056).Part2(0);
-			Run("input").Part2(0);
+			Run("test1").Part1(6);
+			Run("input").Part1(1056).Part2(10915260);
 		}
 
-		protected override long Part1(string[] input)
+		protected override int Part1(string[] input)
 		{
 			var computer = new Computer(input);
 
@@ -31,49 +28,98 @@ namespace AdventOfCode.Y2018.Day19
 			return reg0;
 		}
 
-		protected override long Part2(string[] input)
+		protected override int Part2(string[] input)
 		{
 			var computer = new Computer(input);
+
+			// The input program first does some init and then jump back to
+			// the main loop-code in line 1-16, annotated as pseuode-code:
+			//  0 addi 3 16 3  goto 17
+			//  1 seti 1 2 5     f = 1
+			//  2 seti 1 3 2     c = 1
+			//  3 mulr 5 2 1     b = c * f
+			//  4 eqrr 1 4 1     b = b==e?1:0
+			//  5 addr 1 3 3     ip += b       # goto 7 if b==e
+			//  6 addi 3 1 3     ip++          # else goto 8
+			//  7 addr 5 0 0     a += f
+			//  8 addi 2 1 2     c++
+			//  9 gtrr 2 4 1     b = c>e?1:0
+			// 10 addr 3 1 3     ip += b       # goto 12 if c>e
+			// 11 seti 2 5 3     ip = 2        # else goto 3
+			// 12 addi 5 1 5     f++
+			// 13 gtrr 5 4 1     b = f>e?1:0
+			// 14 addr 1 3 3     ip += b       # goto 16 if f>e
+			// 15 seti 1 2 3     ip = 2        # else goto 2
+			// 16 mulr 3 3 3     ip = 256      # exit
+			// 17 addi 4 2 4 init stuff
+			//    mulr 4 4 4  more etc ...
+			//    .....
+			//    seti 0 7 3 goto 1
+			//
+			// The pseudo-code can be rewritten as:
+			//   f = 1
+			//   do
+			//       do
+			//           c = 1
+			//           if (c * f == e)
+			//               a += f
+			//           c++
+			//       while c <= e
+			//       f++
+			//   while f <= e
+			//
+			// which can be rewritten as
+			//   for (f = 1; f <= e; f++)
+			//       for (c = 1; c <= e; c++)
+			//           if (c * f == e)
+			//               a += f
+			//
+			// which can be reduced to
+			//   for (f = 1; f <= e; f++)
+			//       if (e % f == 0)
+			//           a += f
+			//
+			// So we will simply run the init-code and then do the above as
+			// real code. Modify instriction 1 to set ip to something invalid
+			// so it will bail after the init-code.
 
 			computer.Regs[0] = 1;
-
-			//computer._ins[3] = new Computer.Ins { Opcode = Computer.Opcode.addr, A = 5, B = 0, C = 0 };
-
-			computer._ins[3] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[4] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[5] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[6] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			//computer._ins[7] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[8] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[9] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[10] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
-			computer._ins[11] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 0, B = 0, C = 1 };
+			computer.Instructions[1] = new Computer.Ins { Opcode = Computer.Opcode.seti, A = 999, B = 0, C = computer.IpRegister };
 			computer.Run();
-			var reg0 = computer.Regs[0];
 
-			return reg0;
+			var a = computer.Regs[0];
+			var e = computer.Regs[4];
+
+			for (var f = 1; f <= e; f++)
+			{
+				if (e % f == 0)
+				{
+					a += f;
+				}
+			}
+
+			return a;
 		}
 
 		internal class Computer
 		{
-			public Ins[] _ins;
-			private readonly int _ipreg;
-			private int _ip;
-
-			public readonly long[] Regs;
-
 			public enum Opcode
 			{
 				addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr
 			}
 
+			public readonly int[] Regs = new int[6];
+			public readonly int IpRegister;
+			public readonly Ins[] Instructions;
+
 			public Computer(string[] input)
 			{
-				//#ip 3
-				//addi 3 16 3
-				//seti 1 2 5
-				_ipreg = input[0].RxMatch("#ip %d").Get<int>();
-				_ins = input
+				// Example:
+				// #ip 3
+				// addi 3 16 3
+				// seti 1 2 5
+				IpRegister = input[0].RxMatch("#ip %d").Get<int>();
+				Instructions = input
 					.Skip(1)
 					.Select(s =>
 					{
@@ -87,10 +133,6 @@ namespace AdventOfCode.Y2018.Day19
 						};
 					})
 					.ToArray();
-				Regs = new long[6];
-				_ip = 0;
-
-				IpSeen = new int[_ins.Length];
 			}
 
 			public class Ins
@@ -104,56 +146,38 @@ namespace AdventOfCode.Y2018.Day19
 
 			public void Run()
 			{
-				_ip = 0;
-				while (_ip >= 0 && _ip < _ins.Length)
+				for (var ip = 0; ip >= 0 && ip < Instructions.Length; ip++)
 				{
-					Step();
+					var ins = Instructions[ip];
+					var (opcode, a, b, c) = (ins.Opcode, ins.A, ins.B, ins.C);
+
+					Regs[IpRegister] = ip;
+
+					switch (opcode)
+					{
+						case Opcode.addr: Regs[c] = Regs[a] + Regs[b]; break;
+						case Opcode.addi: Regs[c] = Regs[a] + b; break;
+						case Opcode.mulr: Regs[c] = Regs[a] * Regs[b]; break;
+						case Opcode.muli: Regs[c] = Regs[a] * b; break;
+						case Opcode.banr: Regs[c] = Regs[a] & Regs[b]; break;
+						case Opcode.bani: Regs[c] = Regs[a] & b; break;
+						case Opcode.borr: Regs[c] = Regs[a] | Regs[b]; break;
+						case Opcode.bori: Regs[c] = Regs[a] | b; break;
+						case Opcode.setr: Regs[c] = Regs[a]; break;
+						case Opcode.seti: Regs[c] = a; break;
+						case Opcode.gtir: Regs[c] = a > Regs[b] ? 1 : 0; break;
+						case Opcode.gtri: Regs[c] = Regs[a] > b ? 1 : 0; break;
+						case Opcode.gtrr: Regs[c] = Regs[a] > Regs[b] ? 1 : 0; break;
+						case Opcode.eqir: Regs[c] = a == Regs[b] ? 1 : 0; break;
+						case Opcode.eqri: Regs[c] = Regs[a] == b ? 1 : 0; break;
+						case Opcode.eqrr: Regs[c] = Regs[a] == Regs[b] ? 1 : 0; break;
+						default:
+							throw new Exception($"Unknown opcode {opcode}");
+					}
+
+					ip = Regs[IpRegister];
 				}
-			}
-
-			private int[] IpSeen;
-
-			private void Step()
-			{
-				IpSeen[_ip]++;
-
-				Console.WriteLine(string.Join(" ", IpSeen.Select(x => x.ToString("D3"))));
-
-				var ins = _ins[_ip];
-				var (opcode, a, b, c) = (ins.Opcode, ins.A, ins.B, ins.C);
-
-				//Console.Write($"ip={_ip} [{string.Join(",", _regs.Select(x=>x.ToString()))}] {opcode} {a} {b} {c}");
-
-				Regs[_ipreg] = _ip;
-
-				switch (ins.Opcode)
-				{
-					case Opcode.addr: Regs[c] = Regs[a] + Regs[b]; break;
-					case Opcode.addi: Regs[c] = Regs[a] + b; break;
-					case Opcode.mulr: Regs[c] = Regs[a] * Regs[b]; break;
-					case Opcode.muli: Regs[c] = Regs[a] * b; break;
-					case Opcode.banr: Regs[c] = Regs[a] & Regs[b]; break;
-					case Opcode.bani: Regs[c] = Regs[a] & b; break;
-					case Opcode.borr: Regs[c] = Regs[a] | Regs[b]; break;
-					case Opcode.bori: Regs[c] = Regs[a] | b; break;
-					case Opcode.setr: Regs[c] = Regs[a]; break;
-					case Opcode.seti: Regs[c] = a; break;
-					case Opcode.gtir: Regs[c] = a > Regs[b] ? 1 : 0; break;
-					case Opcode.gtri: Regs[c] = Regs[a] > b ? 1 : 0; break;
-					case Opcode.gtrr: Regs[c] = Regs[a] > Regs[b] ? 1 : 0; break;
-					case Opcode.eqir: Regs[c] = a == Regs[b] ? 1 : 0; break;
-					case Opcode.eqri: Regs[c] = Regs[a] == b ? 1 : 0; break;
-					case Opcode.eqrr: Regs[c] = Regs[a] == Regs[b] ? 1 : 0; break;
-					default:
-						throw new Exception($"Unknown opcode {opcode}");
-				}
-
-				_ip = (int)Regs[_ipreg];
-				_ip++;
-
-				//Console.WriteLine($" [{string.Join(",", _regs.Select(x => x.ToString()))}]");
 			}
 		}
-
 	}
 }
