af6f48a6b8d6c64e7c7555ff7150cf56988a6891
Richard Flamsholt <richard@flamsholt.dk>
2021-12-07 23:20:23 +0100

    Introduce MutableArray

---
+++ AdventOfCode/Helpers/MutableArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Helpers
+{
+	internal class MutableArray<T> : IEnumerable
+	{
+		private readonly T[] _array;
+
+		public MutableArray(T[] array) => _array = array;
+
+		public IEnumerator<T> GetEnumerator() => new PurgeableArrayEnumerator(_array);
+		IEnumerator IEnumerable.GetEnumerator() => throw new Exception();
+
+		public void Purge(T item)
+		{
+			var index = Array.FindIndex(_array, x => item.Equals(x));
+			if (index != -1)
+			{
+				_array[index] = default;
+			}
+		}
+
+		public int Length => _array.Count(x => x != null);
+
+		public class PurgeableArrayEnumerator : IEnumerator<T>
+		{
+			private readonly T[] _array;
+			private int _index = -1;
+
+			public PurgeableArrayEnumerator(T[] array) => _array = array;
+
+			public T Current => _array[_index];
+			object IEnumerator.Current => Current;
+
+			public void Reset()
+			{
+				_index = -1;
+			}
+
+			public bool MoveNext()
+			{
+				do
+				{
+					_index++;
+					if (_index >= _array.Length)
+						return false;
+				} while (_array[_index] == null);
+				return true;
+			}
+
+			public void Dispose() { }
+		}
+	}
+
+	internal static class MutableArrayExtensions
+	{
+		public static MutableArray<T> ToMutableArray<T>(this IEnumerable<T> enumerable)
+		{
+			return new MutableArray<T>(enumerable.ToArray());
+		}
+	}
+
+}
--- AdventOfCode/Y2021/Day04/Puzzle04.cs
+++ AdventOfCode/Y2021/Day04/Puzzle04.cs
@@ -61,14 +61,14 @@ namespace AdventOfCode.Y2021.Day04
 			throw new Exception("No winning board");
 		}
 
-		private static (int[], PurgeableArray<Board>) ReadBoards(string[] input)
+		private static (int[], MutableArray<Board>) ReadBoards(string[] input)
 		{
 			var numbers = input.First().ToIntArray();
 			var boards = input
 				.Skip(1)
 				.GroupByEmptyLine()
 				.Select(lines => new Board(lines))
-				.ToPurgeableArray();
+				.ToMutableArray();
 			return (numbers, boards);
 		}
 
@@ -104,70 +104,4 @@ namespace AdventOfCode.Y2021.Day04
 			}
 		}
 	}
-
-	public static class Extensions
-	{
-		public static PurgeableArray<T> ToPurgeableArray<T>(this IEnumerable<T> enumerable)
-		{
-			return new PurgeableArray<T>(enumerable.ToArray());
-		}
-	}
-
-
-	public class PurgeableArray<T> : IEnumerable
-	{
-		private readonly T[] _array;
-
-		public PurgeableArray(T[] array) => _array = array;
-
-		public IEnumerator<T> GetEnumerator() => new PurgeableArrayEnumerator(_array);
-		IEnumerator IEnumerable.GetEnumerator() => 
-			throw new Exception();
-//			_enumerator;
-
-		public void Purge(T item)
-		{
-			var index = Array.FindIndex(_array, x => item.Equals(x));
-			if (index != -1)
-			{
-				_array[index] = default;
-			}
-		}
-
-		public int Length => _array.Count(x => x != null);
-
-		public class PurgeableArrayEnumerator : IEnumerator<T>
-		{
-			private readonly T[] _array;
-			private int _index = -1;
-
-			public PurgeableArrayEnumerator(T[] array) => _array = array;
-
-			public T Current => _array[_index];
-
-			object IEnumerator.Current => Current;
-
-			public void Dispose()
-			{
-				//throw new NotImplementedException();
-			}
-
-			public bool MoveNext()
-			{
-				do
-				{
-					_index++;
-					if (_index >= _array.Length)
-						return false;
-				} while (_array[_index] == null);
-				return true;
-			}
-
-			public void Reset()
-			{
-				_index = -1;
-			}
-		}
-	}
-
 }
