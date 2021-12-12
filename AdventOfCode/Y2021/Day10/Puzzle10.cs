483876a6ac02e59b1290721ac69d92740d702626
ricflams <richard@flamsholt.dk>
2021-12-12 23:30:55 +0100

    2021 Day 10: Syntax Scoring (clean)

--- AdventOfCode/Y2021/Day10/Puzzle10.cs
+++ AdventOfCode/Y2021/Day10/Puzzle10.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2021.Day10
 {
@@ -20,33 +16,56 @@ namespace AdventOfCode.Y2021.Day10
 		public void Run()
 		{
 			Run("test1").Part1(26397).Part2(288957);
-			//Run("test1").Part2(288957);
-
-			//Run("test2").Part1(0).Part2(0);
-
 			Run("input").Part1(215229).Part2(1105996483);
 		}
 
 		protected override long Part1(string[] input)
 		{
-			var n = 0;
-			foreach (var s in input)
-			{
-				var expect = new Stack<char>();
-				var x = FirstIllegal(s, ref expect);
-				//if (x != 0)
-				//	Console.WriteLine(x);
-				n += x;
-			}
-			//var n = input
-			//	.Select(FirstIllegal)
-			//	.Sum();
-
-			return n;
+			// Find the sum of all invalid lines' syntax-error-scores
+			var score = input
+				.Select(s =>
+				{
+					var (x, _) = FindSyntaxErrorScoreAndExpectedTail(s);
+					return x;
+				})
+				.Sum();
+			return score;
 		}
 
-		private static int FirstIllegal(string s, ref Stack<char> expect)
+		protected override long Part2(string[] input)
 		{
+			// Find the scores of all non-invalid lines' missing parts
+			var scores = input
+				.Select(s =>
+				{
+					var (syntaxScore, expect) = FindSyntaxErrorScoreAndExpectedTail(s);
+					var score = 0L;
+					if (syntaxScore == 0)
+					{
+						while (expect.Any())
+						{
+							var ch = expect.Pop();
+							score *= 5;
+							if (ch == ')') score += 1;
+							if (ch == ']') score += 2;
+							if (ch == '}') score += 3;
+							if (ch == '>') score += 4;
+						}
+					}
+					return score;
+				})
+				.Where(x => x != 0)
+				.OrderBy(x => x)
+				.ToArray();
+			var score = scores[scores.Length / 2];
+
+			return score;
+		}
+
+		private static (int, Stack<char>) FindSyntaxErrorScoreAndExpectedTail(string s)
+		{
+			var expect = new Stack<char>();
+
 			foreach (var ch in s)
 			{
 				switch (ch)
@@ -56,62 +75,25 @@ namespace AdventOfCode.Y2021.Day10
 					case '{': expect.Push('}'); break;
 					case '<': expect.Push('>'); break;
 					default:
+						if (expect.Peek() != ch)
 						{
-							var e = expect.Peek();
-							if (e != ch)
+							var score = ch switch
 							{
-								if (ch == ')') return 3;
-								if (ch == ']') return 57;
-								if (ch == '}') return 1197;
-								if (ch == '>') return 25137;
-								return 0;
-							}
-							expect.Pop();
+								')' => 3,
+								']' => 57,
+								'}' => 1197,
+								'>' => 25137,
+								_ => throw new Exception($"Unexpected {ch}")
+							};
+							return (score, expect);
 						}
+						expect.Pop();
 						break;
 				}
 			}
-			return 0;
+
+			// Line is valid so syntax-error-score is 0
+			return (0, expect);
 		}
-
-		protected override long Part2(string[] input)
-		{
-			var lines = input
-				.Select(s =>
-				{
-					var expect = new Stack<char>();
-					var x = FirstIllegal(s, ref expect);
-					if (x == 0)
-					{
-				//		Console.Write($"{s}: ");
-						var add = "";
-						//expect.Pop();
-						//expect.Pop();
-						var score = 0L;
-						while (expect.Count > 0)
-						{
-							var ch = expect.Pop();
-							score *= 5;
-							if (ch == ')') score += 1;
-							if (ch == ']') score += 2;
-							if (ch == '}') score += 3;
-							if (ch == '>') score += 4;
-
-							//add += expect.Pop();
-						}
-				//		Console.WriteLine(add);
-						return score;
-					}
-					return 0;
-				})
-				.Where(x => x != 0)
-				.OrderBy(x => x)
-				.ToArray();
-			var score = lines[lines.Length / 2];
-
-
-			return score;
-		}
-
 	}
 }
