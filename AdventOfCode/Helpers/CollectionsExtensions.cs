1fea434dce99dfebd01fc8bedb3ba86af1d3980d
ricflams <richard@flamsholt.dk>
2022-01-16 23:26:28 +0100

    Add Windowed()

---
+++ AdventOfCode/Helpers/CollectionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Helpers
+{
+	internal static class CollectionsExtensions
+	{
+		public static IEnumerable<T[]> Windowed<T>(this IEnumerable<T> collection, int windowSize)
+		{
+			var v = collection.ToArray();
+			var n = v.Length - windowSize + 1;
+			for (var i = 0; i < n; i++)
+			{
+				yield return v[i..(i + windowSize)];
+			}
+		}
+	}
+}
