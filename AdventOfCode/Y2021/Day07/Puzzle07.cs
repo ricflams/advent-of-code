f72bd1650516027f6fde737a402176e7548c2def
ricflams <richard@flamsholt.dk>
2022-01-16 23:27:43 +0100

    2021 Day 7: The Treachery of Whales (clean)

--- AdventOfCode/Y2021/Day07/Puzzle07.cs
+++ AdventOfCode/Y2021/Day07/Puzzle07.cs
@@ -1,18 +1,14 @@
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2021.Day07
 {
 	internal class Puzzle : Puzzle<int, int>
 	{
-		public static Puzzle Instance = new Puzzle();
+		public static Puzzle Instance = new();
 		public override string Name => "The Treachery of Whales";
 		public override int Year => 2021;
 		public override int Day => 7;
@@ -20,55 +16,48 @@ namespace AdventOfCode.Y2021.Day07
 		public void Run()
 		{
 			Run("test1").Part1(37).Part2(168);
-			//Run("test2").Part1(0).Part2(0);
 			Run("input").Part1(344535).Part2(95581659);
-
-			// todo: clean
 		}
 
 		protected override int Part1(string[] input)
 		{
 			var v = input.First().ToIntArray();
 
-			var s = v.Sum();
-
 			var min = v.Min();
 			var max = v.Max();
-			var minresult = int.MaxValue;
-			for (var xx = min; xx <= max; xx++)
+			var minFuel = int.MaxValue;
+			for (var xpos = min; xpos <= max; xpos++)
 			{
-				var dif = v.Select(x => Math.Abs(x - xx)).Sum();
-				if (dif < minresult)
-					minresult = dif;
+				var fuel = v.Select(x => Math.Abs(x - xpos)).Sum();
+				if (fuel < minFuel)
+				{
+					minFuel = fuel;
+				}
 			}
 
-
-			return minresult;
+			return minFuel;
 		}
 
 		protected override int Part2(string[] input)
 		{
 			var v = input.First().ToIntArray();
 
-			var s = v.Sum();
-
 			var min = v.Min();
 			var max = v.Max();
-			var minresult = int.MaxValue;
-			for (var xx = min; xx <= max; xx++)
+			var minFuel = int.MaxValue;
+			for (var xpos = min; xpos <= max; xpos++)
 			{
-				var dif = v.Select(x =>
+				var fuel = v
+					.Select(x => Math.Abs(x - xpos))
+					.Select(d => d * (d + 1) / 2)
+					.Sum();
+				if (fuel < minFuel)
 				{
-					var d = Math.Abs(x - xx);
-					var fuel = d * (d + 1) / 2;
-					return fuel;
-				}).Sum();
-				if (dif < minresult)
-					minresult = dif;
+					minFuel = fuel;
+				}
 			}
 
-
-			return minresult;
+			return minFuel;
 		}
 	}
 }
