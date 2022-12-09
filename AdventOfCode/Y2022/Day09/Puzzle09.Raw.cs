69ac85a85ed7814dc0dfc2134bd4a9fc53108d98
ricflams <richard@flamsholt.dk>
2022-12-09 15:08:04 +0100

    2022 Day 9: Rope Bridge

---
+++ AdventOfCode/Y2022/Day09/Puzzle09.Raw.cs
@@ -0,0 +1,381 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using AdventOfCode.Helpers.String;
+
+namespace AdventOfCode.Y2022.Day09.Raw
+{
+	internal class Puzzle : Puzzle<long, long>
+	{
+		public static Puzzle Instance = new();
+		public override string Name => "Day 9";
+		public override int Year => 2022;
+		public override int Day => 9;
+
+		public void Run()
+		{
+			Run("test1").Part1(13).Part2(1);
+			Run("test2").Part2(36);
+			Run("input").Part1(5981).Part2(2352);
+		}
+
+		protected override long Part1(string[] input)
+		{
+			var map = new CharMap();
+			var head = Point.Origin;
+			var tail = Point.Origin;
+
+			// L 3
+			// U 5
+			// R 2
+			// U 13			
+			map[tail] = '#';
+			foreach (var s in input)
+			{
+				var (dir0, n) = s.RxMatch("%c %d").Get<char, int>();
+				for (var i = 0; i < n; i++)
+				{
+					var dir = dir0;
+					head = Move(dir, head);
+
+					tail = Pull3(dir, head, tail);
+					//Pull(ref dir, head, ref tail);
+			//		Console.WriteLine(tail);
+					map[tail] = '#';
+				}
+				// Console.WriteLine();
+				// map.ConsoleWrite();
+			}
+
+			// 6321 not right
+			// 1137 not right
+
+			return map.Count('#');
+		}
+
+		private Point Move(char dir, Point p)
+		{
+			return dir switch
+			{
+				'R' => p.Right,
+				'L' => p.Left,
+				'U' => p.Up,
+				'D' => p.Down,
+				_ => throw new Exception()
+			};
+		}
+
+		private Point Pull3(char dir, Point head, Point tail)
+		{
+			// var len = 2;
+			var dx = head.X - tail.X;
+			var dy = head.Y - tail.Y;
+
+			var adx = Math.Abs(dx);
+			var ady = Math.Abs(dy);
+
+			if (adx == 2 && ady == 0)
+			{
+				return Point.From(tail.X + (dx > 0 ? 1 : -1), tail.Y);
+			}
+			if (ady == 2 && adx == 0)
+			{
+				return Point.From(tail.X, tail.Y + (dy > 0 ? 1 : -1));
+			}
+			if (adx <= 1 && ady <= 1)
+				return tail;
+			
+			if (dx > 0 && dy > 0)
+				return tail.DiagonalDownRight;
+			if (dx > 0 && dy < 0)
+				return tail.DiagonalUpRight;
+			if (dx < 0 && dy < 0)
+				return tail.DiagonalUpLeft;
+			if (dx < 0 && dy > 0)
+				return tail.DiagonalDownLeft;
+
+			return tail;
+		}
+
+		// private void Pull(ref char dir, Point head, ref Point tail)
+		// {
+		// 	// var len = 2;
+		// 	var dx = head.X - tail.X;
+		// 	var dy = head.Y - tail.Y;
+
+		// 	var adx = Math.Abs(dx);
+		// 	var ady = Math.Abs(dy);
+			
+		// 	if (adx < 2 && ady < 2)
+		// 		return;
+			
+		// 	var (x, y) = (tail.X, tail.Y);
+
+		// 	switch (dir)
+		// 	{
+		// 		case 'U':
+		// 			if (ady > 1)
+		// 			{
+		// 				x = head.X;
+		// 				y = head.Y + 1;
+		// 			}
+		// 			else if (adx > 1)
+		// 			{
+		// 				y = head.Y;
+		// 				x += dx > 0 ? 1 : -1;
+		// 			}
+		// 			break;
+		// 		case 'D':
+		// 			if (ady > 1)
+		// 			{
+		// 				x = head.X;
+		// 				y = head.Y - 1;
+		// 			}
+		// 			else if (adx > 1)
+		// 			{
+		// 				y = head.Y;
+		// 				x += dx > 0 ? 1 : -1;
+		// 			}
+
+		// 			// if (dy > 1)
+		// 			// {
+		// 			// 	y++;
+		// 			// 	if (adx >= 1)
+		// 			// 	{
+		// 			// 		x += dx > 0 ? 1 : -1;
+		// 			// 		//dir = dx > 0 ? 'R' : 'L';
+		// 			// 	}					
+		// 			// }
+		// 			break;
+		// 		case 'L':
+		// 			if (adx > 1)
+		// 			{
+		// 				x = head.X + 1;
+		// 				y = head.Y;
+		// 			}
+		// 			else if (ady > 1)
+		// 			{
+		// 				x = head.X;
+		// 				y += dy > 0 ? 1 : -1;
+		// 			}
+		// 			// if (dx < -1)
+		// 			// {
+		// 			// 	x--;
+		// 			// 	if (ady >= 1)
+		// 			// 	{
+		// 			// 		y += dy > 0 ? 1 : -1;
+		// 			// 		//dir = dy > 0 ? 'D' : 'U';
+		// 			// 	}
+		// 			// }
+		// 			break;
+		// 		case 'R':
+		// 			if (adx > 1)
+		// 			{
+		// 				x = head.X - 1;
+		// 				y = head.Y;
+		// 			}
+		// 			else if (ady > 1)
+		// 			{
+		// 				x = head.X;
+		// 				y += dy > 0 ? 1 : -1;
+		// 			}
+		// 			// if (dx > 1)
+		// 			// {
+		// 			// 	x++;
+		// 			// 	if (ady >= 1)
+		// 			// 	{
+		// 			// 		y += dy > 0 ? 1 : -1;
+		// 			// 		//dir = dy > 0 ? 'D' : 'U';
+		// 			// 	}
+		// 			// }
+		// 			break;
+		// 	}
+		// 	tail = Point.From(x, y);
+
+		// }
+
+
+		// private bool OldPull(ref char dir, Point head, ref Point tail)
+		// {
+		// 	var len = 2;
+		// 	var dx = Math.Abs(head.X - tail.X);
+		// 	var dy = Math.Abs(head.Y - tail.Y);
+			
+		// 	if (dx == 0)
+		// 	{
+		// 		if (dy >= len)
+		// 		{
+		// 			tail = dir switch
+		// 			{
+		// 				'R' => tail.Right,
+		// 				'L' => tail.Left,
+		// 				'U' => tail.Up,
+		// 				'D' => tail.Down,
+		// 				_ => throw new Exception()
+		// 			};
+		// 			return true;
+		// 		}
+		// 	}
+		// 	else if (dy == 0)
+		// 	{
+		// 		if (dx >= len)
+		// 		{
+		// 			tail = dir switch
+		// 			{
+		// 				'R' => tail.Right,
+		// 				'L' => tail.Left,
+		// 				'U' => tail.Up,
+		// 				'D' => tail.Down,
+		// 				_ => throw new Exception()
+		// 			};
+		// 			return true;
+		// 		}
+		// 	}
+		// 	else if (dx > 1)
+		// 	{
+		// 		tail = dir switch
+		// 		{
+		// 			'R' => tail.Right,
+		// 			'L' => tail.Left,
+		// 			'U' => tail.Up,
+		// 			'D' => tail.Down,
+		// 			_ => throw new Exception()
+		// 		};
+		// 		if (tail.Y > head.Y)
+		// 		{
+		// 			tail = Point.From(tail.X, head.Y);
+		// 			dir = 'U';
+		// 		}
+		// 		else if (tail.Y < head.Y)
+		// 		{
+		// 			tail = Point.From(tail.X, head.Y);
+		// 			dir = 'D';
+		// 		}
+		// 		return true;
+		// 	}
+		// 	else if (dy > 1)
+		// 	{
+		// 		tail = dir switch
+		// 		{
+		// 			'R' => tail.Right,
+		// 			'L' => tail.Left,
+		// 			'U' => tail.Up,
+		// 			'D' => tail.Down,
+		// 			_ => throw new Exception()
+		// 		};
+		// 		if (tail.X > head.X)
+		// 		{
+		// 			tail = Point.From(head.X, tail.Y);
+		// 			dir = 'L';
+		// 		}
+		// 		else if (tail.X < head.X)
+		// 		{
+		// 			tail = Point.From(head.X, tail.Y);
+		// 			dir = 'R';
+		// 		}
+		// 		return true;
+		// 	}
+		// 	return false;
+		// }
+
+		protected override long Part2(string[] input)
+		{
+			var map = new CharMap();
+			var len = 10;
+			var rope = new Point[len];
+			for (var i = 0; i < len; i++)
+				rope[i] = Point.Origin;
+			// var head = Point.Origin;
+			// var tail = Point.Origin;
+
+			// L 3
+			// U 5
+			// R 2
+			// U 13			
+			map[Point.Origin] = '#';
+
+			foreach (var s in input)
+			{
+				var (dir0, n) = s.RxMatch("%c %d").Get<char, int>();
+				for (var i = 0; i < n; i++)
+				{
+					var dir = dir0;
+					rope[0] = Move(dir, rope[0]);
+					for (var j = 0; j < len-1; j++)
+					{
+						var headi = rope[j];
+						var taili = rope[j+1];
+						taili = Pull3(dir, headi, taili);
+						if (rope[j+1] == taili)
+							break;
+						rope[j+1] = taili;
+					}
+					var tail = rope[len-1];
+					map[tail] = '#';
+
+
+					// foreach (var p in rope)
+					// {
+					// 	if (map[p] != '#')
+					// 		map[p] = '.';
+					// }
+
+					// Console.WriteLine();
+					// Console.WriteLine(s);
+					// foreach (var ss in map.Render((p, ch) =>
+					// {
+					// 	for (var i = 0; i < len; i++)
+					// 	{
+					// 		if (rope[i] == p)
+					// 			return i == 0 ? 'H' : (char)('0' + i);
+					// 	}
+					// 	if (p == Point.Origin)
+					// 		return 's';
+					// 	return '.';
+					// }))
+					// {
+					// 	Console.WriteLine(ss);
+					// }
+
+				}
+
+				// foreach (var p in rope)
+				// {
+				// 	if (map[p] != '#')
+				// 		map[p] = '.';
+				// }
+
+				// Console.WriteLine();
+				// Console.WriteLine(s);
+				// foreach (var ss in map.Render((p, ch) =>
+				// {
+				// 	for (var i = 0; i < len; i++)
+				// 	{
+				// 		if (rope[i] == p)
+				// 			return i == 0 ? 'H' : (char)('0' + i);
+				// 	}
+				// 	if (p == Point.Origin)
+				// 		return 's';
+				// 	return '.';
+				// }))
+				// {
+				// 	Console.WriteLine(ss);
+				// }
+			}
+
+			// 4413 not right
+
+			// 6321 not right
+
+			return map.Count('#');
+		}
+
+
+
+	}
+}
--- AdventOfCode/Y2022/Day09/Puzzle09.cs
+++ AdventOfCode/Y2022/Day09/Puzzle09.cs
@@ -1,162 +1,117 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2022.Day09
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "Day 9";
+		public override string Name => "Rope Bridge";
 		public override int Year => 2022;
 		public override int Day => 9;
 
 		public void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-
-			//Run("test2").Part1(0).Part2(0);
-
-			//Run("input").Part1(0).Part2(0);
+			Run("test1").Part1(13).Part2(1);
+			Run("test2").Part2(36);
+			Run("input").Part1(5981).Part2(2352);
 		}
 
 		protected override long Part1(string[] input)
 		{
+			var head = Point.Origin;
+			var tail = Point.Origin;
 
+			var map = new CharMap();
+			foreach (var s in input)
+			{
+				var (dir, n) = s.RxMatch("%c %d").Get<char, int>();
+				while (n-- > 0)
+				{
+					head = Move(dir, head);
+					tail = Pull(head, tail);
+					map[tail] = '#';
+				}
+			}
 
-			return 0;
+			return map.Count('#');
 		}
 
 		protected override long Part2(string[] input)
 		{
+			var length = 10;
+			var rope = Enumerable.Range(0, length).Select(_ => Point.Origin).ToArray();
 
+			var map = new CharMap();
+			foreach (var s in input)
+			{
+				var (dir, n) = s.RxMatch("%c %d").Get<char, int>();
+				while (n-- > 0)
+				{
+					rope[0] = Move(dir, rope[0]);
+					for (var j = 1; j < length; j++)
+					{
+						rope[j] = Pull(rope[j-1], rope[j]);
+					}
+					map[rope.Last()] = '#';
+				}
+			}
 
-			return 0;
+			return map.Count('#');
 		}
 
+		private static Point Move(char dir, Point p) =>
+			dir switch
+			{
+				'R' => p.Right,
+				'L' => p.Left,
+				'U' => p.Up,
+				'D' => p.Down,
+				_ => throw new Exception()
+			};
 
-		/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-		internal class Thing
+		private static Point Pull(Point head, Point tail)
 		{
-			//private readonly 
-			public Thing(string[] lines)
-			{
-			}
+			var dx = head.X - tail.X;
+			var dy = head.Y - tail.Y;
+			var dxAbs = Math.Abs(dx);
+			var dyAbs = Math.Abs(dy);
+
+			// For nearby knots the robe isn't being pulled at all
+			if (dxAbs < 2 && dyAbs < 2)
+				return tail;
+
+			// Pure up/down, which may pull the robe
+			if (dxAbs == 0)
+				return dy switch
+				{
+					2 => tail.Down,
+					-2 => tail.Up,
+					_ => tail
+				};
+
+			// Pure left/right, which may pull the robe
+			if (dyAbs == 0)
+				return dx switch
+				{
+					2 => tail.Right,
+					-2 => tail.Left,
+					_ => tail
+				};
+
+			// Rope is now definitely being pulled diagonally
+			if (dx > 0 && dy > 0)
+				return tail.DiagonalDownRight;
+			if (dx > 0 && dy < 0)
+				return tail.DiagonalUpRight;
+			if (dx < 0 && dy < 0)
+				return tail.DiagonalUpLeft;
+			if (dx < 0 && dy > 0)
+				return tail.DiagonalDownLeft;
+
+			throw new Exception("Unhandled situation");
 		}
-
-		class SomeGraph : Graph<HashSet<uint>> { }
-
-		internal void Sample(string[] input)
-		{
-			{
-				var v = input.Select(int.Parse).ToArray();
-			}
-			{
-				var v = input[0].ToIntArray();
-			}
-			{
-				var things = input
-					.Skip(1)
-					.GroupByEmptyLine()
-					.Select(lines => new Thing(lines))
-					.ToMutableArray();
-			}
-			{
-				var map = new SparseMap<int>();
-				foreach (var s in input)
-				{
-					var (x1, y1, x2, y2) = s.RxMatch("%d,%d -> %d,%d").Get<int, int, int, int>();
-				}
-			}
-			{
-				var map = CharMap.FromArray(input);
-				var maze = new Maze(map)
-					.WithEntry(map.FirstOrDefault(c => c == '0')); // or Point.From(1, 1);
-				var dest = Point.From(2, 3);
-				var graph = Graph<char>.BuildUnitGraphFromMaze(maze);
-				var steps = graph.ShortestPathDijkstra(maze.Entry, dest);
-			}
-			{
-				var map = new CharMap('#');
-				var maze = new Maze(map).WithEntry(Point.From(1, 1));
-				var graph = SomeGraph.BuildUnitGraphFromMaze(maze);
-				var queue = new Queue<(SomeGraph.Vertex, uint, int)>();
-				queue.Enqueue((graph.Root, 0U, 0));
-				while (queue.Any())
-				{
-					var (node, found, steps) = queue.Dequeue();
-					if (node.Value.Contains(found))
-						continue;
-					node.Value.Add(found);
-					var ch = map[node.Pos];
-					if (char.IsDigit(ch))
-					{
-
-					}
-					foreach (var n in node.Edges.Keys.Where(n => !n.Value.Contains(found)))
-					{
-						queue.Enqueue((n, found, steps + 1));
-					}
-				}
-			}
-			{
-				var ship = new Pose(Point.Origin, Direction.Right);
-				foreach (var line in input)
-				{
-					var n = int.Parse(line.Substring(1));
-					switch (line[0])
-					{
-						case 'N': ship.MoveUp(n); break;
-						case 'S': ship.MoveDown(n); break;
-						case 'E': ship.MoveRight(n); break;
-						case 'W': ship.MoveLeft(n); break;
-						case 'L': ship.RotateLeft(n); break;
-						case 'R': ship.RotateRight(n); break;
-						case 'F': ship.Move(n); break;
-						default:
-							throw new Exception($"Unknown action in {line}");
-					}
-				}
-				var dist = ship.Point.ManhattanDistanceTo(Point.Origin);
-			}
-			{
-				var departure = int.Parse(input[0]);
-				var id = input[1]
-					.Replace(",x", "")
-					.Split(",")
-					.Select(int.Parse)
-					.Select(id => new
-					{
-						Id = id,
-						Time = id - departure % id
-					})
-					.OrderBy(x => x.Time)
-					.First();
-			}
-			{
-				var map = CharMatrix.FromArray(input);
-				for (var i = 0; i < 100; i++)
-				{
-					map = map.Transform((ch, adjacents) =>
-					{
-						var n = 0;
-						foreach (var c in adjacents)
-						{
-							if (c == '|' && ++n >= 3)
-								return '|';
-						}
-						return ch;
-					});
-				}
-			}
-		}
-
 	}
 }
