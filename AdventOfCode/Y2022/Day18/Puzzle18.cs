20f263ef6e8b376af187bacfbbdb2dafdb8e1476
Richard Flamsholt <richard@flamsholt.dk>
2022-12-18 11:04:34 +0100

    2022 Day 18: Boiling Boulders (raw)

--- AdventOfCode/Y2022/Day18/Puzzle18.cs
+++ AdventOfCode/Y2022/Day18/Puzzle18.cs
@@ -19,144 +19,337 @@ namespace AdventOfCode.Y2022.Day18
 
 		public void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-
-			//Run("test2").Part1(0).Part2(0);
-
-			//Run("input").Part1(0).Part2(0);
+			//Run("test1").Part1(10);
+		//	Run("test2").Part1(64).Part2(58);
+			Run("input").Part1(3576).Part2(0);
+			// 3402 not right
+			// 3358 too high
+			// 3307 too high
 		}
 
 		protected override long Part1(string[] input)
 		{
+			var cubes = input.Select(s => s.ToIntArray()).Select(c => new Point3D(c[0], c[1], c[2])).ToArray();
 
+			var sides = cubes.Length * 6;
+			for (var i = 0; i < cubes.Length; i++)
+			{
+				for (var j = i+1; j < cubes.Length; j++)
+				{
+					var (c1, c2) = (cubes[i], cubes[j]);
+					var dx = Math.Abs(c1.X - c2.X);
+					var dy = Math.Abs(c1.Y - c2.Y);
+					var dz = Math.Abs(c1.Z - c2.Z);
+					if (dx+dy+dz == 1)
+					{
+						//Console.WriteLine($"{c1} {c2}", c1, c2);
+						sides -= 2;
+					}
+				}
+			}
 
-			return 0;
+			return sides;
 		}
 
 		protected override long Part2(string[] input)
 		{
+			var cubes = input.Select(s => s.ToIntArray()).Select(c => new Point3D(c[0], c[1], c[2])).ToArray();
 
-
-			return 0;
-		}
-
-
-		/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-		internal class Thing
-		{
-			//private readonly 
-			public Thing(string[] lines)
+			var sides = cubes.Length * 6;
+			for (var i = 0; i < cubes.Length; i++)
 			{
-			}
-		}
-
-		class SomeGraph : Graph<HashSet<uint>> { }
-
-		internal void Sample(string[] input)
-		{
-			{
-				var v = input.Select(int.Parse).ToArray();
-			}
-			{
-				var v = input[0].ToIntArray();
-			}
-			{
-				var things = input
-					.Skip(1)
-					.GroupByEmptyLine()
-					.Select(lines => new Thing(lines))
-					.ToMutableArray();
-			}
-			{
-				var map = new SparseMap<int>();
-				foreach (var s in input)
+				for (var j = i+1; j < cubes.Length; j++)
 				{
-					var (x1, y1, x2, y2) = s.RxMatch("%d,%d -> %d,%d").Get<int, int, int, int>();
+					var (c1, c2) = (cubes[i], cubes[j]);
+					var dx = Math.Abs(c1.X - c2.X);
+					var dy = Math.Abs(c1.Y - c2.Y);
+					var dz = Math.Abs(c1.Z - c2.Z);
+					if (dx+dy+dz == 1)
+					{
+						//Console.WriteLine($"{c1} {c2}", c1, c2);
+						sides -= 2;
+					}
 				}
 			}
+
+			// find all embedded holes
+			var displacements = new[] { new Point3D(-1,0,0), new Point3D(1,0,0), new Point3D(0,-1,0), new Point3D(0,1,1), new Point3D(0,0,-1), new Point3D(0,0,1) };
+			//var holes = new HashSet<long>();
+			var singlehole = new HashSet<Point3D>();
+			var ends = new HashSet<Point3D>();
+			var edge = new HashSet<Point3D>();
+			var allcubes = new HashSet<Point3D>(cubes);
+			foreach (var c in cubes)
 			{
-				var map = CharMap.FromArray(input);
-				var maze = new Maze(map)
-					.WithEntry(map.FirstOrDefault(c => c == '0')); // or Point.From(1, 1);
-				var dest = Point.From(2, 3);
-				var graph = Graph<char>.BuildUnitGraphFromMaze(maze);
-				var steps = graph.ShortestPathDijkstra(maze.Entry, dest);
-			}
-			{
-				var map = new CharMap('#');
-				var maze = new Maze(map).WithEntry(Point.From(1, 1));
-				var graph = SomeGraph.BuildUnitGraphFromMaze(maze);
-				var queue = new Queue<(SomeGraph.Vertex, uint, int)>();
-				queue.Enqueue((graph.Root, 0U, 0));
-				while (queue.Any())
+				foreach (var d in displacements)
 				{
-					var (node, found, steps) = queue.Dequeue();
-					if (node.Value.Contains(found))
+					var space = c.Add(d);
+					// var neighbours = cubes.Count(x =>
+					// {
+					// 	var dx = Math.Abs(space.X - x.X);
+					// 	var dy = Math.Abs(space.Y - x.Y);
+					// 	var dz = Math.Abs(space.Z - x.Z);
+					// 	return dx+dy+dz == 1;
+					// });
+					if (allcubes.Contains(space))
 						continue;
-					node.Value.Add(found);
-					var ch = map[node.Pos];
-					if (char.IsDigit(ch))
-					{
 
-					}
-					foreach (var n in node.Edges.Keys.Where(n => !n.Value.Contains(found)))
+					var neighbourCubes = cubes.Where(x =>
 					{
-						queue.Enqueue((n, found, steps + 1));
+						var dx = Math.Abs(space.X - x.X);
+						var dy = Math.Abs(space.Y - x.Y);
+						var dz = Math.Abs(space.Z - x.Z);
+						return dx+dy+dz == 1;
+					}).ToArray();
+					var neighbours = neighbourCubes.Count();
+
+					if (neighbours == 6)
+					{
+						//var key = ((long)space.X << 16) + ((long)space.Y << 8) + space.Z;
+						//holes.Add(key);
+						singlehole.Add(space);
 					}
+					else if (neighbours == 5)
+					{
+						ends.Add(space);
+					}
+					if (neighbours > 1)
+						edge.Add(space);
 				}
 			}
+
+			var externals = sides - singlehole.Count() * 6;
+
+			var spaces = singlehole.ToArray();
+			var spaces2 = singlehole.ToArray().OrderBy(x => x.X).ThenBy(x => x.Y).ThenBy(x => x.Z);
+			var ends2 = ends.ToArray().OrderBy(x => x.X).ThenBy(x => x.Y).ThenBy(x => x.Z);
+			// foreach (var s in spaces2)
+			// 	Console.WriteLine($"space {s}");
+			// foreach (var s in ends2)
+			// 	Console.WriteLine($"end {s}");
+
+			var min = new Point3D(100, 100, 100);
+			var max = new Point3D(-100, -100, -100);
+			Console.WriteLine();
+			Console.WriteLine("x,y,z");
+			foreach (var c in cubes)
 			{
-				var ship = new Pose(Point.Origin, Direction.Right);
-				foreach (var line in input)
+				//Console.WriteLine($"{c.X},{c.Y},{c.Z}");
+				if (c.X < min.X)
+					min = min with { X = c.X };
+				if (c.Y < min.Y)
+					min = min with { Y = c.Y };
+				if (c.Z < min.Z)
+					min = min with { Z = c.Z };
+				if (c.X > max.X)
+					max = max with { X = c.X };
+				if (c.Y > max.Y)
+					max = max with { Y = c.Y };
+				if (c.Z > max.Z)
+					max = max with { Z = c.Z };
+			}
+			Console.WriteLine();
+
+			var world = new HashSet<Point3D>();
+			for (var x = min.X-2; x <= max.X+1; x++)
+			{
+				for (var y = min.Y-2; y <= max.Y+1; y++)
 				{
-					var n = int.Parse(line.Substring(1));
-					switch (line[0])
+					for (var z = min.Z-2; z <= max.Z+1; z++)
 					{
-						case 'N': ship.MoveUp(n); break;
-						case 'S': ship.MoveDown(n); break;
-						case 'E': ship.MoveRight(n); break;
-						case 'W': ship.MoveLeft(n); break;
-						case 'L': ship.RotateLeft(n); break;
-						case 'R': ship.RotateRight(n); break;
-						case 'F': ship.Move(n); break;
-						default:
-							throw new Exception($"Unknown action in {line}");
+						world.Add(new Point3D(x, y, z));
 					}
 				}
-				var dist = ship.Point.ManhattanDistanceTo(Point.Origin);
 			}
+
+			Console.WriteLine("all cubes");
+			foreach (var c in allcubes.OrderBy(x => x.X).ThenBy(x => x.Y).ThenBy(x => x.Z))
+				Console.WriteLine(c);
+			Console.WriteLine();
+
+			var outside = min.Add(new Point3D(-1, -1, -1));
+			var rim = new HashSet<Point3D>();
+			FindRim(outside);
+			void FindRim(Point3D p)
 			{
-				var departure = int.Parse(input[0]);
-				var id = input[1]
-					.Replace(",x", "")
-					.Split(",")
-					.Select(int.Parse)
-					.Select(id => new
+				if (p == new Point3D(4, 3, 11))
+					;
+				rim.Add(p);
+				var nexts = displacements
+					.Select(d => p.Add(d))
+					.Where(c =>
 					{
-						Id = id,
-						Time = id - departure % id
+						var wc = world.Contains(c);
+						var rc = rim.Contains(c);
+						var ac = allcubes.Contains(c);
+						return wc && !rc &&  !ac;
 					})
-					.OrderBy(x => x.Time)
-					.First();
-			}
+					.ToArray();
+					// .Where(c => world.Contains(c))
+					// .Where(c => !rim.Contains(c))
+					// .Where(c => !allcubes.Contains(c));
+				foreach (var next in nexts)
+					FindRim(next);
+			}			
+
+			var world1 = world.Count();
+			Console.WriteLine(world.Count());
+			RemoveEmptySpace(outside);
+			Console.WriteLine(world.Count());
+			Console.WriteLine(world1 - world.Count());
+			Console.WriteLine(rim.Count());
+			foreach (var c in allcubes)
+				world.Remove(c);
+			foreach (var c in singlehole)
+				world.Remove(c);
+			Console.WriteLine(world.Count());
+			foreach (var c in world.ToArray())
 			{
-				var map = CharMatrix.FromArray(input);
-				for (var i = 0; i < 100; i++)
+				var empties = displacements.Select(d => c.Add(d)).Count(c => world.Contains(c));
+				if (empties == 0)
 				{
-					map = map.Transform((ch, adjacents) =>
-					{
-						var n = 0;
-						foreach (var c in adjacents)
-						{
-							if (c == '|' && ++n >= 3)
-								return '|';
-						}
-						return ch;
-					});
+					// how?!
+					Console.WriteLine($"why empty at {c}");
 				}
 			}
+
+			// https://chart-studio.plotly.com/create/
+
+			Console.WriteLine();
+			foreach (var s in world)
+			{
+				Console.WriteLine($"{s.X},{s.Y},{s.Z}");
+			}			
+
+			void RemoveEmptySpace(Point3D p)
+			{
+				if (p == new Point3D(4, 3, 11))
+					;
+				world.Remove(p);
+				var nexts = displacements
+					.Select(d => p.Add(d))
+					.Where(c => world.Contains(c))
+					.Where(c => !allcubes.Contains(c))
+					.ToArray();
+				foreach (var next in nexts)
+					RemoveEmptySpace(next);
+			}
+
+
+			// var allspaces = new HashSet<Point3D>();
+			// for (var x = min.X+1; x < max.X; x++)
+			// {
+			// 	for (var y = min.Y+1; y < max.Y; y++)
+			// 	{
+			// 		for (var z = min.Z+1; z < max.Z; z++)
+			// 		{
+			// 			var p = new Point3D(x, y, z);
+			// 			if (cubes.Contains(p))
+			// 				continue;
+
+			// 			// var touches = displacements.Select(d => p.Add(d)).Any(c => allcubes.Contains(c));
+			// 			// if (!touches)
+			// 			// 	continue;
+			// 			var touchesSpace = displacements.Select(d => p.Add(d)).Any(c => holes.Contains(c));
+			// 			if (!touchesSpace)
+			// 				continue;
+
+			// 			allspaces.Add(p);
+			// 		}
+			// 	}
+			// }
+
+			// foreach (var s in allspaces)
+			// {
+			// 	Console.WriteLine($"{s.X},{s.Y},{s.Z}");
+			// }
+
+			// foreach (var h in holes)
+			// {
+			// 	var paths = displacements.Select(d => h.Add(d)).Where(c => !allcubes.Contains(c)).ToArray();
+			// 	if (paths.Length > 0)
+			// 	{
+			// 		Console.WriteLine($"{h.X},{h.Y},{h.Z}");
+			// 		foreach (var p in paths)
+			// 			Console.WriteLine($"{p.X},{p.Y},{p.Z}");
+			// 	}
+			// }			
+			
+			// for (var i = 0; i < spaces.Length; i++)
+			// {
+			// 	for (var j = i+1; j < spaces.Length; j++)
+			// 	{
+			// 		var (c1, c2) = (spaces[i], spaces[j]);
+			// 		var dx = Math.Abs(c1.X - c2.X);
+			// 		var dy = Math.Abs(c1.Y - c2.Y);
+			// 		var dz = Math.Abs(c1.Z - c2.Z);
+			// 		if (dx+dy+dz == 1)
+			// 		{
+			// 			//Console.WriteLine($"{c1} {c2}", c1, c2);
+			// 			externals += 2;
+			// 		}
+			// 	}
+			// }
+
+			var internalsides = 0;
+			// foreach (var e1 in ends)
+			// {
+			// 	var empty = displacements.Select(d => e1.Add(d)).Where(c => !allcubes.Contains(c)).ToArray();
+			// 	if (empty.Length == 1)
+			// 	{
+			// 		// possible internal path, follow it
+			// 		var from = e1;
+			// 		var p = empty.Single();
+			// 		Console.WriteLine($"Follow {from}");
+			// 		var seen = 2;
+			// 		while (true)
+			// 		{
+			// 			Console.WriteLine($"  Look into {p}");
+			// 			if (ends.Contains(p))
+			// 				break;
+			// 			var nexts = displacements
+			// 				.Select(d => p.Add(d))
+			// 				.Where(c => c != from)
+			// 				.Where(c => !allcubes.Contains(c))
+			// 				.ToArray();
+			// 			if (nexts.Length > 1)
+			// 			{
+			// 				// open space, stop
+			// 				seen = 0;
+			// 				break;
+			// 			}
+			// 			var next = nexts.Single();
+			// 			seen++;
+			// 			from = p;
+			// 			p = next;
+			// 		}
+			// 		if (seen > 0)
+			// 		{
+			// 			internalsides += seen * 4 + 2;
+			// 		}
+					
+			// 	}
+			// }
+
+			return externals - internalsides / 2;
 		}
 
+	}
+
+	public record Point3D(int X, int Y, int Z)
+	{
+		public override int GetHashCode()
+		{
+			return X * 1000000 + Y * 1000 + Z;
+		}
+		public override string ToString() => $"{X},{Y},{Z}";
+	}
+
+	internal static class Extensions
+	{
+		public static Point3D Add(this Point3D a, Point3D b)
+		{
+			return new Point3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+		}
 	}
 }
