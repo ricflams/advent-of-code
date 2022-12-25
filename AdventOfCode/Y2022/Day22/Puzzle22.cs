89ae0b6eeabfa0117e391516273ceaec16a2ea27
ricflams <richard@flamsholt.dk>
2022-12-26 00:29:49 +0100

    2022 Day 22: Monkey Map

--- AdventOfCode/Y2022/Day22/Puzzle22.cs
+++ AdventOfCode/Y2022/Day22/Puzzle22.cs
@@ -1,162 +1,264 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2022.Day22
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "Day 22";
+		public override string Name => "Monkey Map";
 		public override int Year => 2022;
 		public override int Day => 22;
 
 		public void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-
-			//Run("test2").Part1(0).Part2(0);
-
-			//Run("input").Part1(0).Part2(0);
+			Run("test1").Part1(6032).Part2(5031);
+			Run("input").Part1(66292).Part2(127012);
 		}
 
 		protected override long Part1(string[] input)
 		{
+			var map = CharMap.FromArray(input[0..^2], ' ');
+			var turns = input[^1].Replace("R",",R,").Replace("L", ",L,").Split(',');
 
+			var (w, h) = map.Size();
 
-			return 0;
+			var pose = new Pose(Point.Origin, Direction.Right);
+			while (map[pose.Point] == ' ')
+				pose.Move();
+			
+			foreach (var turn in turns)
+			{
+				if (turn == "R")
+					pose.TurnRight();
+				else if (turn == "L")
+					pose.TurnLeft();
+				else
+				{
+					var steps = int.Parse(turn);
+					while (map[PeekAhead().Point] == '.' && steps-- > 0)
+						pose = PeekAhead();
+				}
+			}
+
+			Pose PeekAhead()
+			{
+				var move = pose;
+				while (true)
+				{
+					var p = move.PeekAhead;
+					var (x, y) = (p.X, p.Y);
+					if (x >= w)
+						x = -1;
+					else if (x < 0)
+						x = w;
+					else if (y >= h)
+						y = -1;
+					else if (y < 0)
+						y = h;
+					else if (map[p] != ' ')
+						return Pose.From(x, y, move.Direction);
+					move = Pose.From(x, y, move.Direction);
+				}
+			}
+
+			// Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
+			var facing = pose.Direction switch
+			{
+				Direction.Right => 0,	
+				Direction.Down => 1,	
+				Direction.Left => 2,	
+				Direction.Up => 3,	
+				_ => throw new Exception()
+			};
+			var password = (pose.Point.Y + 1) * 1000 + (pose.Point.X + 1) * 4 + facing;
+
+			return password;
 		}
 
 		protected override long Part2(string[] input)
 		{
+			var map = CharMap.FromArray(input[0..^2]);
+			var turns = input[^1].Replace("R",",R,").Replace("L", ",L,").Split(',');
 
+			// Can only solve cubes with 3 inner corners, not 2 or 4
+			//
+			//           #            #   #
+			//    yes:  ##      no:   #   #
+			//          #            ###  #
+			//         ##             #  ###
 
-			return 0;
-		}
+			var (w, h) = map.Size();
+			bool OnMap(Point p) => map[p] == '.' || map[p] == '#';
 
+			// var pose = new Pose(Point.Origin, Direction.Right);
+			// while (map[pose.Point] != '.')
+			// 	pose.Move();
 
-		/////////////////////////////////////////////////////////////////////////////////////////////////////
+			var start = Pose.From(Point.Origin, Direction.Right);
+			while (map[start.Point] != '.')
+				start.Move();
 
-		internal class Thing
-		{
-			//private readonly 
-			public Thing(string[] lines)
+			// Find the 3 inner corners
+			var cornerInfo = InnerCorners().Skip(1).Take(3).ToArray();
+			IEnumerable<(Pose Corner, int Dist)> InnerCorners()
 			{
-			}
-		}
-
-		class SomeGraph : Graph<HashSet<uint>> { }
-
-		internal void Sample(string[] input)
-		{
-			{
-				var v = input.Select(int.Parse).ToArray();
-			}
-			{
-				var v = input[0].ToIntArray();
-			}
-			{
-				var things = input
-					.Skip(1)
-					.GroupByEmptyLine()
-					.Select(lines => new Thing(lines))
-					.ToMutableArray();
-			}
-			{
-				var map = new SparseMap<int>();
-				foreach (var s in input)
+				var p = start.Copy();
+				var dist = 0;
+				while (true)
 				{
-					var (x1, y1, x2, y2) = s.RxMatch("%d,%d -> %d,%d").Get<int, int, int, int>();
-				}
-			}
-			{
-				var map = CharMap.FromArray(input);
-				var maze = new Maze(map)
-					.WithEntry(map.FirstOrDefault(c => c == '0')); // or Point.From(1, 1);
-				var dest = Point.From(2, 3);
-				var graph = Graph<char>.BuildUnitGraphFromMaze(maze);
-				var steps = graph.ShortestPathDijkstra(maze.Entry, dest);
-			}
-			{
-				var map = new CharMap('#');
-				var maze = new Maze(map).WithEntry(Point.From(1, 1));
-				var graph = SomeGraph.BuildUnitGraphFromMaze(maze);
-				var queue = new Queue<(SomeGraph.Vertex, uint, int)>();
-				queue.Enqueue((graph.Root, 0U, 0));
-				while (queue.Any())
-				{
-					var (node, found, steps) = queue.Dequeue();
-					if (node.Value.Contains(found))
-						continue;
-					node.Value.Add(found);
-					var ch = map[node.Pos];
-					if (char.IsDigit(ch))
+					if (OnMap(p.PeekAhead))
+						p.Move();
+					else
+						p.TurnRight();
+					dist++;
+					if (OnMap(p.PeekLeft)) // just ahead of an inner corner
 					{
-
-					}
-					foreach (var n in node.Edges.Keys.Where(n => !n.Value.Contains(found)))
-					{
-						queue.Enqueue((n, found, steps + 1));
+						yield return (p.Copy(), dist);
+						p.TurnLeft();
+						p.Move();
+						dist = 0;
 					}
 				}
 			}
-			{
-				var ship = new Pose(Point.Origin, Direction.Right);
-				foreach (var line in input)
+
+			// Create corner-mappers for the 3 inner corners
+			var corners = cornerInfo
+				.Select(c =>
 				{
-					var n = int.Parse(line.Substring(1));
-					switch (line[0])
+					// They all look like this, but in all kind of directions.
+					// Here, > marks the corner's Pose.
+					//
+					//  #
+					//  #
+					//  A
+					//  vB##
+					//
+					// The two tracer-poses, A and B, goes in each their direction:
+					// A points "left" and moves "backwards", counter-clockwise
+					// B points "back" and moves "left", clockwise
+					var p = c.Corner;
+					var corner = new Corner
 					{
-						case 'N': ship.MoveUp(n); break;
-						case 'S': ship.MoveDown(n); break;
-						case 'E': ship.MoveRight(n); break;
-						case 'W': ship.MoveLeft(n); break;
-						case 'L': ship.RotateLeft(n); break;
-						case 'R': ship.RotateRight(n); break;
-						case 'F': ship.Move(n); break;
-						default:
-							throw new Exception($"Unknown action in {line}");
-					}
-				}
-				var dist = ship.Point.ManhattanDistanceTo(Point.Origin);
-			}
-			{
-				var departure = int.Parse(input[0]);
-				var id = input[1]
-					.Replace(",x", "")
-					.Split(",")
-					.Select(int.Parse)
-					.Select(id => new
-					{
-						Id = id,
-						Time = id - departure % id
-					})
-					.OrderBy(x => x.Time)
-					.First();
-			}
-			{
-				var map = CharMatrix.FromArray(input);
-				for (var i = 0; i < 100; i++)
-				{
-					map = map.Transform((ch, adjacents) =>
-					{
-						var n = 0;
-						foreach (var c in adjacents)
+						Distance = c.Dist,
+						M1 = new Corner.Mapper
 						{
-							if (c == '|' && ++n >= 3)
-								return '|';
+							IsClockwise = false,
+							Pose = Pose.From(p.PeekBehind, p.Direction.TurnLeft()),
+							Dir = p.Direction.TurnAround(),
+						},
+						M2 = new Corner.Mapper
+						{
+							IsClockwise = true,
+							Pose = Pose.From(p.PeekLeft, p.Direction.TurnAround()),
+							Dir = p.Direction.TurnLeft()
 						}
-						return ch;
-					});
+					};					
+					return corner;
+				})
+				.ToArray();
+
+			// Determine the "width" of each mapper. The full span of mapped points going
+			// out from each inner corner is the distance between it and its two neighbors
+			// minus the distance between them; ie each mapper's length is (A+B-C)/2
+			for (var i = 0; i < 3; i++)
+			{
+				var n1 = corners[i].Distance;
+				var n2 = corners[(i+1)%3].Distance;
+				var n3 = corners[(i+2)%3].Distance;
+				corners[i].Length = (n1 + n2 - n3) / 2;
+			}
+
+			// Now build the edge-mappings. Move the mappers from their starting
+			// points to create mappings from each edge-pose in both directions.
+			var mappings = new Dictionary<string, Pose>();
+			foreach (var mapper in corners)
+			{
+				for (var i = 0; i < mapper.Length; i++)				
+				{
+					var m1back = mapper.M1.Pose.Copy(); m1back.TurnAround();
+					var m2back = mapper.M2.Pose.Copy(); m2back.TurnAround();
+					AddMapping(mapper.M1, m2back);
+					AddMapping(mapper.M2, m1back);
+				}
+
+				void AddMapping(Corner.Mapper m, Pose dest)
+				{
+					mappings[m.Pose.ToString()] = dest;
+					m.Pose.Move(m.Dir);
+					if (!OnMap(m.Pose.Point))
+					{
+						if (m.IsClockwise) // for clockwise, always turn right
+						{
+							m.Pose.Move(m.Dir.TurnAround());
+							m.Pose.TurnRight();
+							m.Dir = m.Dir.TurnRight();
+						}
+						else // for counter-clockwise, always turn left
+						{
+							m.Pose.Move(m.Dir.TurnAround());
+							m.Pose.TurnLeft();
+							m.Dir = m.Dir.TurnLeft();
+						}
+					}
 				}
 			}
+
+			// Finally traverse the cube
+			var pose = start;
+			foreach (var turn in turns)
+			{
+				if (turn == "R")
+					pose.TurnRight();
+				else if (turn == "L")
+					pose.TurnLeft();
+				else
+				{
+					var steps = int.Parse(turn);
+					while (steps-- > 0)
+					{
+						var next = pose.Copy();
+						if (mappings.TryGetValue(pose.ToString(), out var moveTo))
+							next = moveTo.Copy();
+						else
+							next.Move();
+						if (map[next.Point] == '#')
+							break;
+						pose = next;
+					}
+				}
+			}
+
+			// Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
+			var facing = pose.Direction switch
+			{
+				Direction.Right => 0,	
+				Direction.Down => 1,	
+				Direction.Left => 2,	
+				Direction.Up => 3,	
+				_ => throw new Exception()
+			};
+			var password = (pose.Point.Y + 1) * 1000 + (pose.Point.X + 1) * 4 + facing;
+
+			return password;
 		}
 
+		private class Corner
+		{
+			public int Length;
+			public Mapper M1;
+			public Mapper M2;
+			public int Distance;
+
+			public class Mapper
+			{
+				public bool IsClockwise;
+				public Pose Pose;
+				public Direction Dir;
+			}
+		}
 	}
 }
---
+++ AdventOfCode/Y2022/Day22/Puzzle22.raw.cs
@@ -0,0 +1,330 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using AdventOfCode.Helpers.String;
+
+namespace AdventOfCode.Y2022.Day22.Raw
+{
+	internal class Puzzle : Puzzle<long, long>
+	{
+		public static Puzzle Instance = new();
+		public override string Name => "Day 22";
+		public override int Year => 2022;
+		public override int Day => 22;
+
+		public void Run()
+		{
+			Run("test1").Part1(6032).Part2(5031);
+			Run("input").Part1(66292).Part2(127012);
+		}
+
+		protected override long Part1(string[] input)
+		{
+			var map = CharMap.FromArray(input[0..^2], ' ');
+			var turns = input[^1].Replace("R",",R,").Replace("L", ",L,").Split(',');
+
+			var (w, h) = map.Size();
+
+		//	map.ConsoleWrite();
+
+			var pose = new Pose(Point.Origin, Direction.Right);
+			while (map[pose.Point] != '.')
+				pose.Move();
+			
+	//		Draw();
+			foreach (var turn in turns)
+			{
+				//Console.WriteLine(turn);
+				if (turn == "R")
+					pose.TurnRight();
+				else if (turn == "L")
+					pose.TurnLeft();
+				else
+				{
+					var steps = int.Parse(turn);
+					while (map[PeekAhead2().Point] == '.' && steps-- > 0)
+						Move();
+				}
+		//		Draw();				
+			}
+
+			Pose PeekAhead2()
+			{
+				Check();
+				var move = pose;
+				while (true)
+				{
+					var p = move.PeekAhead;
+					var (x, y) = (p.X, p.Y);
+					if (x >= w)
+						x = -1;
+					else if (x < 0)
+						x = w;
+					else if (y >= h)
+						y = -1;
+					else if (y < 0)
+						y = h;
+					else if (map[p] != ' ')
+						return Pose.From(x, y, move.Direction);
+					move = Pose.From(x, y, move.Direction);
+				}
+			}
+
+			void Check()
+			{
+				if (pose.Point.X < 0 || pose.Point.X >= w)
+					throw new Exception();
+				if (pose.Point.Y < 0 || pose.Point.Y >= h)
+					throw new Exception();
+			
+			}
+			void Move()
+			{
+				pose = PeekAhead2();
+				Draw();
+			}
+
+			void Draw()
+			{
+				// var lines = map.Render((p, ch) => p == pose.Point ? pose.Direction.AsChar() : ch).ToArray();
+				// var from = Math.Max(0, pose.Point.Y - 2);
+				// var to = Math.Min(h-1, pose.Point.Y + 2);
+				// for(var y = from; y <= to; y++)
+				// {
+				// 	Console.WriteLine(lines[y]);
+				// }
+				// Console.WriteLine();
+			}
+
+			// Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
+			var facing = pose.Direction switch
+			{
+				Direction.Right => 0,	
+				Direction.Down => 1,	
+				Direction.Left => 2,	
+				Direction.Up => 3,	
+				_ => throw new Exception()
+			};
+			var password = (pose.Point.Y + 1) * 1000 + (pose.Point.X + 1) * 4 + facing;
+
+			return password;
+		}
+
+		protected override long Part2(string[] input)
+		{
+			var map = CharMap.FromArray(input[0..^2]);
+			var turns = input[^1].Replace("R",",R,").Replace("L", ",L,").Split(',');
+
+			var (w, h) = map.Size();
+
+			var innerCorners = new List<Point>();
+			for (var x = 0; x < w-1; x++)
+			{
+				for (var y = 0; y < h-1; y++)
+				{
+					if (On(x, y) + On(x+1, y) + On(x, y+1) + On(x+1, y+1) == 3)
+					{
+						var p = Point.From(x, y);
+						innerCorners.Add(p);
+						//Console.WriteLine(p);
+					}
+					int On(int x, int y) => OnMap(x, y) ? 1 : 0;
+				}
+			}
+			bool OnMap(int x, int y) => map[x, y] == '.' || map[x, y] == '#';
+			bool OnMap2(Point p) => map[p] == '.' || map[p] == '#';
+
+			var mappings = new Dictionary<string, Pose>();
+
+			var mappers = innerCorners
+				.Select(corner =>
+				{
+					//  A##B
+					// ######
+					//  D##C
+					Pose p1, p2; // p1 goes counter-clockwise ccw, p2 goes clockwise
+					Direction d1, d2;
+					if (!OnMap2(corner)) // A
+					{
+						p1 = Pose.From(corner.Down, Direction.Up); d1 = Direction.Left;
+						p2 = Pose.From(corner.Right, Direction.Left); d2 = Direction.Up;
+					}
+					else if (!OnMap2(corner.Right)) // B
+					{
+						p1 = Pose.From(corner, Direction.Right); d1 = Direction.Up;
+						p2 = Pose.From(corner.DiagonalDownRight, Direction.Up); d2 = Direction.Right;
+					}
+					else if (!OnMap2(corner.DiagonalDownRight)) // C
+					{
+						p1 = Pose.From(corner.Right, Direction.Down); d1 = Direction.Right;
+						p2 = Pose.From(corner.Down, Direction.Right); d2 = Direction.Down;
+					}
+					else if (!OnMap2(corner.Down)) // D
+					{
+						p1 = Pose.From(corner.DiagonalDownRight, Direction.Left); d1 = Direction.Down;
+						p2 = Pose.From(corner, Direction.Down); d2 = Direction.Left;
+					}
+					else
+						throw new Exception();
+					return new MapperPair
+					{
+						M1 = new Mapper
+						{
+							IsClockwise = false,
+							Pose = p1,
+							Dir = d1
+						},
+						M2 = new Mapper
+						{
+							IsClockwise = true,
+							Pose = p2,
+							Dir = d2
+						}
+					};
+				})
+				.ToArray();
+
+			if (w > 100)
+			{
+				mappers[0].Length = 200;
+				mappers[1].Length = 50;
+				mappers[2].Length = 100;
+			}
+			else
+			{
+				mappers[0].Length = 8;
+				mappers[1].Length = 12;
+				mappers[2].Length = 8;
+			}
+
+			foreach (var mapper in mappers)
+			{
+				for (var i = 0; i < mapper.Length; i++)				
+				{
+					var m1copy = mapper.M1.Pose.Copy(); m1copy.TurnAround();
+					var m2copy = mapper.M2.Pose.Copy(); m2copy.TurnAround();
+					MoveIt(mapper.M1, m2copy);
+					MoveIt(mapper.M2, m1copy);
+				}
+		//		DrawMappings();
+			}
+
+			bool MoveIt(Mapper m, Pose dest)
+			{
+				mappings[m.Pose.ToString()] = dest;
+				m.Pose.Move(m.Dir);
+				if (!OnMap2(m.Pose.Point))
+				{
+					if (m.IsClockwise) // for clockwise, always turn right
+					{
+						m.Pose.Move(m.Dir.TurnAround(), 1);
+						m.Pose.TurnRight();
+						m.Dir = m.Dir.TurnRight();
+					}
+					else // for counter-clockwise, always turn left
+					{
+						m.Pose.Move(m.Dir.TurnAround(), 1);
+						m.Pose.TurnLeft();
+						m.Dir = m.Dir.TurnLeft();
+					}
+				}
+				return true;
+			}
+
+			void DrawMappings()
+			{
+				map.ConsoleWrite((p, ch) =>
+				{
+					var ps = p.ToString();
+					var mapping = mappings.Keys.Where(k => k[..^1] == ps).ToArray();
+					return mapping.Length == 0
+						? ch
+						: mapping.Length == 1
+							? mapping[0][^1]
+							: (char)('0'+mapping.Length);
+				});
+				Console.WriteLine();
+			}
+
+
+	//		DrawMappings();
+
+			var pose = new Pose(Point.Origin, Direction.Right);
+			while (map[pose.Point] != '.')
+				pose.Move();
+			
+			//Draw();
+			foreach (var turn in turns)
+			{
+				//Console.WriteLine(turn);
+				if (turn == "R")
+					pose.TurnRight();
+				else if (turn == "L")
+					pose.TurnLeft();
+				else
+				{
+					var steps = int.Parse(turn);
+					while (steps-- > 0)
+					{
+						var next = pose.Copy();
+						if (mappings.TryGetValue(pose.ToString(), out var moveTo))
+						{
+							next = moveTo.Copy();
+						}
+						else
+						{
+							next.Move();
+						}
+						if (map[next.Point] == '#')
+							break;
+						pose = next;
+					}
+				}
+			}
+
+
+			void Draw()
+			{
+				var lines = map.Render((p, ch) => p == pose.Point ? pose.Direction.AsChar() : ch).ToArray();
+				var from = Math.Max(0, pose.Point.Y - 2);
+				var to = Math.Min(h-1, pose.Point.Y + 2);
+				for(var y = from; y <= to; y++)
+				{
+					Console.WriteLine(lines[y]);
+				}
+				Console.WriteLine();
+			}
+
+			// Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
+			var facing = pose.Direction switch
+			{
+				Direction.Right => 0,	
+				Direction.Down => 1,	
+				Direction.Left => 2,	
+				Direction.Up => 3,	
+				_ => throw new Exception()
+			};
+			var password = (pose.Point.Y + 1) * 1000 + (pose.Point.X + 1) * 4 + facing;
+
+			return password;
+		}
+		private class MapperPair
+		{
+			public int Length;
+			public Mapper M1;
+			public Mapper M2;
+		}
+		private class Mapper
+		{
+			public bool IsClockwise;
+			public Pose Pose;
+			public Direction Dir;
+		}
+	}
+
+}
