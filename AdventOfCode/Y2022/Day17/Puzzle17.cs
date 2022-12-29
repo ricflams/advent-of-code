4e8df76a8698a7b89540f1de46886f3e87ea7724
ricflams <richard@flamsholt.dk>
2022-12-29 23:18:58 +0100

    2022 Day 17: Pyroclastic Flow

--- AdventOfCode/Y2022/Day17/Puzzle17.cs
+++ AdventOfCode/Y2022/Day17/Puzzle17.cs
@@ -1,184 +1,118 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2022.Day17
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "Day 17";
+		public override string Name => "Pyroclastic Flow";
 		public override int Year => 2022;
 		public override int Day => 17;
 
 		public void Run()
 		{
-			Run("test1").Part1(3068);// TODO .Part2(1514285714288);
+			Run("test1").Part1(3068).Part2(1514285714288);
+			Run("test9").Part1(3227).Part2(1597714285698);
 			Run("input").Part1(3133).Part2(1547953216393);
 		}
 
-		private readonly string[][] Rocks = new string[][]
-		{
-			new string[]{ "####" },
-			new string[]{ ".#.", "###", ".#." },
-			new string[]{ "..#", "..#", "###" },
-			new string[]{ "#", "#", "#", "#" },
-			new string[]{ "##", "##"}
-		};
-
 		protected override long Part1(string[] input)
 		{
-			var jets = input[0];
-
-			var width = 7;
-			var top = 0;
-
-			var map = new CharMap('.');
-			for (var x = 1; x < width+1; x++)
-				map[x][0] = '-';
-			map[0][0] = map[width+1][0] = '+';			
-
-			void FillEmptyRow(int y)
-			{
-				for (var x = 1; x < width+1; x++)
-					map[x][y] = '.';
-				map[0][y] = map[width+1][y] = '|';
-			}
-
-			var ip = 0;
+			var cave = new Cave(input);
 			for (var j = 0; j < 2022; j++)
 			{
-				var rock = Rocks[j % Rocks.Length];
-				for (var i = 0; i < 3 + rock.Length; i++)
-				{
-					FillEmptyRow(top - 1 - i);
-				}
-
-				var w = rock[0].Length;
-				var x = 2 + 1; // [0] is wall
-				var y = top - (3 + rock.Length);
-
-				while (true)
-				{
-					var jet = jets[ip++ % jets.Length];
-					if (jet == '<')
-					{
-						if (CanMove(rock, x-1, y))
-							x--;
-					}
-					else
-					{
-						if (CanMove(rock, x+1, y))
-							x++;
-					}
-					if (!CanMove(rock, x, y+1))
-						break;
-					y++;
-				}
-				Draw(rock, x, y);
-				if (y < top)
-					top = y;
+				cave.DropRock();
 			}
-
-			bool CanMove(string[] rock, int x0, int y0)
-			{
-				var w = rock[0].Length;
-				var h = rock.Length;
-				for (var x = 0; x < w; x++)
-				{
-					for (var y = 0; y < h; y++)
-					{
-						if (rock[y][x] == '#' && map[x0+x][y0+y] != '.')
-							return false;
-					}
-				}
-				return true;
-			}
-
-			void Draw(string[] rock, int x0, int y0)
-			{
-				var w = rock[0].Length;
-				var h = rock.Length;
-				for (var x = 0; x < w; x++)
-				{
-					for (var y = 0; y < h; y++)
-					{
-						if (rock[y][x] == '#')
-							map[x0+x][y0+y] = '#';
-					}
-				}
-			}
-
-			return -top;
+			return cave.Height;
 		}
 
 		protected override long Part2(string[] input)
 		{
-			var jets = input[0];
+			var cave = new Cave(input);
 
-			var width = 7;
-			var top = 0;
+			var heights = new List<int>();
+			var cycles = new Dictionary<string, (int,int)>();
 
-			var map = new CharMap('.');
-			for (var x = 1; x < width+1; x++)
-				map[x][0] = '-';
-			map[0][0] = map[width+1][0] = '+';			
-
-			void FillEmptyRow(int y)
+			var fullHeight = 0L;
+			while (fullHeight == 0)
 			{
-				for (var x = 1; x < width+1; x++)
-					map[x][y] = '.';
-				map[0][y] = map[width+1][y] = '|';
+				// Remember the height at each drop for easy calculation of remaining height at the end
+				heights.Add(cave.Height);
+
+				// Drop a rock and check if we end up in a cycle, looking at the exact same
+				// place in jets and rocks and with a cycle-time (delta) exactly like last time
+				cave.DropRock((drop, jet, rock) =>
+				{
+					var delta = -1;
+					var key = $"{jet}-{rock}";
+					if (cycles.TryGetValue(key, out var lastseen))
+					{
+						var (drop0, delta0) = lastseen;
+						delta = heights.Last() - heights[drop0];
+						if (delta == delta0) // it's a cycle
+						{
+							var rocks = 1000000000000 - drop0;
+							var rocksPerCycle = drop - drop0;
+							var fullCycles = rocks / rocksPerCycle;
+							var remainingRocks = (int)(rocks % rocksPerCycle);
+							var remainingHeight = heights[drop0 + remainingRocks];
+							fullHeight = fullCycles * delta + remainingHeight;
+							return true;	
+						}
+					}
+					cycles[key] = (drop, delta);
+					return false;
+				});
+			}
+			return fullHeight;
+		}
+
+		private class Cave : CharMap
+		{
+			public Cave(string[] input) : base('.')
+			{
+				Jets = input[0];
+
+				for (var x = 1; x < Width+1; x++)
+					this[x][0] = '-';
 			}
 
-			var ip = 0L;
-			var heights = new List<int>();
+			public const int Width = 7;
+			public readonly string Jets;
+			public int Height => -_top;
 
-			var delta0 = -1;
-			var j0 = 0;
+			private int _top = 0;
+			private int _jet = 0;
+			private int _drop = 0;
 
-			for (var j = 0;; j++)
+			public void FillEmptyRow(int y)
 			{
-				heights.Add(-top);
+				for (var x = 1; x < Width+1; x++)
+					this[x][y] = '.';
+				this[0][y] = this[Width+1][y] = '|';
+			}
 
-				var rock = Rocks[j % Rocks.Length];
+			public void DropRock(Func<int, int, int, bool> EavesDrop = null)
+			{
+				var rock = Rocks[_drop % Rocks.Length];
 				for (var i = 0; i < 3 + rock.Length; i++)
 				{
-					FillEmptyRow(top - 1 - i);
+					FillEmptyRow(_top - 1 - i);
 				}
 
 				var w = rock[0].Length;
 				var x = 2 + 1; // [0] is wall
-				var y = top - (3 + rock.Length);
+				var y = _top - (3 + rock.Length);
 
 				while (true)
 				{
-					if (ip % jets.Length == 0 && j % Rocks.Length == 0)
-					{
-						var delta = heights.Last() - heights[j0];
-						if (delta == delta0)
-						{
-							var rocks = 1000000000000 - j0;
-							var rocksPerCycle = j - j0;
-							var fullCycles = rocks / rocksPerCycle;
-
-							var remainingRocks = (int)(rocks % rocksPerCycle);
-							var remainingHeight = heights[j0 + remainingRocks];
-
-							var fullHeight = fullCycles * delta + remainingHeight;
-							return fullHeight;					
-						}
-						delta0 = delta;
-						j0 = j;
-					}
-
-					var jet = jets[(int)(ip++ % jets.Length)];
+					if (EavesDrop?.Invoke(_drop, _jet % Jets.Length, _drop % Rocks.Length) ?? false)
+						return;
+					var jet = Jets[_jet++ % Jets.Length];
 					if (jet == '<')
 					{
 						if (CanMove(rock, x-1, y))
@@ -194,11 +128,13 @@ namespace AdventOfCode.Y2022.Day17
 					y++;
 				}
 				Draw(rock, x, y);
-				if (y < top)
-					top = y;
-		 	}
+				if (y < _top)
+					_top = y;
 
-			bool CanMove(string[] rock, int x0, int y0)
+				_drop++;
+			}
+
+			public bool CanMove(string[] rock, int x0, int y0)
 			{
 				var w = rock[0].Length;
 				var h = rock.Length;
@@ -206,14 +142,14 @@ namespace AdventOfCode.Y2022.Day17
 				{
 					for (var y = 0; y < h; y++)
 					{
-						if (rock[y][x] == '#' && map[x0+x][y0+y] != '.')
+						if (rock[y][x] == '#' && this[x0+x][y0+y] != '.')
 							return false;
 					}
 				}
 				return true;
 			}
 
-			void Draw(string[] rock, int x0, int y0)
+			public void Draw(string[] rock, int x0, int y0)
 			{
 				var w = rock[0].Length;
 				var h = rock.Length;
@@ -222,10 +158,42 @@ namespace AdventOfCode.Y2022.Day17
 					for (var y = 0; y < h; y++)
 					{
 						if (rock[y][x] == '#')
-							map[x0+x][y0+y] = '#';
+							this[x0+x][y0+y] = '#';
 					}
 				}
-			}
+			}			
+
+			private readonly string[][] Rocks = new string[][]
+			{
+				new []
+				{
+					"####"
+				},
+				new []
+				{
+					".#.",
+					"###",
+					".#."
+				},
+				new []
+				{
+					"..#",
+					"..#",
+					"###"
+				},
+				new []
+				{
+					"#",
+					"#",
+					"#",
+					"#"
+				},
+				new []
+				{
+					"##",
+					"##"
+				}
+			};
 		}
 	}
 }
