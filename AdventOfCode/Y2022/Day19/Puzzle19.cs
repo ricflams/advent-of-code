fd59e2acd6a215763fe6d2c66b850b963581ab32
Richard Flamsholt <richard@flamsholt.dk>
2023-01-02 21:39:49 +0100

    2022 Day 19: Not Enough Minerals

--- AdventOfCode/Y2022/Day19/Puzzle19.cs
+++ AdventOfCode/Y2022/Day19/Puzzle19.cs
@@ -1,258 +1,188 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2022.Day19
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "Day 19";
+		public override string Name => "Not Enough Minerals";
 		public override int Year => 2022;
 		public override int Day => 19;
 
 		public void Run()
 		{
-//			Run("test1").Part1(33); // no part 2
-//			Run("test9").Part1(1346).Part2(7644);
-			Run("input").Part1(1466);//.Part2(8250);
-		}
-
-		private const int Ore = 0;
-		private const int Clay = 1;
-		private const int Obs = 2;
-		private const int Geo = 3;
-		// private enum Material
-		// {
-		// 	Ore = 0, Clay, Obs, Geo
-		// }
-		private record RobotDef(int[] Costs);
-		private class Blueprint
-		{
-			public RobotDef[] Defs;
-			public int ValueOf(int[] m)
-			{
-				var a = Defs[0].Costs[0];
-				var b = Defs[1].Costs[0];
-				var c = Defs[2].Costs[0] + Defs[2].Costs[1] * b;
-				var d = Defs[3].Costs[0] + Defs[3].Costs[2] * c;
-				var value = a*m[0] + b*m[1] + c*m[2] + d*m[3];
-				return value;
-			}
+			Run("test1").Part1(33); // no part 2
+			Run("test9").Part1(1346).Part2(7644);
+			Run("input").Part1(1466).Part2(8250);
 		}
 
 		protected override long Part1(string[] input)
 		{
-			// Blueprint 16: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 11 clay. Each geode robot costs 3 ore and 15 obsidian.
-			var blueprints = input
-				.Select(s =>
-				{
-					var (_, a, b, c, d, e, f) = s.RxMatch("Blueprint %d: Each ore robot costs %d ore. Each clay robot costs %d ore. Each obsidian robot costs %d ore and %d clay. Each geode robot costs %d ore and %d obsidian.").Get<int, int, int, int, int, int, int>();
-					return new Blueprint
-					{
-						Defs = new RobotDef[]
-						{
-							new RobotDef(new [] { a, 0, 0, 0 }),
-							new RobotDef(new [] { b, 0, 0, 0 }),
-							new RobotDef(new [] { c, d, 0, 0 }),
-							new RobotDef(new [] { e, 0, f, 0 })
-						}
-					};
-				})
-				.ToArray();
-
-			var sum = blueprints
-				.Select((b, idx) => FindMaxGeodesOpened(b, idx, 24) * (idx+1))
+			var sum = input
+				.Select(Blueprint.Parse)
+				.Select((b, idx) => b.FindMaxGeodesOpened(24) * (idx+1))
 				.Sum();
-
 			return sum;
 		}
 
-
-
 		protected override long Part2(string[] input)
 		{
-			// Blueprint 16: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 11 clay. Each geode robot costs 3 ore and 15 obsidian.
-			var blueprints = input
+			var prod = input
 				.Take(3)
-				.Select(s =>
-				{
-					var (_, a, b, c, d, e, f) = s.RxMatch("Blueprint %d: Each ore robot costs %d ore. Each clay robot costs %d ore. Each obsidian robot costs %d ore and %d clay. Each geode robot costs %d ore and %d obsidian.").Get<int, int, int, int, int, int, int>();
-					return new Blueprint
-					{
-						Defs = new RobotDef[]
-						{
-							new RobotDef(new [] { a, 0, 0, 0 }),
-							new RobotDef(new [] { b, 0, 0, 0 }),
-							new RobotDef(new [] { c, d, 0, 0 }),
-							new RobotDef(new [] { e, 0, f, 0 })
-						}
-					};
-				})
-				.ToArray();
-
-			var prod = blueprints
-				.Select((b, idx) => (int)FindMaxGeodesOpened(b, idx,32))
+				.Select(Blueprint.Parse)
+				.Select(b => b.FindMaxGeodesOpened(32))
 				.Prod();
 			return prod;
 		}		
 
-		private static long FindMaxGeodesOpened(Blueprint bp, int index, int minutes)
+		private class Blueprint
 		{
-			var seen = new Dictionary<long, List<int[]>>();
-			var seenhighest = new Dictionary<int, int[]>();
-			var seenvalue= new Dictionary<int, int>();
-			var seenrobots = new Dictionary<long, int>();
-			var seengeodes = new Dictionary<long, int>();
-			var statesseen = new HashSet<long>();
+			private record RobotDef(int[] Costs) {};
+			private RobotDef[] Defs;
+			private const int Ore = 0;
+			private const int Clay = 1;
+			private const int Obs = 2;
+			private const int Geo = 3;
 
-			long Key(int[] x) {
-				var x0 = x[0]<<21;
-				var x1 = x[1]<<14;
-				var x2 = x[2]<<7;
-				var x3 = x[3];
-				var xx = x0 + x1 + x2 + x3;
-				var xxx = (long)xx;
-				return xxx;//(long)(x0 + x[1]<<16 + x[2]<<8 + x[3]);
-			}
-			long State(int[] materials, int[] robots) => (Key(materials) << 28) + Key(robots);
+			private int MaxOreCost;
+			private int MaxClayCost;
+			private int MaxObsCost;
 
-			var highestgeodes = new Dictionary<int, int>();
-
-			var materials0 = new[] { 0, 0, 0, 0}; // 1 ore
-			var robots0 = new[] { 1, 0, 0, 0}; // 1 ore-producing robot
-			var state0 = State(materials0, robots0);
-
-			var N = 4;
-
-			var maxtime = 0;
-			var maxproduced = 0L;
-			var queue = new PriorityQueue<(long State, int[] Materials, int[] Robots, int Time), int>();
-			queue.Enqueue((state0, materials0, robots0, 0), 0);
-			while (queue.TryDequeue(out var item, out var _))
+			public static Blueprint Parse(string s)
 			{
-				var (state, materials, robots, time) = item;
-				//Console.WriteLine($"t={time} #queue={queue.Count} robots={string.Join(' ', robots)} materials={string.Join(' ', materials)}");
-
-				if (time > maxtime)
+				// Blueprint 16: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 11 clay. Each geode robot costs 3 ore and 15 obsidian.
+				var (_, a, b, c, d, e, f) = s.RxMatch("Blueprint %d: Each ore robot costs %d ore. Each clay robot costs %d ore. Each obsidian robot costs %d ore and %d clay. Each geode robot costs %d ore and %d obsidian.").Get<int, int, int, int, int, int, int>();
+				return new Blueprint
 				{
-				//	Console.WriteLine($"time={time}");
-					maxtime = time;
-				}
-				var geodes = materials[Geo];
-
-				if (time == minutes)
-				{
-					//Console.WriteLine($"t={time} robots={string.Join(' ', robots)} materials={string.Join(' ', materials)}");
-					if (geodes > maxproduced)
+					Defs = new RobotDef[]
 					{
-						maxproduced = geodes;
-				//		Console.WriteLine($"t={time} #queue={queue.Count} robots={string.Join(' ', robots)} materials={string.Join(' ', materials)} max={maxproduced}");
-					}
-					break;
-					continue;
-				}
-
-				var maxmore = Enumerable.Range(robots[Geo], minutes - time).Sum();
-				if (materials[Geo] + maxmore <= maxproduced)
-					continue;
-
-
-				var rstate = (long)Key(robots) * minutes + time;
-
-				if (!seen.TryGetValue(rstate, out var mats))
-				{
-					mats = seen[rstate] = new List<int[]>();
-				}
-				if (mats.Any(m => IsAllLessThanOrEqual(materials, m)))
-					continue;
-				foreach (var lower in mats.Where(m => IsAllLessThanOrEqual(m, materials)).ToArray())
-				{
-					//Console.Write("-");
-					mats.Remove(lower);
-				}
-				mats.Add(materials);
-
-
-				bool IsAllLessThanOrEqual(int[] a, int[] b)
-				{
-					for (var i = 0; i < N; i++)
-						if (a[i] > b[i])
-							return false;
-					return true;
-				}
-
-
-				bool CanBuildRobot(int[] m2, int material)
-				{
-					var costs = bp.Defs[material].Costs;
-					for (var i = 0; i < N; i++)
-					{
-						if (costs[i] > m2[i])
-							return false;
-					}
-					return true;
-				}
-
-				void BuildRobot(int[] r2, int[] m2, int material)
-				{
-					var costs = bp.Defs[material].Costs;
-					for (var i = 0; i < N; i++)
-					{
-						m2[i] -= costs[i];
-					}
-					r2[material]++;
-				}	
-
-				for (var material = N-1; material >= 0; material--)
-				{
-					var r2 = robots.ToArray();
-					var m2 = materials.ToArray();
-					if (CanBuildRobot(m2, material))
-					{
-						BuildRobot(r2, m2, material);
-					}
-
-					for (var i = 0; i < N; i++)
-					{
-						m2[i] += robots[i];
-					}
-
-					var s2 = State(m2, r2) * minutes + time;
-					if (statesseen.Contains(s2))
-						continue;
-					statesseen.Add(s2);
-
-					var maxmore2 = Enumerable.Range(r2[Geo], minutes - (time + 1)).Sum();
-					var m2maxproduce = m2[Geo] + maxmore2;
-					if (m2maxproduce <= maxproduced)
-						continue;
-
-					//var prio = 10000000*(minutes-time) - ((r2[Geo]+m2[Geo])*10000 + (r2[Obs]+m2[Obs])*100); 250sec
-					//var prio = time*5 - ((r2[Geo]+m2[Geo])*10 + (r2[Obs]+m2[Obs])); // 11sec
-					//var prio = time*10 - ((r2[Geo]+m2[Geo])*10 + (r2[Obs]+m2[Obs])); // 11sec
-					var prio = -m2maxproduce;
-					
-					//queue.Enqueue((s2, m2, r2, time + 1), prio);
-					queue.Enqueue((s2, m2, r2, time + 1), prio);
-				}
-
-
+						new RobotDef(new [] { a, 0, 0, 0 }),
+						new RobotDef(new [] { b, 0, 0, 0 }),
+						new RobotDef(new [] { c, d, 0, 0 }),
+						new RobotDef(new [] { e, 0, f, 0 })
+					},
+					MaxOreCost = new[] {b, c, e}.Max(),
+					MaxClayCost = d,
+					MaxObsCost = f
+				};
 			}
 
-			Console.WriteLine($"##### {index} max={maxproduced}");
-			// Console.WriteLine();
-			return maxproduced;
+			public int FindMaxGeodesOpened(int minutes)
+			{
+				var N = 4;
 
+				var maxGeodes = 0;
+
+				// Optimization
+				var seenRobots = new Dictionary<long, List<int[]>>();
+
+				var stack = new Stack<(int[], int[], int)>();
+				var robots0 = new[] { 1, 0, 0, 0}; // 1 ore-producing robot
+				var materials0 = new[] { 0, 0, 0, 0};
+				stack.Push((robots0, materials0, 0));
+
+				while (stack.TryPop(out var item))
+				{
+					var (robots, materials, time) = item;
+
+					// Record the max number of geodes produced
+					if (time == minutes)
+					{
+						var geodes = materials[Geo];
+						if (geodes > maxGeodes)
+							maxGeodes = geodes;
+						continue;
+					}
+
+					// Optimization: Skip if this state can't ever possibly produce more
+					// geodes than the maximum we've seen so far
+					var maxmoreGeodes = robots[Geo] == 0
+						? (minutes-time)*(minutes-time+1)/2
+						: Enumerable.Range(robots[Geo], minutes - time).Sum();
+					if (materials[Geo] + maxmoreGeodes <= maxGeodes)
+						continue;
+
+					// Optimization: Skip if we've seen this set of robots before in an
+					// earlier state at the same time, but with less materials been produced
+					static long Key(int[] x) => (long)(((long)x[0]<<21) + ((long)x[1]<<14) + (x[2]<<7) + x[3]);
+					var rstate = (long)Key(robots) * minutes + time;
+					if (!seenRobots.TryGetValue(rstate, out var seenMaterials))
+						seenMaterials = seenRobots[rstate] = new List<int[]>();
+					if (seenMaterials.Any(m => IsAllLessThanOrEqual(materials, m)))
+						continue;
+					foreach (var lower in seenMaterials.Where(m => IsAllLessThanOrEqual(m, materials)).ToArray())
+						seenMaterials.Remove(lower);
+					seenMaterials.Add(materials);
+
+					bool IsAllLessThanOrEqual(int[] a, int[] b)
+					{
+						for (var i = 0; i < N; i++)
+							if (a[i] > b[i])
+								return false;
+						return true;
+					}
+
+					// Check if we've got robots enough already to produce any kind of
+					// desired robot at every round so we don't need to build more of them
+					var hasEnoughObs = robots[Obs] >= MaxObsCost; // possibly fewer needed, not counting existing materials
+					var hasEnoughOre = robots[Ore] >= MaxOreCost;
+					var hasEnoughClay = materials[Clay] >= MaxClayCost;
+
+					// If we've not yet enough Obs/Orb robots then idle for a round
+					if (!hasEnoughObs || !hasEnoughOre)
+					{
+						var m2 = materials.ToArray();
+						for (var i = 0; i < N; i++)
+							m2[i] += robots[i];
+						stack.Push((robots, m2, time + 1));
+					}
+
+					// We always want to try to build a Geodes-robot. If there's time left
+					// for newly built robots to actually be productive then we also want
+					// to try to build them; but only those we need.
+
+					TryBuildRobot(Geo);
+					if (time < minutes)
+					{
+						if (!hasEnoughObs)
+							TryBuildRobot(Obs);
+						if (!hasEnoughClay)
+							TryBuildRobot(Clay);
+						if (!hasEnoughOre)
+							TryBuildRobot(Ore);
+					}
+
+					void TryBuildRobot(int material)
+					{
+						// Check if there's enough materials for this type of robot
+						var costs = Defs[material].Costs;
+						for (var i = 0; i < N; i++)
+						{
+							if (costs[i] > materials[i])
+								return;
+						}
+
+						// Build new robot out of materials and add it to set of robots
+						var r2 = robots.ToArray();
+						var m2 = materials.ToArray();
+						for (var i = 0; i < N; i++)
+						{
+							m2[i] -= costs[i];
+						}
+						r2[material]++;
+
+						// Let existing robots produce their materials and submit the state
+						for (var i = 0; i < N; i++)
+							m2[i] += robots[i];
+						stack.Push((r2, m2, time + 1));
+					}				
+				}
+
+				return maxGeodes;
+			}
 		}
-
-
-
 	}
 }
