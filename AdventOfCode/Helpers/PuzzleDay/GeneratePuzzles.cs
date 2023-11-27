723630f9071e981258dd2411dadca37960797ce3
ricflams <richard@flamsholt.dk>
2023-11-27 16:48:20 +0100

    Don't include snippets in all days' initial code

--- AdventOfCode/Helpers/PuzzleDay/GeneratePuzzles.cs
+++ AdventOfCode/Helpers/PuzzleDay/GeneratePuzzles.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using AdventOfCode.Helpers;
+﻿using System.IO;
 
 namespace AdventOfCode.Helpers.PuzzleDay
 {
