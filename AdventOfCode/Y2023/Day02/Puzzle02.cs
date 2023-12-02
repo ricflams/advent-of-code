b5f7aef657d24fb16359ba3bfb7f93eb7f5db6e5
Richard Flamsholt <richard@flamsholt.dk>
2023-12-02 07:03:20 +0100

    2023 Day 2: Cube Conundrum

--- AdventOfCode/Y2023/Day02/Puzzle02.cs
+++ AdventOfCode/Y2023/Day02/Puzzle02.cs
@@ -1,41 +1,75 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Y2023.Day02
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "TODAY";
+		public override string Name => "Cube Conundrum";
 		public override int Year => 2023;
 		public override int Day => 2;
 
 		public void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-			//Run("test2").Part1(0).Part2(0);
-			Run("input").Part1(0).Part2(0);
+			Run("test1").Part1(8).Part2(2286);
+			Run("input").Part1(2348).Part2(76008);
 		}
 
 		protected override long Part1(string[] input)
 		{
+			var games = ReadGames(input);
 
+			var possibleGames = games
+				.Sum(g => g.Cubes.All(c => c.R <= 12 && c.G <= 13 && c.B <= 14) ? g.Id : 0);
 
-			return 0;
+			return possibleGames;
 		}
 
 		protected override long Part2(string[] input)
 		{
+			var games = ReadGames(input);
 
+			var powerSum = games
+				.Sum(game =>
+				{
+					var r = game.Cubes.Max(x => x.R);
+					var g = game.Cubes.Max(x => x.G);
+					var b = game.Cubes.Max(x => x.B);
+					return r * g * b;
+				});
 
-			return 0;
+			return powerSum;
 		}
+
+		private record Game(int Id, (int R, int G, int B)[] Cubes);
+
+		private static Game[] ReadGames(string[] input)
+		{
+			// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+			return input
+				.Select((s,i) => {
+					var cubes = s.Split(':')[1]
+						.Split(';')
+						.Select(set =>
+						(
+							R: CubeCount(set, "red"),
+							G: CubeCount(set, "green"),
+							B: CubeCount(set, "blue")
+						))
+						.ToArray();
+					return new Game(i + 1, cubes);
+				})
+				.ToArray();
+
+			static int CubeCount(string set, string color)
+			{
+				var m = Regex.Match(set, $"(\\d+) {color}");
+				return m.Success ? int.Parse(m.Groups[1].Value) : 0;
+			}
+		}
+
 	}
 }
---
+++ AdventOfCode/Y2023/Day02/Puzzle02.raw.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using AdventOfCode.Helpers.String;
+using System.Runtime.Intrinsics.Arm;
+
+namespace AdventOfCode.Y2023.Day02.Raw
+{
+	internal class Puzzle : Puzzle<long, long>
+	{
+		public static Puzzle Instance = new();
+		public override string Name => "TODAY";
+		public override int Year => 2023;
+		public override int Day => 2;
+
+		public void Run()
+		{
+			Run("test1").Part1(8).Part2(2286);
+			Run("test2").Part1(0).Part2(0);
+			Run("input").Part1(0).Part2(0);
+		}
+
+		protected override long Part1(string[] input)
+		{
+
+			// Game 8: 4 green, 2 red, 14 blue; 9 green, 1 red, 15 blue; 2 green, 9 red, 8 blue; 11 green, 7 red, 8 blue; 9 red, 7 green, 6 blue
+			var games = input
+				.Select((s,i) => {
+					var x1 = s.Split(':');
+					var game = i+1;
+					var rgbs = x1[1].Split(';').Select(rgb => {
+						int r=0, g=0, b=0;
+						foreach (var x in rgb.Split(','))
+						{
+							var (v, color) = x.RxMatch("%d %s").Get<int, string>();
+							if (color == "red")
+								r = v;
+							else if (color == "green")
+								g = v;
+							else if (color == "blue")
+								b = v;
+							else
+								throw new Exception();
+						}
+						
+						// var (r,g,b) = rgb.RxMatch("%d red, %d red, %d blue").Get<int, int, int>();
+						return (r, g, b);
+					})
+					.ToArray();
+					return rgbs;
+				})
+				.ToArray();
+
+
+
+				var possible = games
+					.Select((g,i) => {
+						var rgbs = g;
+						return rgbs.All(x => x.r <= 12 && x.g <= 13 && x.b <= 14) ? i+1 : 0;
+					})
+					.Sum();
+
+			return possible;
+		}
+
+		protected override long Part2(string[] input)
+		{
+			// Game 8: 4 green, 2 red, 14 blue; 9 green, 1 red, 15 blue; 2 green, 9 red, 8 blue; 11 green, 7 red, 8 blue; 9 red, 7 green, 6 blue
+			var games = input
+				.Select((s,i) => {
+					var x1 = s.Split(':');
+					var game = i+1;
+					var rgbs = x1[1].Split(';').Select(rgb => {
+						int r=0, g=0, b=0;
+						foreach (var x in rgb.Split(','))
+						{
+							var (v, color) = x.RxMatch("%d %s").Get<int, string>();
+							if (color == "red")
+								r = v;
+							else if (color == "green")
+								g = v;
+							else if (color == "blue")
+								b = v;
+							else
+								throw new Exception();
+						}
+						
+						// var (r,g,b) = rgb.RxMatch("%d red, %d red, %d blue").Get<int, int, int>();
+						return (r, g, b);
+					})
+					.ToArray();
+					return rgbs;
+				})
+				.ToArray();
+
+
+
+				var possible = games
+					.Select((g,i) => {
+						var rgbs = g;
+						// if (rgbs.All(x => x.r <= 12 && x.g <= 13 && x.b <= 14)) {
+
+						// }
+						var minr = rgbs.Max(x => x.r);
+						var ming = rgbs.Max(x => x.g);
+						var minb = rgbs.Max(x => x.b);
+						return minr * ming * minb;
+					})
+					.Sum();
+
+			return possible;
+		}
+	}
+}
