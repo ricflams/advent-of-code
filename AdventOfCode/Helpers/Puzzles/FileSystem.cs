76a3314b362ceff8433a96f7e5967ea5f6ce61ba
Richard Flamsholt <richard@flamsholt.dk>
2023-12-03 21:16:13 +0100

    Leaner FileSystem

--- AdventOfCode/Helpers/Puzzles/FileSystem.cs
+++ AdventOfCode/Helpers/Puzzles/FileSystem.cs
@@ -2,61 +2,41 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Xml.XPath;
 
 namespace AdventOfCode.Helpers.Puzzles
 {
 	internal class FileSystem(string root)
 	{
 		public static readonly FileSystem Instance = new(".");
-
 		private readonly string _root = root;
-		private readonly FileCache _cache = new(root);
 
 		public string[] ReadFile(int year, int day, string name)
 		{
-			var input = name == "input"
-				? _cache.CachedInputFile(year, day)
+			var text = name == "input"
+				? ReadInputFile(year, day)
 				: File.ReadAllLines(Path.Combine(_root, $"Y{year}/Day{day:D2}/{name}.txt"));
-			return input;
-		}
-	}
-
-	internal class FileCache
-	{
-		private readonly string CookieFilename = "aoc-sessioncookie";
-		private readonly string _cacheRoot;
-		private readonly string _sessionCookie;
-		private int _throttleInterval = 0;
-
-		public FileCache(string parent)
-		{
-			Console.WriteLine(Directory.GetCurrentDirectory());
-			_sessionCookie = File.Exists(CookieFilename) ? File.ReadAllText(CookieFilename) : null;
-			_cacheRoot = Path.Combine(parent, "cache");
+			return text;
 		}
 
-		public string[] CachedInputFile(int year, int day)
+
+		private string[] ReadInputFile(int year, int day)
 		{
-			var filename = Path.Combine(_cacheRoot, $"{year}_{day:D2}_input.txt");
+			var cache = Path.Combine(_root, "cache");
+			var filename = Path.Combine(cache, $"{year}_{day:D2}_input.txt");
 
 			if (!File.Exists(filename))
 			{
-				if (_sessionCookie == null)
+				var cookieFilename = Path.Combine(_root, "aoc-sessioncookie");
+				if (!File.Exists(cookieFilename))
 				{
-					throw new Exception($"Missing cookie file '{CookieFilename}'");
+					throw new Exception($"Missing cookie file '{cookieFilename}'");
 				}
-				if (string.IsNullOrEmpty(_sessionCookie))
-				{
-					throw new Exception($"Missing cookie in file '{CookieFilename}'");
-				}
+				var sessionCookie = File.ReadAllText(cookieFilename);
 
-				// Ensure cache folder exists
-				Directory.CreateDirectory(_cacheRoot);
 
 				var apiUrl = $"https://adventofcode.com/{year}/day/{day}/input";
 				using var client = new HttpClient();
-				client.DefaultRequestHeaders.Add("Cookie", $"session={_sessionCookie}");
+				client.DefaultRequestHeaders.Add("Cookie", $"session={sessionCookie}");
 				client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "github.com/ricflams/advent-of-code by richard@flamsholt.dk");
 
 				var response = client.GetAsync(apiUrl).Result;
@@ -67,12 +47,10 @@ namespace AdventOfCode.Helpers.Puzzles
 				}
 
 				var content = response.Content.ReadAsStringAsync().Result;
+				Directory.CreateDirectory(cache);
 				File.WriteAllText(filename, content);
 
-				Task.Delay(_throttleInterval).Wait();
-
-				if (_throttleInterval < 2000)
-					_throttleInterval += 1000;
+				Task.Delay(2000).Wait();
 			}
 
 			var input = File.ReadAllLines(filename);
