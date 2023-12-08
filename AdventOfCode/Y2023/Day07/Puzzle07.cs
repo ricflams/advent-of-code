ba1dd3b21a6d9c48f02eb2bedeb8a6e8db535493
Richard Flamsholt <richard@flamsholt.dk>
2023-12-08 07:40:41 +0100

    2023 Day 7 done

--- AdventOfCode/Y2023/Day07/Puzzle07.cs
+++ AdventOfCode/Y2023/Day07/Puzzle07.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
@@ -16,225 +15,109 @@ namespace AdventOfCode.Y2023.Day07
 		public override void Run()
 		{
 			Run("test1").Part1(6440).Part2(5905);
-			Run("test2").Part1(0).Part2(0);
 			Run("input").Part1(253933213).Part2(253473930);
 			Run("extra").Part1(253313241).Part2(253362743);
 		}
 
-		private int Value(char[] strength, string hand)
-		{
-			foreach (var s in strength)
-			{
-				if (hand.All(x => x == s))
-					return 1;
-			}
-			foreach (var s in strength)
-			{
-				if (hand.Count(x => x == s) == 4)
-					return 2;
-			}
-			foreach (var s1 in strength)
-			{
-				foreach (var s2 in strength)
-				{
-					if (s1 == s2)
-						continue;
-					if (hand.Count(x => x == s1) == 3 && hand.Count(x => x == s2) == 2)
-						return 3;
-				}						
-			}
-			foreach (var s in strength)
-			{
-				if (hand.Count(x => x == s) == 3)
-					return 4;
-			}
-			foreach (var s1 in strength)
-			{
-				foreach (var s2 in strength)
-				{
-					if (s1 == s2)
-						continue;
-					if (hand.Count(x => x == s1) == 2 && hand.Count(x => x == s2) == 2)
-						return 5;
-				}						
-			}
-			foreach (var s in strength)
-			{
-				if (hand.Count(x => x == s) == 2)
-					return 6;
-			}
-			return 7;
-		}
-
 		protected override long Part1(string[] input)
 		{
-			var hands = input
-				.Select(x => x.Split(' ').ToArray())
-				.Select(x => (Hand: x[0], Bid: int.Parse(x[1])))
-				.ToArray();
-			var strength = new char[] { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
+			var strengths = new char[] { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
 
-			var orderedHands = hands
-				.Select(hand => (hand, Value(strength, hand.Hand)))
-				.OrderByDescending(x => x.Item2)
-				.ThenBy(x => Array.IndexOf(strength, x.hand.Hand[0]))
-				.ThenBy(x => Array.IndexOf(strength, x.hand.Hand[1]))
-				.ThenBy(x => Array.IndexOf(strength, x.hand.Hand[2]))
-				.ThenBy(x => Array.IndexOf(strength, x.hand.Hand[3]))
-				.ThenBy(x => Array.IndexOf(strength, x.hand.Hand[4]))
-				.ToArray();
-
-			// Console.WriteLine();
-			// foreach (var h in orderedHands.Take(20))
-			// 	Console.WriteLine(h.hand);
-
-			var wins = orderedHands.Select((x, i) => (long)x.hand.Bid * (i+1))
-				.Sum();
+			var wins = CalculateWins(input, (cards, bid) =>
+			{
+				var strength = Hand.CalculateStrength(strengths, cards);
+				var value = Hand.CalculateValue(strengths, cards);
+				return new Hand(strength, value, bid);
+			});
 
 			return wins;
 		}
 
 		protected override long Part2(string[] input)
 		{
-			var hands = input
-				.Select(x => x.Split(' ').ToArray())
-				.Select(x => (Hand: x[0], Bid: int.Parse(x[1])))
-				.ToArray();
-			var strength = new char[] { 'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A' };
+			var strengths = new char[] { 'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A' };
 
-			IEnumerable<string> Jokerize(string hand, int[] pos)
+			var wins = CalculateWins(input, (cards, bid) =>
 			{
-				if (pos.Length == 0)
-					yield return hand;
-				else
-				{
-					foreach (var s in strength)
-					{
-						if (s == 'J') continue;
-						var jokered = hand.ToCharArray();
-						jokered[pos[0]] = s;
-						var hand2 = new string(jokered);
-						foreach (var j in Jokerize(hand2, pos[1..]))
-							yield return j;
-					}
-				}
-			}
+				// Replace any joker J with the most card that yields the highest value.
+				// Seems it will always be the most frequent card but calculating the
+				// max-value is about just as fast as finding the most frequent card.
+				var njokers = cards.Count(c => c == 'J');
+				var strength = njokers == 0 && njokers == 5
+					? Hand.CalculateStrength(strengths, cards)
+					: cards.Select(c => Hand.CalculateStrength(strengths, cards.Replace('J', c))).Max();
 
-			var orderedHands = hands
-				.Select(hand =>
-				{
-					var jokers = hand.Hand.Select((h, i) => (h,i)).Where(x => x.h == 'J').Select(x => x.i).ToArray();
-					// if (jokers.Length >= 4)
-					// 	return (hand.Hand, Value: 1, hand.Bid);
-					// if (jokers.Length >= 3)
-					// 	Console.Write('#');
-					// var jokered = Jokerize(hand.Hand, jokers)
-					// 	.ToArray()
-					// 	.Select(x => (Hand: x, Value: Value(strength, x)))
-					// 	.OrderByDescending(x => x.Value)
-					// 	.ThenBy(x => Array.IndexOf(strength, x.Hand[0]))
-					// 	.ThenBy(x => Array.IndexOf(strength, x.Hand[1]))
-					// 	.ThenBy(x => Array.IndexOf(strength, x.Hand[2]))
-					// 	.ThenBy(x => Array.IndexOf(strength, x.Hand[3]))
-					// 	.ThenBy(x => Array.IndexOf(strength, x.Hand[4]))						
-					// 	.ToArray();
-					// var jokervalue = jokered.Last().Value;
-					
-
-
-// 1  Five of a kind, where all five cards have the same label: AAAAA
-// 2  Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-// 3  Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-// 4  Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-// 5  Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-// 6  One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-// 7  High card, where all cards' labels are distinct: 23456					
-
-// JJJJJ _ -> 1
-
-// JJJJa _ -> 1
-
-// JJJaa 3 -> 1
-// JJJab 4 -> 2
-
-// JJaaa 3 -> 1
-// JJaab 5 -> 2
-// JJabc 6 -> 4
-
-// Jaaaa 2 -> 1 
-// Jaaab 4 -> 2
-// Jaabb 5 -> 3
-// Jaabc 6 -> 4
-// Jabcd 7 -> 6
-
-// abcde -> value
-
-					var cards = hand.Hand;
-					var njokers = hand.Hand.Count(c => c == 'J');
-					if (njokers > 0 && njokers < 5)
-					{
-						var most = cards
-							.ToArray()
-							.Where(c => c != 'J')
-							.GroupBy(c => c)
-							.OrderByDescending(x => x.Count())
-							.Select(x => x.Key)
-							.First();
-						cards = cards.Replace('J', most);
-					}
-					var jokervalue2 = Value(strength, cards);
-					// var njokers = hand.Hand.Count(c => c == 'J');
-					// var value = Value(strength, hand.Hand);
-					// var jokervalue2 = njokers switch
-					// {
-					// 	5 => 1,
-					// 	4 => 1,
-					// 	3 => value switch
-					// 		{
-					// 			3 => 1,
-					// 			4 => 2,
-					// 			_ => throw new Exception()
-					// 		},
-					// 	2 => value switch
-					// 		{
-					// 			3 => 1,
-					// 			5 => 2,
-					// 			6 => 4,
-					// 			_ => throw new Exception()
-					// 		},
-					// 	1 => value switch
-					// 		{
-					// 			2 => 1,
-					// 			4 => 2,
-					// 			5 => 3,
-					// 			6 => 4,
-					// 			7 => 6,
-					// 			_ => throw new Exception()
-					// 		},
-					// 	_ => value
-					// };
-
-					// if (jokervalue != jokervalue2)
-					// 	Console.WriteLine($"{hand.Hand}: {jokervalue2} should be {jokervalue}");
-
-					return (hand.Hand, Value: jokervalue2, hand.Bid);
-				})
-				.OrderByDescending(x => x.Value)
-				.ThenBy(x => Array.IndexOf(strength, x.Hand[0]))
-				.ThenBy(x => Array.IndexOf(strength, x.Hand[1]))
-				.ThenBy(x => Array.IndexOf(strength, x.Hand[2]))
-				.ThenBy(x => Array.IndexOf(strength, x.Hand[3]))
-				.ThenBy(x => Array.IndexOf(strength, x.Hand[4]))
-				.ToArray();
-
-			// Console.WriteLine();
-			// foreach (var h in orderedHands.Take(5))
-			// 	Console.WriteLine(h.Hand);
-
-			var wins = orderedHands.Select((x, i) => (long)x.Bid * (i+1))
-				.Sum();
+				var value = Hand.CalculateValue(strengths, cards);
+				return new Hand(strength, value, bid);
+			});
 
 			return wins;
 		}
+
+		private static long CalculateWins(string[] input, Func<string, int, Hand> MakeHand) =>
+			input
+				.Select(x => x.Split(' ').ToArray())
+				.Select(x =>
+				{
+ 					var (cards, bid) = (x[0], int.Parse(x[1]));
+					return MakeHand(cards, bid);
+				})
+				.OrderBy(h => h.Strength)
+				.ThenBy(h => h.Value)
+				.Select((h, i) => (long)(i+1) * h.Bid)
+				.Sum();	
+
+		private class Hand(int strength, int value, int bid)
+		{
+			public int Strength { get; private set; } = strength;
+			public int Value { get; private set; } = value;
+			public int Bid { get; private set; } = bid;
+
+			public static int CalculateStrength(char[] strengths, string cards)
+			{
+				// Find out how many there is of all the cards;
+				// eg cardCount 'Q' == 2 means there are two Qs
+				var cardCount = new int[strengths.Length+1];
+				foreach (var c in cards)
+				{
+					var v = Array.IndexOf(strengths, c);
+					cardCount[v]++;
+				}
+
+				// Now find out how many of the kinds there are;
+				// Eg kinds[3] == 1 means that there is one set of threes, ie three of a kind
+				var kinds = new int[6];
+				foreach (var v in cardCount)
+				{
+					kinds[v]++;
+				}
+
+				// Give cards strengths from 7 (5 of a kind, strongest) to 1 (weakest)
+				var strength =
+					kinds[5] == 1 ? 7 :
+					kinds[4] == 1 ? 6 :
+					kinds[3] == 1 ? (kinds[2] == 1 ? 5 : 4) :
+					kinds[2] == 2 ? 3 :
+					kinds[2] == 1 ? 2 :
+					1;
+
+				return strength;
+			}
+
+			public static int CalculateValue(char[] strengths, string cards)
+			{
+				// Calculate the value of the cards as one single number
+				// which is optimal for sorting
+				var value = 0;
+				foreach (var c in cards)
+				{
+					var v =  Array.IndexOf(strengths, c);
+					value = value * strengths.Length + v;
+				}
+
+				return value;
+			}
+		}
+
 	}
 }
