84fa3e26c0c45b84e387dff86d4f0f9546cabcdb
Richard Flamsholt <richard@flamsholt.dk>
2023-12-11 21:29:17 +0100

    2023 Day 11: Cosmic Expansion

--- AdventOfCode/Y2023/Day11/Puzzle11.cs
+++ AdventOfCode/Y2023/Day11/Puzzle11.cs
@@ -1,42 +1,74 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2023.Day11
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "";
+		public override string Name => "Cosmic Expansion";
 		public override int Year => 2023;
 		public override int Day => 11;
 
 		public override void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-			Run("test2").Part1(0).Part2(0);
-			Run("input").Part1(0).Part2(0);
-			Run("extra").Part1(0).Part2(0);
+			Run("test1").Part1(374);
+			Run("input").Part1(9233514).Part2(363293506944);
+			Run("extra").Part1(9795148).Part2(650672493820);
 		}
 
 		protected override long Part1(string[] input)
 		{
-
-
-			return 0;
+			return SumByExpansion(input, 2);
 		}
 
 		protected override long Part2(string[] input)
 		{
+			return SumByExpansion(input, 1_000_000);
+		}
 
+		private static long SumByExpansion(string[] input, long expandTo)
+		{
+			// Find all galaxies
+			var map = CharMap.FromArray(input);
+			var galaxies = map.AllPoints(c => c == '#').ToArray();
 
-			return 0;
+			// Find x,y coordinates of all galaxies
+			var xCoords = new HashSet<int>(galaxies.Select(g => g.X));
+			var yCoords = new HashSet<int>(galaxies.Select(g => g.Y));
+			var (w, h) = map.Size();
+
+			// Find all the empty rows/columns
+			var xEmpty = Enumerable.Range(0, w).Where(x => !xCoords.Contains(x)).ToArray();
+			var yEmpty = Enumerable.Range(0, h).Where(y => !yCoords.Contains(y)).ToArray();
+
+			// Map all x and y coordinates just once for use in mapping all
+			// the galaxies' x,y-coordinates below.
+			var xMapping =  Enumerable.Range(0, w).Select(x => x + xEmpty.Count(p => p < x)*(expandTo-1)).ToArray();
+			var yMapping =  Enumerable.Range(0, h).Select(y => y + yEmpty.Count(p => p < y)*(expandTo-1)).ToArray();
+
+			// Expand all the galaxies, using the one-time mapping we found above
+			var expanded = galaxies
+				.Select(p => (X: xMapping[p.X], Y: yMapping[p.Y]))
+				.ToArray();
+
+			// Sum all the distances between every pair of galaxies.
+			// Just use two simple loops because it's faster than linq etc
+			var sum = 0L;
+			for (var i = 0; i < expanded.Length; i++)
+			{
+				var (xi, yi) = expanded[i];
+				for (var j = i+1; j < expanded.Length; j++)
+				{
+					var (xj, yj) = expanded[j];
+					sum += Math.Abs(xi - xj) + Math.Abs(yi - yj);
+				}
+			}
+
+			return sum;
 		}
 	}
 }
---
+++ AdventOfCode/Y2023/Day11/Puzzle11.raw.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using AdventOfCode.Helpers.String;
+using AdventOfCode.Y2021.Day23.Raw;
+
+namespace AdventOfCode.Y2023.Day11.Raw
+{
+	internal class Puzzle : Puzzle<long, long>
+	{
+		public static Puzzle Instance = new();
+		public override string Name => "";
+		public override int Year => 2023;
+		public override int Day => 11;
+
+		public override void Run()
+		{
+			Run("test1").Part1(374).Part2(0);
+			//Run("test2").Part1(0).Part2(0);
+			Run("input").Part1(0).Part2(0);
+			// not right 36337370229
+			// not right 363293870229
+			//Run("extra").Part1(0).Part2(0);
+		}
+
+		protected override long Part1(string[] input)
+		{
+			var map = CharMap.FromArray(input);
+
+			var galaxies = map.AllPoints(c => c == '#').ToArray();
+			var (w, h) = map.Size();
+
+			var emptyx = Enumerable.Range(0, w).Where(x => !galaxies.Any(p => p.X == x)).ToArray();
+			var emptyy = Enumerable.Range(0, h).Where(y => !galaxies.Any(p => p.Y == y)).ToArray();
+
+			var shifted = galaxies
+				.Select(p => {
+					var (x, y) = p;
+					x += emptyx.Count(p => p < x);
+					y += emptyy.Count(p => p < y);
+					return Point.From(x, y);
+				}).ToArray();
+
+			var sum = 0;
+			for (var i = 0; i < shifted.Length-1; i++)
+			{
+				var g1 = shifted[i];
+				//var minstep = galaxies[(i+1)..].Min(g => CalcDist(g1, g));
+				var steps = shifted[(i+1)..].Sum(g => CalcDist(g1, g));
+				sum += steps;
+				
+			}
+
+			int CalcDist(Point p1, Point p2)
+			{
+				return p1.ManhattanDistanceTo(p2);
+			}
+
+
+			return sum;
+		}
+
+		protected override long Part2(string[] input)
+		{
+			var map = CharMap.FromArray(input);
+
+			var galaxies = map.AllPoints(c => c == '#').ToArray();
+			var (w, h) = map.Size();
+
+			var emptyx = Enumerable.Range(0, w).Where(x => !galaxies.Any(p => p.X == x)).ToArray();
+			var emptyy = Enumerable.Range(0, h).Where(y => !galaxies.Any(p => p.Y == y)).ToArray();
+
+			var expandBy = 1000000L;
+			var shifted = galaxies
+				.Select(p => {
+					long x = p.X;
+					long y = p.Y;
+					x += emptyx.Count(p => p < x)*(expandBy-1);
+					y += emptyy.Count(p => p < y)*(expandBy-1);
+					return (X:x, Y:y);
+				}).ToArray();
+
+			var sum = 0L;
+			for (var i = 0; i < shifted.Length-1; i++)
+			{
+				var g1 = shifted[i];
+				//var minstep = galaxies[(i+1)..].Min(g => CalcDist(g1, g));
+				var steps = shifted[(i+1)..].Sum(g => 
+					Math.Abs(g.X - g1.X) + Math.Abs(g.Y - g1.Y));
+				sum += steps;
+				
+			}
+
+			// int CalcDist(Point p1, Point p2)
+			// {
+			// 	return Math.Abs(X - pos.X) + Math.Abs(Y - pos.Y);
+			// }
+
+
+			return sum;
+
+			return 0;
+		}
+	}
+}
