d02f19c98738b5e4bb5bcd280357dd97a996a470
Richard Flamsholt <richard@flamsholt.dk>
2023-12-11 18:59:01 +0100

    2023 Day 9: Mirage Maintenance, cleanup

--- AdventOfCode/Y2023/Day09/Puzzle09.cs
+++ AdventOfCode/Y2023/Day09/Puzzle09.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
 using AdventOfCode.Helpers.String;
@@ -13,56 +8,50 @@ namespace AdventOfCode.Y2023.Day09
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "";
+		public override string Name => "Mirage Maintenance";
 		public override int Year => 2023;
 		public override int Day => 9;
 
 		public override void Run()
 		{
 			Run("test1").Part1(114).Part2(2);
-			//Run("test2").Part1(0).Part2(0);
-			Run("input").Part1(1731106378).Part2(0);
-			//Run("extra").Part1(0).Part2(1087);
+			Run("input").Part1(1731106378).Part2(1087);
 		}
 
 		protected override long Part1(string[] input)
 		{
-			var oasis = input
-				.Select(s => s.ToIntArray())
-				.ToArray();
+			var reports = input.Select(s => s.ToIntArray());
 
-			var sum = oasis.Sum(LastNumber);
-
-			int LastNumber(int[] hist)
-			{
-				var diff = Enumerable.Range(0, hist.Length - 1).Select(i => hist[i+1] - hist[i]).ToArray();
-				if (diff.All(x => x == 0))
-					return hist.Last();
-				return hist.Last() + LastNumber(diff);
-			}
-
+			var sum = reports.Sum(ExtrapolateNextValue);
 			return sum;
-		}
 
-		
+			static int ExtrapolateNextValue(int[] seq)
+			{
+				// Make new array of all the diffs
+				// If they're all 0 then we're done; else repeat, adding to the last number
+				var nextSeq = seq.Skip(1).Select((v, i) => v - seq[i]).ToArray();
+				if (nextSeq.All(x => x == 0))
+					return seq.Last();
+				return seq.Last() + ExtrapolateNextValue(nextSeq);
+			}
+		}
 
 		protected override long Part2(string[] input)
 		{
-			var oasis = input
-				.Select(s => s.ToIntArray())
-				.ToArray();
+			var reports = input.Select(s => s.ToIntArray());
 
-			var sum = oasis.Sum(LastNumber);
-
-			int LastNumber(int[] hist)
-			{
-				var diff = Enumerable.Range(0, hist.Length - 1).Select(i => hist[i+1] - hist[i]).ToArray();
-				if (diff.All(x => x == 0))
-					return hist.Last();
-				return hist.First() - LastNumber(diff);
-			}
-
+			var sum = reports.Sum(ExtrapolatePrevValue);
 			return sum;
+
+			static int ExtrapolatePrevValue(int[] seq)
+			{
+				// Make new array of all the diffs
+				// If they're all 0 then we're done; else repeat, subtracting from the first number
+				var nextSeq = seq.Skip(1).Select((v, i) => v - seq[i]).ToArray();
+				if (nextSeq.All(x => x == 0))
+					return seq.Last();
+				return seq.First() - ExtrapolatePrevValue(nextSeq);
+			}
 		}
 	}
 }
