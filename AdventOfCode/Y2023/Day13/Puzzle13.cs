7099d028f4cb8680247840550e1b62acac1e7242
Richard Flamsholt <richard@flamsholt.dk>
2023-12-13 20:56:58 +0100

    2023 Day 13: Point of Incidence

--- AdventOfCode/Y2023/Day13/Puzzle13.cs
+++ AdventOfCode/Y2023/Day13/Puzzle13.cs
@@ -1,42 +1,104 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.IO;
-using System.Text;
 using AdventOfCode.Helpers;
 using AdventOfCode.Helpers.Puzzles;
-using AdventOfCode.Helpers.String;
 
 namespace AdventOfCode.Y2023.Day13
 {
 	internal class Puzzle : Puzzle<long, long>
 	{
 		public static Puzzle Instance = new();
-		public override string Name => "";
+		public override string Name => "Point of Incidence";
 		public override int Year => 2023;
 		public override int Day => 13;
 
 		public override void Run()
 		{
-			Run("test1").Part1(0).Part2(0);
-			Run("test2").Part1(0).Part2(0);
-			Run("input").Part1(0).Part2(0);
-			Run("extra").Part1(0).Part2(0);
+			Run("test1").Part1(405).Part2(400);
+			Run("input").Part1(31877).Part2(42996);
+			Run("extra").Part1(30802).Part2(37876);
 		}
 
 		protected override long Part1(string[] input)
 		{
+			return input.GroupByEmptyLine().Sum(ReflectionValue);
 
+			static int ReflectionValue(string[] map)
+			{
+				var yreflex = FindReflections(map).FirstOrDefault();
+				if (yreflex > 0)
+					return 100*yreflex;
 
-			return 0;
+				map = CharMatrix.FromArray(map).RotateClockwise(90).ToStringArray();
+				return FindReflections(map).First();
+			}
+
 		}
 
 		protected override long Part2(string[] input)
 		{
+			return input.GroupByEmptyLine().Sum(ReflectionSmudge);
 
+			static int ReflectionSmudge(string[] map)
+			{
+				var reflection = FindReflectionSmudge(map).FirstOrDefault();
+				if (reflection > 0)
+					return 100*reflection;
 
-			return 0;
+				map = CharMatrix.FromArray(map).RotateClockwise(90).ToStringArray();
+				return FindReflectionSmudge(map).First();
+
+				static IEnumerable<int> FindReflectionSmudge(string[] map)
+				{
+					var reflection = FindReflections(map).FirstOrDefault();
+
+					for (var i = 0; i < map.Length; i++)
+					{
+						var line = map[i].ToCharArray();
+						for (var j = 0; j < line.Length; j++)
+						{
+							map[i] = Flip(line, j);
+							foreach (var r in FindReflections(map).Where(r => r != reflection))
+								yield return r;
+							map[i] = Flip(line, j);
+						}
+					}
+
+					static string Flip(char[] line, int pos)
+					{
+						line[pos] = line[pos] == '.' ? '#' : '.';
+						return new string(line);
+					}
+				}			
+			}
+		}
+
+		private static IEnumerable<int> FindReflections(string[] map)
+		{
+			// Test all horizontal lines for possible reflection, starting
+			// at 1 since line 0 can't reflect "upwards". For each horizontal
+			// candidate check if all lines up and down are equal; if they are
+			// then we've found a reflection-line at hor.
+			// Just use two pointers, up and down, instead of being clever and
+			// using just one plus some math; this is easier to grasp.
+			var N = map.Length;
+			for (var hor = 1; hor < N; hor++)
+			{
+				var up = hor-1;
+				var down = hor;
+				while (true)
+				{
+					if (up < 0 || down == N)
+					{
+						yield return hor;
+						break;
+					}
+					if (map[up] != map[down])
+						break;
+					up--;
+					down++;
+				}
+			}
 		}
 	}
 }
---
+++ AdventOfCode/Y2023/Day13/Puzzle13.raw.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using AdventOfCode.Helpers.String;
+using System.Net;
+using MathNet.Numerics.Optimization.LineSearch;
+
+namespace AdventOfCode.Y2023.Day13.Raw
+{
+	internal class Puzzle : Puzzle<long, long>
+	{
+		public static Puzzle Instance = new();
+		public override string Name => "";
+		public override int Year => 2023;
+		public override int Day => 13;
+
+		public override void Run()
+		{
+			Run("test1").Part1(405).Part2(400);
+//			Run("test2").Part1(0).Part2(0);
+			Run("input").Part1(31877).Part2(42996);
+			// 27418 too low
+
+//			Run("extra").Part1(0).Part2(0);
+		}
+
+		protected override long Part1(string[] input)
+		{
+			var maps = input
+				.GroupByEmptyLine()
+				.Select(x => x.ToArray())
+				//.Select(lines => CharMap.FromArray(lines))
+				.ToArray();
+			
+			return maps.Sum(ReflectionValue);
+		}
+
+		protected override long Part2(string[] input)
+		{
+			var maps = input
+				.GroupByEmptyLine()
+				.ToArray();
+			
+			return maps.Sum(ReflectionSmudge);
+		}
+
+		private static int ReflectionSmudge(string[] map)
+		{
+			if (map[0] == "###.#....##..")
+				;
+			var orgReflection = ReflectionValue2(map).First();
+			for (var i = 0; i < map.Length; i++)
+			{
+				var org = map[i];
+				for (var j = 0; j < org.Length; j++)
+				{
+					var line = org.ToCharArray();
+					line[j] = line[j] == '.' ? '#' : '.';
+					map[i] = new string(line);
+					var reflections = ReflectionValue2(map).ToArray();
+					var newReflection = reflections.Where(x => x != orgReflection).FirstOrDefault();
+					if (newReflection > 0)
+					//if (reflection > 0 && reflection != orgReflection)
+						return newReflection;
+					map[i] = org;
+				}
+			}
+			Console.WriteLine("bad");
+			CharMap.FromArray(map).ConsoleWrite();
+			return 0;
+			throw new Exception();
+		}
+
+
+
+		private static IEnumerable<int> ReflectionValue2(string[] map)
+		{
+			foreach (var yreflex in ReflectionAt2(map))
+			// if (yreflex >= 0)
+			{
+				yield return 100*yreflex;
+			}
+
+
+			var mm = CharMatrix.FromArray(map);
+			mm = mm.RotateClockwise(90);
+			var map2 = mm.ToStringArray();
+
+			foreach (var xreflex in ReflectionAt2(map2))
+			//if (xreflex >= 0)
+			{
+				yield return xreflex;
+			}
+
+			// return -1;
+		}
+
+		private static IEnumerable<int> ReflectionAt2(string[] map)
+		{
+			var h = map.Length;
+
+			for (var top = 1; top < h; top++)
+			{
+				var y1 = top-1;
+				var y2 = top;
+				while (true)
+				{
+					if (y1 < 0 || y2 == h)
+					{
+						yield return top;
+						break;
+					}
+					if (map[y1] != map[y2])
+						break;
+					y1--;
+					y2++;
+				}
+			}
+		}
+
+		private static int ReflectionValue(string[] map)
+		{
+			var yreflex = ReflectionAt(map);
+			if (yreflex >= 0)
+			{
+				return 100*yreflex;
+			}
+
+
+			var mm = CharMatrix.FromArray(map);
+			mm = mm.RotateClockwise(90);
+			var map2 = mm.ToStringArray();
+
+			var xreflex = ReflectionAt(map2);
+			if (xreflex >= 0)
+			{
+				return xreflex;
+			}
+
+			return -1;
+		}
+
+		private static int ReflectionAt(string[] map)
+		{
+			var h = map.Length;
+
+			for (var top = 1; top < h; top++)
+			{
+				var y1 = top-1;
+				var y2 = top;
+				while (true)
+				{
+					if (y1 < 0 || y2 == h)
+						return top;
+					if (map[y1] != map[y2])
+						break;
+					y1--;
+					y2++;
+				}
+			}
+			return -1;
+
+		}
+
+
+	}
+}
