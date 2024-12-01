898a5fedaccc7ff34177c1ad8a4e3094002ef0a0
Richard Flamsholt <richard@flamsholt.dk>
2024-12-01 07:01:19 +0100

    Add templates for 2024

---
+++ AdventOfCode/Templates/Y2024/Day06/Puzzle06.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using AdventOfCode.Helpers;
+using AdventOfCode.Helpers.Puzzles;
+using AdventOfCode.Helpers.String;
+
+namespace AdventOfCode.Y2024.Day06
+{
+	internal class Puzzle : Puzzle<long, long>
+	{
+		public static Puzzle Instance = new();
+		public override string Name => "";
+		public override int Year => 2024;
+		public override int Day => 6;
+
+		public override void Run()
+		{
+			Run("test1").Part1(0).Part2(0);
+			//Run("test2").Part1(0).Part2(0);
+			Run("input").Part1(0).Part2(0);
+			//Run("extra").Part1(0).Part2(0);
+		}
+
+		protected override long Part1(string[] input)
+		{
+
+
+			return 0;
+		}
+
+		protected override long Part2(string[] input)
+		{
+
+
+			return 0;
+		}
+	}
+}
