e4977e964ab6b3eda13ccb7c0bf7055b50e275fb
Richard Flamsholt <richard@flamsholt.dk>
2024-12-05 07:20:02 +0100

    Comment out all

--- AdventOfCode/Helpers/PuzzleDay/Snippets.cs
+++ AdventOfCode/Helpers/PuzzleDay/Snippets.cs
@@ -1,140 +1,142 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using AdventOfCode.Helpers.String;
+﻿#if false
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Helpers.String;
 
-// namespace AdventOfCode.Helpers.PuzzleDay
-// {
-// 	internal class Thing
-// 	{
-// 		//private readonly 
-// 		public Thing(string[] lines)
-// 		{
-// 		}
-// 	}
+namespace AdventOfCode.Helpers.PuzzleDay
+{
+	internal class Thing
+	{
+		//private readonly 
+		public Thing(string[] lines)
+		{
+		}
+	}
 
-// 	internal class SomeGraph : Graph<HashSet<uint>> { }
+	internal class SomeGraph : Graph<HashSet<uint>> { }
 	
-// 	internal class Snippets
-// 	{
-// 		internal void Part1(string[] input)
-// 		{
-// 			{
-// 				var v = input.Select(int.Parse).ToArray();
-// 			}
-// 			{
-// 				var v = input[0].ToIntArray();
-// 			}
-// 			{
-// 				var things = input
-// 					.Skip(1)
-// 					.GroupByEmptyLine()
-// 					.Select(lines => new Thing(lines))
-// 					.ToMutableArray();
-// 			}
-// 			{
-// 				var things = input.Select(s => {
-// 					// case '%': sb.Append('%'); break;
-// 					// case '*': sb.Append(@"(.+)"); break;
-// 					// case 's': sb.Append(@"(\w+)"); break;
-// 					// case 'c': sb.Append(@"(.)"); break;
-// 					// case 'd': sb.Append(@"([-+]?\d+)"); break;
-// 					// case 'D': sb.Append(@"\s*([-+]?\d+)"); break;					
-// 					var (str, ch, d1, d2, rest) = s.RxMatch("%% %s,%c -> %d,%D %*").Get<string, char, int, int, string>();
-// 					return (str, ch, d1, d2, rest);
-// 				})
-// 				.ToArray();
-// 			}			
-// 			{
-// 				var map = new SparseMap<int>();
-// 				foreach (var s in input)
-// 				{
-// 					var (x1, y1, x2, y2) = s.RxMatch("%d,%d -> %d,%d").Get<int, int, int, int>();
-// 				}
-// 			}
-// 			{
-// 				var map = CharMap.FromArray(input);
-// 				var maze = new Maze(map)
-// 					.WithEntry(map.FirstOrDefault(c => c == '0')); // or Point.From(1, 1);
-// 				var dest = Point.From(2, 3);
-// 				var graph = Graph<char>.BuildUnitGraphFromMaze(maze);
-// 				var steps = graph.ShortestPathDijkstra(maze.Entry, dest);
-// 			}
-// 			{
-// 				var map = new CharMap('#');
-// 				var maze = new Maze(map).WithEntry(Point.From(1, 1));
-// 				var graph = SomeGraph.BuildUnitGraphFromMaze(maze);
-// 				var queue = new Queue<(SomeGraph.Vertex, uint, int)>();
-// 				queue.Enqueue((graph.Root, 0U, 0));
-// 				while (queue.Any())
-// 				{
-// 					var (node, found, steps) = queue.Dequeue();
-// 					if (node.Value.Contains(found))
-// 						continue;
-// 					node.Value.Add(found);
-// 					var ch = map[node.Pos];
-// 					if (char.IsDigit(ch))
-// 					{
+	internal class Snippets
+	{
+		internal void Part1(string[] input)
+		{
+			{
+				var v = input.Select(int.Parse).ToArray();
+			}
+			{
+				var v = input[0].ToIntArray();
+			}
+			{
+				var things = input
+					.Skip(1)
+					.GroupByEmptyLine()
+					.Select(lines => new Thing(lines))
+					.ToMutableArray();
+			}
+			{
+				var things = input.Select(s => {
+					// case '%': sb.Append('%'); break;
+					// case '*': sb.Append(@"(.+)"); break;
+					// case 's': sb.Append(@"(\w+)"); break;
+					// case 'c': sb.Append(@"(.)"); break;
+					// case 'd': sb.Append(@"([-+]?\d+)"); break;
+					// case 'D': sb.Append(@"\s*([-+]?\d+)"); break;					
+					var (str, ch, d1, d2, rest) = s.RxMatch("%% %s,%c -> %d,%D %*").Get<string, char, int, int, string>();
+					return (str, ch, d1, d2, rest);
+				})
+				.ToArray();
+			}			
+			{
+				var map = new SparseMap<int>();
+				foreach (var s in input)
+				{
+					var (x1, y1, x2, y2) = s.RxMatch("%d,%d -> %d,%d").Get<int, int, int, int>();
+				}
+			}
+			{
+				var map = CharMap.FromArray(input);
+				var maze = new Maze(map)
+					.WithEntry(map.FirstOrDefault(c => c == '0')); // or Point.From(1, 1);
+				var dest = Point.From(2, 3);
+				var graph = Graph<char>.BuildUnitGraphFromMaze(maze);
+				var steps = graph.ShortestPathDijkstra(maze.Entry, dest);
+			}
+			{
+				var map = new CharMap('#');
+				var maze = new Maze(map).WithEntry(Point.From(1, 1));
+				var graph = SomeGraph.BuildUnitGraphFromMaze(maze);
+				var queue = new Queue<(SomeGraph.Vertex, uint, int)>();
+				queue.Enqueue((graph.Root, 0U, 0));
+				while (queue.Any())
+				{
+					var (node, found, steps) = queue.Dequeue();
+					if (node.Value.Contains(found))
+						continue;
+					node.Value.Add(found);
+					var ch = map[node.Pos];
+					if (char.IsDigit(ch))
+					{
 
-// 					}
-// 					foreach (var n in node.Edges.Keys.Where(n => !n.Value.Contains(found)))
-// 					{
-// 						queue.Enqueue((n, found, steps + 1));
-// 					}
-// 				}
-// 			}
-// 			{
-// 				var ship = new Pose(Point.Origin, Direction.Right);
-// 				foreach (var line in input)
-// 				{
-// 					var n = int.Parse(line.Substring(1));
-// 					switch (line[0])
-// 					{
-// 						case 'N': ship.MoveUp(n); break;
-// 						case 'S': ship.MoveDown(n); break;
-// 						case 'E': ship.MoveRight(n); break;
-// 						case 'W': ship.MoveLeft(n); break;
-// 						case 'L': ship.RotateLeft(n); break;
-// 						case 'R': ship.RotateRight(n); break;
-// 						case 'F': ship.Move(n); break;
-// 						default:
-// 							throw new Exception($"Unknown action in {line}");
-// 					}
-// 				}
-// 				var dist = ship.Point.ManhattanDistanceTo(Point.Origin);
-// 			}
-// 			{
-// 				var departure = int.Parse(input[0]);
-// 				var id = input[1]
-// 					.Replace(",x", "")
-// 					.Split(",")
-// 					.Select(int.Parse)
-// 					.Select(id => new
-// 					{
-// 						Id = id,
-// 						Time = id - departure % id
-// 					})
-// 					.OrderBy(x => x.Time)
-// 					.First();
-// 			}
-// 			{
-// 				var map = CharMatrix.FromArray(input);
-// 				for (var i = 0; i < 100; i++)
-// 				{
-// 					map = map.Transform((ch, adjacents) =>
-// 					{
-// 						var n = 0;
-// 						foreach (var c in adjacents)
-// 						{
-// 							if (c == '|' && ++n >= 3)
-// 								return '|';
-// 						}
-// 						return ch;
-// 					});
-// 				}
-// 			}
-// 		}
+					}
+					foreach (var n in node.Edges.Keys.Where(n => !n.Value.Contains(found)))
+					{
+						queue.Enqueue((n, found, steps + 1));
+					}
+				}
+			}
+			{
+				var ship = new Pose(Point.Origin, Direction.Right);
+				foreach (var line in input)
+				{
+					var n = int.Parse(line.Substring(1));
+					switch (line[0])
+					{
+						case 'N': ship.MoveUp(n); break;
+						case 'S': ship.MoveDown(n); break;
+						case 'E': ship.MoveRight(n); break;
+						case 'W': ship.MoveLeft(n); break;
+						case 'L': ship.RotateLeft(n); break;
+						case 'R': ship.RotateRight(n); break;
+						case 'F': ship.Move(n); break;
+						default:
+							throw new Exception($"Unknown action in {line}");
+					}
+				}
+				var dist = ship.Point.ManhattanDistanceTo(Point.Origin);
+			}
+			{
+				var departure = int.Parse(input[0]);
+				var id = input[1]
+					.Replace(",x", "")
+					.Split(",")
+					.Select(int.Parse)
+					.Select(id => new
+					{
+						Id = id,
+						Time = id - departure % id
+					})
+					.OrderBy(x => x.Time)
+					.First();
+			}
+			{
+				var map = CharMatrix.FromArray(input);
+				for (var i = 0; i < 100; i++)
+				{
+					map = map.Transform((ch, adjacents) =>
+					{
+						var n = 0;
+						foreach (var c in adjacents)
+						{
+							if (c == '|' && ++n >= 3)
+								return '|';
+						}
+						return ch;
+					});
+				}
+			}
+		}
 
 
-// 	}
-// }
+	}
+}
+#endif
